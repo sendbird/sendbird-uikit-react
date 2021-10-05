0c77d8e8a54fbdc94fff43b606cd66f7e3c02a14
HoonBaek <hoon@sendbird.com>
2021-10-05 09:27:07 +0900

    Add tests for TextMessageItemBody component

--- src/ui/TextMessageItemBody/__tests__/TextMessageItemBody.spec.js
+++ src/ui/TextMessageItemBody/__tests__/TextMessageItemBody.spec.js
@@ -1,14 +1,157 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import TextMessageItemBody from "../index";
 
+const createMockMessage = (process) => {
+  const mockMessage = {
+    message: '',
+    updatedAt: 0,
+    reactions: [],
+    messageType: 'user',
+    isUserMessage: () => true,
+  };
+  return process ? process(mockMessage) : mockMessage;
+};
+
 describe('TextMessageItemBody', () => {
-  it('should do a snapshot test of the TextMessageItemBody DOM', function() {
-    const text = "example-text";
+  it('should have own className', () => {
+    const insertingClassName = 'test-class-name';
+    const component = mount(
+      <TextMessageItemBody
+        className={insertingClassName}
+        message={createMockMessage()}
+      />
+    );
+    expect(
+      component.find(`.${insertingClassName}`).hostNodes().exists()
+    ).toBe(true);
+    expect(
+      component.find('.sendbird-text-message-item-body').hostNodes().exists()
+    ).toBe(true);
+    expect(
+      component.find('.sendbird-text-message-item-body.outgoing').hostNodes().exists()
+    ).toBe(false);
+    expect(
+      component.find('.sendbird-text-message-item-body.incoming').hostNodes().exists()
+    ).toBe(true);
+    expect(
+      component.find('.sendbird-text-message-item-body.mouse-hover').hostNodes().exists()
+    ).toBe(false);
+    expect(
+      component.find('.sendbird-text-message-item-body.reactions').hostNodes().exists()
+    ).toBe(false);
+    expect(
+      component.find('.sendbird-text-message-item-body__message').hostNodes().exists()
+    ).toBe(false);
+    expect(
+      component.find('.sendbird-text-message-item-body__message.edited').hostNodes().exists()
+    ).toBe(false);
+  });
+
+  it('should have class name by isByMe prop', () => {
+    const outgoingMessage = mount(
+      <TextMessageItemBody
+        message={createMockMessage()}
+        isByMe
+      />
+    );
+    expect(
+      outgoingMessage.find('.sendbird-text-message-item-body.outgoing').hostNodes().exists()
+    ).toBe(true);
+    expect(
+      outgoingMessage.find('.sendbird-text-message-item-body.incoming').hostNodes().exists()
+    ).toBe(false);
+    const incomingMessage = mount(
+      <TextMessageItemBody
+        message={createMockMessage()}
+        isByMe={false}
+      />
+    );
+    expect(
+      incomingMessage.find('.sendbird-text-message-item-body.outgoing').hostNodes().exists()
+    ).toBe(false);
+    expect(
+      incomingMessage.find('.sendbird-text-message-item-body.incoming').hostNodes().exists()
+    ).toBe(true);
+  });
+
+  it('should have class name by mouse hover prop', () => {
+    const component = mount(
+      <TextMessageItemBody
+        message={createMockMessage()}
+        mouseHover
+      />
+    );
+    expect(
+      component.find('.sendbird-text-message-item-body.mouse-hover').hostNodes().exists()
+    ).toBe(true);
+    expect(
+      component.find('.sendbird-text-message-item-body.incoming.mouse-hover').hostNodes().exists()
+    ).toBe(true);
+  });
+
+  it('should have class name by reactions of message prop', () => {
+    const component = mount(
+      <TextMessageItemBody
+        message={createMockMessage((mockMessage) => ({
+          ...mockMessage,
+          reactions: [{}, {}, {}],
+        }))}
+      />
+    );
+    expect(
+      component.find('.sendbird-text-message-item-body.reactions').hostNodes().exists()
+    ).toBe(true);
+  });
+
+  it('should have words component by split message', () => {
+    const messageText = 'First second third fourth fifth';
+    const component = mount(
+      <TextMessageItemBody
+        message={createMockMessage((mockMessage) => ({
+          ...mockMessage,
+          message: messageText,
+        }))}
+      />
+    );
+    expect(
+      component.find('.sendbird-text-message-item-body__message').hostNodes().length
+    ).toBe(messageText.split(/\r/).length);
+    expect(
+      component.find('.sendbird-text-message-item-body__message.edited').hostNodes().exists()
+    ).toBe(false);
+    const editedMessage = mount(
+      <TextMessageItemBody
+        message={createMockMessage((mockMessage) => ({
+          ...mockMessage,
+          message: messageText,
+          updatedAt: 10101010,
+        }))}
+      />
+    );
+    expect(
+      editedMessage.find('.sendbird-text-message-item-body__message').hostNodes().length
+    ).toBe(messageText.split(/\r/).length + 1);
+    expect(
+      editedMessage.find('.sendbird-text-message-item-body__message.edited').hostNodes().exists()
+    ).toBe(true);
+  });
+
+  it('should do a snapshot test of the TextMessageItemBody DOM', function () {
     const component = renderer.create(
-      <TextMessageItemBody />,
+      <TextMessageItemBody
+        className="class-name-for-snapshot"
+        message={createMockMessage((mock) => ({
+          ...mock,
+          message: 'First second third fourth fifth',
+          updatedAt: 1010,
+          reactions: [{}, {}, {}],
+        }))}
+        isByMe
+        mouseHover
+      />,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
