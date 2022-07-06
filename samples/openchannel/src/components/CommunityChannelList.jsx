31e3c46d4e77a14eaedaeb623d27c1abd6b81b3d
HoonBaek <hoon@sendbird.com>
2022-07-07 07:50:11 +0900

    Remove channel on the open channel list when it's banned or deleted

--- samples/openchannel/src/components/CommunityChannelList.jsx
+++ samples/openchannel/src/components/CommunityChannelList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import withSendBird from "../../../lib/SendbirdSdkContext";
 import * as sendbirdSelectors from "../../../lib/selectors";
+import uuidv4 from '../../../../src/utils/uuid';
 
 import "./community-channel-list.scss";
 import OpenChannelPreview from "./OpenChannelPreview";
@@ -18,6 +19,7 @@ function CommunityChannelList({
       return;
     }
     const openChannelListQuery = sdk.OpenChannel.createOpenChannelListQuery();
+    const channelHandlerId = uuidv4();
     // @ts-ignore: Unreachable code error
     openChannelListQuery.customTypes = ["SB_COMMUNITY_TYPE"];
     openChannelListQuery.next(function (openChannels, error) {
@@ -29,6 +31,22 @@ function CommunityChannelList({
         setCurrentChannel(openChannels[0]);
       }
     });
+    const channelHandlerParams = {
+      onUserBanned: (channel, bannedUser) => {
+        if (channel?.url === currentChannelUrl && user?.userId === bannedUser?.userId) {
+          setChannels(channels.filter((ch) => ch?.url !== channel.url));
+        }
+      },
+      onChannelDeleted: (channelUrl, channelType) => {
+        if (channelType === 'open') {
+          setChannels(channels.filter((channel) => channel?.url !== channelUrl));
+        }
+      }
+    };
+    sdk.OpenChannel.addOpenChannelHandler(channelHandlerId, channelHandlerParams);
+    return () => {
+      sdk.OpenChannel.removeOpenChannelHandler(channelHandlerId);
+    }
   }, [sdk]);
 
   return (
