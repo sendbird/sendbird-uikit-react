f8a8ba6556b65deddcfa1155335d1bf8c7dc9693
Sravan S <sravan-s@protonmail.com>
2022-07-18 15:32:05 +0900

    feat: add Basic TS sample

---
+++ samples/typescript_sample/src/const.ts
@@ -0,0 +1,15 @@
+// put your own APP_ID here
+// get your app_id -> https://dashboard.sendbird.com/auth/signin
+export const APP_ID = "2D7B4CDB-932F-4082-9B09-A1153792DC8D";
+// export const APP_ID = "";
+
+// set your own USER_ID and NICKNAME
+export const USER_ID = "sendbird";
+export const NICKNAME = "sendbird";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  APP_ID,
+  USER_ID,
+  NICKNAME
+};
