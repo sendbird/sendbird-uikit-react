5c5392e72aa5d4de14067c9a60341f08c4905795
HoonBaek <hoon@sendbird.com>
2022-07-26 19:26:00 +0900

    Add optional chaining

--- src/ui/Word/index.tsx
+++ src/ui/Word/index.tsx
@@ -47,7 +47,7 @@ export default function Word(props: WordProps): JSX.Element {
             );
           } else if (type === StringObjType.url) {
             const urlRegex = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?");
-            const targetUrl = urlRegex.exec(value)[0];
+            const targetUrl = urlRegex.exec(value)?.[0];
             const stringUrl = { front: '', url: '', back: '' };
             if (targetUrl) {
               const targetUrlIndex = value.indexOf(targetUrl);
