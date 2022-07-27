1d286a3c49d8f9152aad4b55e919f09f501959ad
HoonBaek <hoon@sendbird.com>
2022-07-27 17:21:08 +0900

    fix: Improve URL detection in OG messages(#221)

--- src/ui/Word/index.tsx
+++ src/ui/Word/index.tsx
@@ -46,6 +46,36 @@ export default function Word(props: WordProps): JSX.Element {
               />
             );
           } else if (type === StringObjType.url) {
+            const urlRegex = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?");
+            const targetUrl = urlRegex.exec(value)?.[0];
+            const stringUrl = { front: '', url: '', back: '' };
+            if (targetUrl) {
+              const targetUrlIndex = value.indexOf(targetUrl);
+              if (targetUrlIndex > 0) {
+                stringUrl.front = value.slice(0, targetUrlIndex);
+              }
+              stringUrl.url = value.slice(targetUrlIndex, targetUrlIndex + targetUrl.length);
+              if (targetUrlIndex + targetUrl.length < value.length) {
+                stringUrl.back = value.slice(targetUrlIndex + targetUrl.length);
+              }
+            }
+            if (targetUrl) {
+              return [
+                stringUrl.front ? stringUrl.front : '',
+                stringUrl.url ? (
+                  <LinkLabel
+                    className="sendbird-word__url"
+                    key={uuidv4()}
+                    src={stringUrl.url}
+                    type={LabelTypography.BODY_1}
+                    color={isByMe ? LabelColors.ONCONTENT_1 : LabelColors.ONBACKGROUND_1}
+                  >
+                    {stringUrl.url}
+                  </LinkLabel>
+                ) : null,
+                stringUrl.back ? stringUrl.back : '',
+              ];
+            }
             return (
               <LinkLabel
                 className="sendbird-word__url"
