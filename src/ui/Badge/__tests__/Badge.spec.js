1be1288cdd45204af87ea72c1ad41d6830ac60b1
HoonBaek <hoon@sendbird.com>
2022-09-05 11:59:15 +0900

    Migrate tests of Badge and ReactionBadge

--- src/ui/Badge/__tests__/Badge.spec.js
+++ src/ui/Badge/__tests__/Badge.spec.js
@@ -1,51 +1,35 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 
 import Badge from "../index";
 
 describe('Badge', () => {
   it('should do a snapshot test of the Badge DOM', function () {
     const count = 1;
-    const component = renderer.create(
-      <Badge count={count} />,
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const { asFragment } = render(<Badge count={count} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   it('should display maximum number as a maxLevel: 1', function () {
     const count = 10;
     const level = 1;
 
-    const component = mount(
-      <Badge count={count} maxLevel={level} />
-    );
-    expect(
-      component.find('.sendbird-label').text()
-    ).toEqual('9+');
+    render(<Badge count={count} maxLevel={level} />);
+    expect(screen.getByText('9+')).toBeTruthy();
   });
 
   it('should display maximum number as a maxLevel: 2 (default level)', function () {
     const count = 100;
 
-    const component = mount(
-      <Badge count={count} />
-    );
-    expect(
-      component.find('.sendbird-label').text()
-    ).toEqual('99+');
+    render(<Badge count={count} />);
+    expect(screen.getByText('99+')).toBeTruthy();
   });
 
   it('should display maximum number as a maxLevel: 3', function () {
     const count = 1000;
     const level = 3;
 
-    const component = mount(
-      <Badge count={count} maxLevel={level} />
-    );
-    expect(
-      component.find('.sendbird-label').text()
-    ).toEqual('999+');
+    render(<Badge count={count} maxLevel={level} />);
+    expect(screen.getByText('999+')).toBeTruthy();
   });
 });
--- src/ui/ReactionBadge/__tests__/ReactionBadge.spec.js
+++ src/ui/ReactionBadge/__tests__/ReactionBadge.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 
 import ReactionBadge from "../index";
 import Icon, { IconTypes } from '../../Icon';
@@ -8,27 +7,23 @@ import Icon, { IconTypes } from '../../Icon';
 describe('ReactionBadge', () => {
   it('should have className', function () {
     const text = "example-text";
-    const component = shallow(
+    render(
       <ReactionBadge className={text}>
         <Icon type={IconTypes.CREATE} width="20px" height="20px" />
       </ReactionBadge>
     );
-
-    expect(
-      component.find(".sendbird-reaction-badge").hasClass(text)
-    ).toBe(true);
+    expect(screen.getAllByRole('button')[0].className).toContain(text);
   });
 
   it('should do a snapshot test of the ReactionBadge DOM', function () {
     const text = "example-text";
     const inputCount = 1000;
 
-    const component = renderer.create(
+    const { asFragment } = render(
       <ReactionBadge count={inputCount > 99 ? '+99' : inputCount} className={text}>
         <Icon type={IconTypes.CREATE} width="20px" height="20px" />
       </ReactionBadge>
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
