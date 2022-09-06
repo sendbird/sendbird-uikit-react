4b73e6a81ec2fe58c8d42c640121e8d21bf2b2d0
HoonBaek <hoon@sendbird.com>
2022-09-06 12:59:46 +0900

    Migrate tests of ui/OGMessageItemBody with testing-library

--- src/ui/OGMessageItemBody/__tests__/OGMessageItemBody.spec.js
+++ src/ui/OGMessageItemBody/__tests__/OGMessageItemBody.spec.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 
 import OGMessageItemBody from "../index";
 
 describe('OGMessageItemBody', () => {
   it('should do a snapshot test of the OGMessageItemBody DOM', function() {
     const text = "example-text";
-    const component = renderer.create(
-      <OGMessageItemBody />,
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const { asFragment } = render(<OGMessageItemBody />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
