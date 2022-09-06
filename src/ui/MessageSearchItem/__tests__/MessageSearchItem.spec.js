79c44d5790793b98667badd0e4db367a96bc98ed
HoonBaek <hoon@sendbird.com>
2022-09-06 12:55:07 +0900

    Migrate tests of ui/MessageSearchItem with testing-library

--- src/ui/MessageSearchItem/__tests__/MessageSearchItem.spec.js
+++ src/ui/MessageSearchItem/__tests__/MessageSearchItem.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 
 import MessageSearchItem from "../index";
 import { generateNormalMessage } from '../messageDummyDate.mock';
@@ -8,42 +7,40 @@ import { generateNormalMessage } from '../messageDummyDate.mock';
 jest.mock('date-fns/format', () => () => ('mock-date'));
 
 
-describe('MessageSearchItem', () => {
+describe('ui/MessageSearchItem', () => {
   it('should render necessary elements', function () {
     const className = 'class-name';
-    const component = mount(
+    const { container } = render(
       <MessageSearchItem
         className={className}
         message={generateNormalMessage()}
-      />,
+      />
     );
-
     expect(
-      component.find(`.${className}`).hostNodes().exists()
-    ).toBe(true);
+      container.getElementsByClassName(className).length
+    ).toBe(1);
     expect(
-      component.find('.sendbird-message-search-item__left__sender-avatar').hostNodes().exists()
-    ).toBe(true);
+      container.getElementsByClassName('sendbird-message-search-item__left__sender-avatar').length
+    ).toBe(1);
     expect(
-      component.find('.sendbird-message-search-item__right__sender-name').hostNodes().exists()
-    ).toBe(true);
+      container.getElementsByClassName('sendbird-message-search-item__right__sender-name').length
+    ).toBe(1);
     expect(
-      component.find('.sendbird-message-search-item__right__message-text').hostNodes().exists()
-    ).toBe(true);
+      container.getElementsByClassName('sendbird-message-search-item__right__message-text').length
+    ).toBe(1);
     expect(
-      component.find('.sendbird-message-search-item__right__message-created-at').hostNodes().exists()
-    ).toBe(true);
+      container.getElementsByClassName('sendbird-message-search-item__right__message-created-at').length
+    ).toBe(1);
   });
 
   it('should do a snapshot test of the MessageSearchItem DOM', function () {
     const text = "example-text";
-    const component = renderer.create(
+    const { asFragment } = render(
       <MessageSearchItem
         className={text}
         message={generateNormalMessage()}
-      />,
+      />
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
