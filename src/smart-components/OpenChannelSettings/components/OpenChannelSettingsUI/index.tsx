d153325301d1037c5b1bf6eb97c509c2cb2437c4
HoonBaek <hoon@sendbird.com>
2022-09-17 20:22:49 +0900

    Add isChannelInitialized state into the OpenChannelSettings

--- src/smart-components/OpenChannelSettings/components/OpenChannelSettingsUI/index.tsx
+++ src/smart-components/OpenChannelSettings/components/OpenChannelSettingsUI/index.tsx
@@ -25,13 +25,14 @@ const OpenChannelUI: React.FC<OpenChannelUIProps> = ({
   const {
     channel,
     onCloseClick,
+    isChannelInitialized,
   } = useOpenChannelSettingsContext();
   const globalStore = useSendbirdStateContext();
   const logger = globalStore?.config?.logger;
   const user = globalStore?.stores?.userStore?.user;
 
   const { stringSet } = useContext(LocalizationContext);
-  if (!channel) {
+  if (isChannelInitialized && !channel) {
     return (
       <InvalidChannel
         onCloseClick={() => {
