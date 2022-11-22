f9c96766cc1e745d43c4c11ddad2505ebbc6c60d
HoonBaek <hoon@sendbird.com>
2022-11-22 09:02:19 +0900

    Improve the ParentMessageInfo component * Edit message * ContextMenu * UserProfile * FileViewer

--- src/smart-components/Thread/components/ParentMessageInfo/index.tsx
+++ src/smart-components/Thread/components/ParentMessageInfo/index.tsx
@@ -1,88 +1,233 @@
-import React, { useMemo, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import format from 'date-fns/format';
-import { GroupChannel } from '@sendbird/chat/groupChannel';
 import { FileMessage, UserMessage } from '@sendbird/chat/message';
+import { Role } from '@sendbird/chat';
 
 import './index.scss';
+import RemoveMessage from '../RemoveMessageModal';
+import ParentMessageInfoItem from './ParentMessageInfoItem';
+
+import {
+  getSenderName,
+} from '../../../../utils';
+import { useLocalization } from '../../../../lib/LocalizationContext';
+import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
+import { useThreadContext } from '../../context/ThreadProvider';
+import { UserProfileContext } from '../../../../lib/UserProfileContext';
+import SuggestedMentionList from '../../../Channel/components/SuggestedMentionList';
 
 import Avatar from '../../../../ui/Avatar';
 import Label, { LabelTypography, LabelColors } from '../../../../ui/Label';
-import {
-  getSenderName,
-  getUIKitMessageType,
-  getUIKitMessageTypes,
-  isOGMessage,
-  isTextMessage,
-  isThumbnailMessage,
-} from '../../../../utils';
-import { useLocalization } from '../../../../lib/LocalizationContext';
-import TextMessageItemBody from '../../../../ui/TextMessageItemBody';
-import OGMessageItemBody from '../../../../ui/OGMessageItemBody';
-import FileMessageItemBody from '../../../../ui/FileMessageItemBody';
-import ThumbnailMessageItemBody from '../../../../ui/ThumbnailMessageItemBody';
-import UnknownMessageItemBody from '../../../../ui/UnknownMessageItemBody';
-import EmojiReactions from '../../../../ui/EmojiReactions';
-import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
-import { useThreadContext } from '../../context/ThreadProvider';
+import FileViewer from '../../../../ui/FileViewer';
 import MessageItemMenu from '../../../../ui/MessageItemMenu';
-import RemoveMessage from '../RemoveMessageModal';
 import MessageItemReactionMenu from '../../../../ui/MessageItemReactionMenu';
+import ContextMenu, { MenuItems } from '../../../../ui/ContextMenu';
+import ConnectedUserProfile from '../../../../ui/UserProfile';
+import { UserProfileContextInterface } from '../../../../ui/MessageContent';
+import MessageInput from '../../../../ui/MessageInput';
+import { MessageInputKeys } from '../../../../ui/MessageInput/const';
 
 interface ParentMessageInfoProps {
   className?: string;
-  channel: GroupChannel;
-  message: UserMessage | FileMessage;
 }
 
 export default function ParentMessageInfo({
   className,
-  channel,
-  message,
 }: ParentMessageInfoProps): React.ReactElement {
   const { stores, config } = useSendbirdStateContext();
   const {
     isMentionEnabled,
     isReactionEnabled,
     replyType,
+    isOnline,
+    userMention,
   } = config;
   const userId = stores?.userStore?.user?.userId;
   const { dateLocale } = useLocalization?.();
-  const messageTypes = getUIKitMessageTypes?.();
   const {
     emojiContainer,
-    nicknamesMap,
     toggleReaction,
+    parentMessage,
     currentChannel,
+    updateMessage,
+    deleteMessage,
+    onMoveToParentMessage,
+    onHeaderActionClick,
+    isMuted,
+    isChannelFrozen,
   } = useThreadContext();
 
   const [showRemove, setShowRemove] = useState(false);
   const [supposedHover, setSupposedHover] = useState(false);
+  const [showFileViewer, setShowFileViewer] = useState(false);
   const usingReaction = isReactionEnabled && !currentChannel?.isSuper && !currentChannel?.isBroadcast
 
-  const MemorizedEmojiReactions = useMemo(() => {
-    return replyType === 'THREAD' && message?.reactions?.length > 0 && (
-      <div className="sendbird-parent-message-info__content__reactions">
-        <EmojiReactions
-          userId={userId}
-          message={message}
-          isByMe={false}
-          emojiContainer={emojiContainer}
-          memberNicknamesMap={nicknamesMap}
-          toggleReaction={toggleReaction}
+  // Edit message
+  const [showEditInput, setShowEditInput] = useState(false);
+  const disabled = !isOnline || isMuted || isChannelFrozen && !(currentChannel?.myRole === Role.OPERATOR);
+
+  // Mention
+  const editMessageInputRef = useRef(null);
+  const [mentionNickname, setMentionNickname] = useState('');
+  const [mentionedUsers, setMentionedUsers] = useState([]);
+  const [mentionedUserIds, setMentionedUserIds] = useState([]);
+  const [messageInputEvent, setMessageInputEvent] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [mentionSuggestedUsers, setMentionSuggestedUsers] = useState([]);
+  const [ableMention, setAbleMention] = useState(true);
+  const displaySuggestedMentionList = isOnline
+    && isMentionEnabled
+    && mentionNickname.length > 0
+    && !isMuted
+    && !(isChannelFrozen && !(currentChannel.myRole === Role.OPERATOR));
+  useEffect(() => {
+    if (mentionedUsers?.length >= userMention?.maxMentionCount) {
+      setAbleMention(false);
+    } else {
+      setAbleMention(true);
+    }
+  }, [mentionedUsers]);
+  useEffect(() => {
+    setMentionedUsers(mentionedUsers.filter(({ userId }) => {
+      const i = mentionedUserIds.indexOf(userId);
+      if (i < 0) {
+        return false;
+      } else {
+        mentionedUserIds.splice(i, 1);
+        return true;
+      }
+    }));
+  }, [mentionedUserIds]);
+
+  // User Profile
+  const avatarRef = useRef(null);
+  const {
+    disableUserProfile,
+    renderUserProfile,
+  } = useContext<UserProfileContextInterface>(UserProfileContext);
+
+  if (showEditInput && parentMessage?.isUserMessage?.()) {
+    return (
+      <>
+        {
+          displaySuggestedMentionList && (
+            <SuggestedMentionList
+              targetNickname={mentionNickname}
+              inputEvent={messageInputEvent}
+              // renderUserMentionItem={renderUserMentionItem}
+              onUserItemClick={(user) => {
+                if (user) {
+                  setMentionedUsers([...mentionedUsers, user]);
+                }
+                setMentionNickname('');
+                setSelectedUser(user);
+                setMessageInputEvent(null);
+              }}
+              onFocusItemChange={() => {
+                setMessageInputEvent(null);
+              }}
+              onFetchUsers={(users) => {
+                setMentionSuggestedUsers(users);
+              }}
+              ableAddMention={ableMention}
+              maxMentionCount={userMention?.maxMentionCount}
+              maxSuggestionCount={userMention?.maxSuggestionCount}
+            />
+          )
+        }
+        <MessageInput
+          isEdit
+          disabled={disabled}
+          ref={editMessageInputRef}
+          mentionSelectedUser={selectedUser}
+          isMentionEnabled={isMentionEnabled}
+          message={parentMessage}
+          onStartTyping={() => {
+            currentChannel?.startTyping?.();
+          }}
+          onUpdateMessage={({ messageId, message, mentionTemplate }) => {
+            updateMessage({
+              messageId,
+              message,
+              mentionedUsers,
+              mentionTemplate,
+            });
+            setShowEditInput(false);
+            currentChannel?.endTyping?.();
+          }}
+          onCancelEdit={() => {
+            setMentionNickname('');
+            setMentionedUsers([]);
+            setMentionedUserIds([]);
+            setMentionSuggestedUsers([])
+            setShowEditInput(false);
+            currentChannel?.endTyping?.();
+          }}
+          onUserMentioned={(user) => {
+            if (selectedUser?.userId === user?.userId) {
+              setSelectedUser(null);
+              setMentionNickname('');
+            }
+          }}
+          onMentionStringChange={(mentionText) => {
+            setMentionNickname(mentionText);
+          }}
+          onMentionedUserIdsUpdated={(userIds) => {
+            setMentionedUserIds(userIds);
+          }}
+          onKeyDown={(e) => {
+            if (displaySuggestedMentionList && mentionSuggestedUsers?.length > 0
+              && ((e.key === MessageInputKeys.Enter && ableMention) || e.key === MessageInputKeys.ArrowUp || e.key === MessageInputKeys.ArrowDown)
+            ) {
+              setMessageInputEvent(e);
+              return true;
+            }
+            return false;
+          }}
         />
-      </div>
-    )
-  }, [message?.reactions, replyType, emojiContainer, nicknamesMap, toggleReaction, userId]);
+      </>
+    );
+  }
 
   return (
     <div className={`sendbird-parent-message-info ${className}`}>
-      {/* apply user profile */}
-      <Avatar
-        className="sendbird-parent-message-info__sender"
-        src={message?.sender?.profileUrl}
-        alt="thread message sender"
-        width="40px"
-        height="40px"
+      <ContextMenu
+        menuTrigger={(toggleDropdown) => (
+          <Avatar
+            className="sendbird-parent-message-info__sender"
+            ref={avatarRef}
+            src={
+              currentChannel?.members?.find((m) => m?.userId === parentMessage?.sender?.userId)?.profileUrl
+              || parentMessage?.sender?.profileUrl
+            }
+            alt="thread message sender"
+            width="40px"
+            height="40px"
+            onClick={() => {
+              if (!disableUserProfile) {
+                toggleDropdown();
+              }
+            }}
+          />
+        )}
+        menuItems={(closeDropdown) => (
+          <MenuItems
+            parentRef={avatarRef}
+            parentContainRef={avatarRef}
+            closeDropdown={closeDropdown}
+            style={{ paddingTop: '0px', paddingBottom: '0px' }}
+          >
+            {renderUserProfile
+              ? renderUserProfile({ user: parentMessage?.sender, close: closeDropdown })
+              : (
+                <ConnectedUserProfile
+                  user={parentMessage?.sender}
+                  currentUserId={userId}
+                  onSuccess={closeDropdown}
+                />
+              )}
+          </MenuItems>
+        )}
       />
       <div className="sendbird-parent-message-info__content">
         <div className="sendbird-parent-message-info__content__info">
@@ -92,8 +237,8 @@ export default function ParentMessageInfo({
             color={LabelColors.ONBACKGROUND_2}
           >
             {
-              channel?.members?.find((member) => member?.userId === message?.sender?.userId)
-                ?.nickname || getSenderName?.(message as UserMessage | FileMessage)
+              currentChannel?.members?.find((member) => member?.userId === parentMessage?.sender?.userId)
+                ?.nickname || getSenderName?.(parentMessage as UserMessage | FileMessage)
             }
           </Label>
           <Label
@@ -101,93 +246,60 @@ export default function ParentMessageInfo({
             type={LabelTypography.CAPTION_3}
             color={LabelColors.ONBACKGROUND_2}
           >
-            {format(message?.createdAt || 0, 'p', { locale: dateLocale })}
+            {format(parentMessage?.createdAt || 0, 'p', { locale: dateLocale })}
           </Label>
         </div>
         {/* message content body */}
-        {isTextMessage(message as UserMessage) && (
-          <TextMessageItemBody
-            className="sendbird-parent-message-info__content__body"
-            message={message as UserMessage}
-            isByMe={false}
-            // mouseHover -> for style
-            isMentionEnabled={isMentionEnabled}
-            isReactionEnabled={usingReaction}
-          />
-        )}
-        {isOGMessage(message as UserMessage) && (
-          <OGMessageItemBody
-            className="sendbird-parent-message-info__content__body"
-            message={message as UserMessage}
-            isByMe={false}
-            // mouseHover -> for style
-            isMentionEnabled={isMentionEnabled}
-            isReactionEnabled={usingReaction}
-          />
-        )}
-        {getUIKitMessageType(message as FileMessage) === messageTypes?.FILE && (
-          <FileMessageItemBody
-            className="sendbird-parent-message-info__content__body"
-            message={message as FileMessage}
-            isByMe={false}
-            // mouseHover -> for style
-            isReactionEnabled={usingReaction}
-          />
-        )}
-        {isThumbnailMessage(message as FileMessage) && (
-          <ThumbnailMessageItemBody
-            className="sendbird-parent-message-info__content__body"
-            message={message as FileMessage}
-            isByMe={false}
-            // showFileViewer
-            // mouseHover -> for style
-            isReactionEnabled={usingReaction}
-            style={{
-              width: '200px',
-              height: '148px',
-            }}
-          />
-        )}
-        {getUIKitMessageType(message) === messageTypes?.UNKNOWN && (
-          <UnknownMessageItemBody
-            className="sendbird-parent-message-info__content__body"
-            message={message}
-            isByMe={false}
-            // mouseHover -> for style
-            isReactionEnabled={usingReaction}
-          />
-        )}
-        {/* reactions */}
-        {MemorizedEmojiReactions}
+        <ParentMessageInfoItem
+          message={parentMessage}
+          showFileViewer={setShowFileViewer}
+        />
       </div>
       {/* context menu */}
       <MessageItemMenu
-        className={`sendbird-parent-message-info__context-menu ${usingReaction ? 'use-reaction' : ''} ${supposedHover ? 'supposed-hover' : ''}`}
-        channel={channel}
-        message={message}
-        isByMe={userId === message?.sender?.userId}
+        className={`sendbird-parent-message-info__context-menu ${usingReaction ? 'use-reaction' : ''} ${supposedHover ? 'sendbird-mouse-hover' : ''}`}
+        channel={currentChannel}
+        message={parentMessage}
+        isByMe={userId === parentMessage?.sender?.userId}
         replyType={replyType}
+        showEdit={setShowEditInput}
         showRemove={setShowRemove}
         setSupposedHover={setSupposedHover}
+        onMoveToParentMessage={() => {
+          onMoveToParentMessage({ message: parentMessage, channel: currentChannel });
+        }}
       />
       {usingReaction && (
         <MessageItemReactionMenu
-          className={`sendbird-parent-message-info__reaction-menu ${usingReaction ? 'supposed-hover' : ''}`}
-          message={message}
+          className={`sendbird-parent-message-info__reaction-menu ${supposedHover ? 'sendbird-mouse-hover' : ''}`}
+          message={parentMessage}
           userId={userId}
           emojiContainer={emojiContainer}
           toggleReaction={toggleReaction}
           setSupposedHover={setSupposedHover}
         />
       )}
-      {
-        showRemove && (
-          <RemoveMessage
-            onCancel={() => setShowRemove(false)}
-            message={message}
-          />
-        )
-      }
+      {showRemove && (
+        <RemoveMessage
+          onCancel={() => setShowRemove(false)}
+          onSubmit={() => {
+            onHeaderActionClick?.();
+          }}
+          message={parentMessage}
+        />
+      )}
+      {showFileViewer && (
+        <FileViewer
+          message={parentMessage as FileMessage}
+          onClose={() => setShowFileViewer(false)}
+          onDelete={() => {
+            deleteMessage(parentMessage)
+              .then(() => {
+                setShowFileViewer(false);
+              });
+          }}
+        />
+      )}
     </div>
   );
 }
