f5cfde295741130db941f4aec6de30ffb134fd77
Sravan S <sravan-s@protonmail.com>
2023-01-30 21:12:33 +0900

    feat: implement unread count

--- src/smart-components/NotificationChannel/stories/AppLayout.tsx
+++ src/smart-components/NotificationChannel/stories/AppLayout.tsx
@@ -9,17 +9,21 @@ import AddChannel from '../../ChannelList/components/AddChannel';
 import Label, { LabelTypography, LabelColors } from '../../../ui/Label';
 import Icon, { IconTypes, IconColors } from '../../../ui/Icon';
 import IconButton from '../../../ui/IconButton';
-import ContextMenu, { MenuItem, MenuItems } from '../../../ui/ContextMenu';
+import ContextMenu, { MenuItems } from '../../../ui/ContextMenu';
 import NotificationChannel from '../';
 import useSendbirdStateContext from '../../../hooks/useSendbirdStateContext';
-import { GroupChannelCreateParams, SendbirdGroupChat } from '@sendbird/chat/groupChannel';
+import { GroupChannel, GroupChannelCreateParams, GroupChannelHandler } from '@sendbird/chat/groupChannel';
+import uuidv4 from '../../../utils/uuid';
+import Badge from '../../../ui/Badge';
 
 const getNotificationChannelUrl = (userId: string) => {
   return `SENDBIRD_NOTIFICATION_CHANNEL_NOTIFICATION_${userId}`;
 };
 
 export const CustomTitle = () => {
-  const [ notificationChannel, setNotificationChannel ] = useState(null);
+  const [ notificationChannel, setNotificationChannel ] = useState<GroupChannel>(null);
+  const [ isNotficationsOpen, setIsNotficationsOpen ] = useState(false);
+  const [ unreadCount, setUnreadCount] = useState(0);
   const store = useSendbirdStateContext();
   const userId = store?.config?.userId;
   const sdk = store?.stores?.sdkStore?.sdk;
@@ -29,6 +33,7 @@ export const CustomTitle = () => {
       const notificaionChannelUrl = getNotificationChannelUrl(userId);
       sdk?.groupChannel?.getChannel(notificaionChannelUrl).then((channel) => {
         setNotificationChannel(channel);
+        setUnreadCount(channel?.unreadMessageCount);
       }).catch(() => {
         const params: GroupChannelCreateParams = {
           channelUrl: notificaionChannelUrl,
@@ -39,10 +44,29 @@ export const CustomTitle = () => {
         };
         sdk?.groupChannel?.createChannel(params).then((channel) => {
           setNotificationChannel(channel);
+          setUnreadCount(channel?.unreadMessageCount);
         });
       });
     }
   }, [userId, sdk]);
+
+  useEffect(() => {
+    if (sdk?.groupChannel?.addGroupChannelHandler && notificationChannel?.url) {
+      const handlerId = uuidv4();
+      const channelHandler: GroupChannelHandler = {
+        onMessageReceived: (channel) => {
+          if (channel?.url === notificationChannel?.url && !isNotficationsOpen) {
+            const _channel = channel as GroupChannel;
+            setUnreadCount(_channel?.unreadMessageCount);
+          }
+        },
+      };
+      sdk?.groupChannel?.addGroupChannelHandler(handlerId, new GroupChannelHandler(channelHandler));
+      return () => {
+        sdk?.groupChannel?.removeGroupChannelHandler(handlerId);
+      };
+    }
+  }, [notificationChannel, sdk]);
 
   const parentRef = React.useRef(null);
 
@@ -61,20 +85,35 @@ export const CustomTitle = () => {
               width="32px"
               height="32px"
               onClick={() => {
+                setIsNotficationsOpen(true);
                 toggleDropdown();
               }}
             >
-              <Icon
-                type={IconTypes.NOTIFICATIONS}
-                fillColor={IconColors.PRIMARY}
-              />
+              <>
+                <Icon
+                  type={IconTypes.NOTIFICATIONS}
+                  fillColor={IconColors.PRIMARY}
+                />
+                {
+                  (unreadCount > 0) && !isNotficationsOpen && (
+                    <Badge
+                      count={unreadCount}
+                      maxLevel={2}
+                      className='sendbird-app__channellist-alarm-badge'
+                    />
+                  )
+                }
+              </>
             </IconButton>
           )}
           menuItems={(closeDropdown) => (
             <MenuItems
               parentRef={parentRef}
               parentContainRef={parentRef}
-              closeDropdown={closeDropdown}
+              closeDropdown={() => {
+                setIsNotficationsOpen(false);
+                closeDropdown();
+              }}
               className='sendbird-app__channellist-alarm-dropdown'
             >
               <NotificationChannel
