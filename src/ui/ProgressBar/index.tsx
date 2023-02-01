1742d723d1cc8b4090b1782c99b06e178f5e19a0
HoonBaek <hoon@sendbird.com>
2023-02-02 08:37:38 +0900

    Modify ProgressBar and update snapshot

--- src/ui/ProgressBar/index.tsx
+++ src/ui/ProgressBar/index.tsx
@@ -2,20 +2,22 @@ import React, { useMemo } from 'react';
 import './index.scss';
 
 export interface ProgressBarProps {
+  className?: string;
   maxSize: number;
   currentSize: number;
 }
 
 export const ProgressBar = ({
+  className ='',
   maxSize,
   currentSize,
 }: ProgressBarProps): React.ReactElement => {
   const width = useMemo(() => {
-    console.log('use memo', currentSize / maxSize)
-    return `${currentSize / maxSize}%`;
+    console.log('progress bar comp', currentSize, maxSize)
+    return `${currentSize / maxSize * 100}%`;
   }, [currentSize, maxSize]);
   return (
-    <div className="sendbird-progress-bar">
+    <div className={`sendbird-progress-bar ${className}`}>
       <div className="sendbird-progress-bar__fill" style={{ width }} />
     </div>
   );
