5fd0d39d8c3e70ea9d13c428b1040ccb34d1da24
HoonBaek <hoon@sendbird.com>
2023-02-02 14:12:46 +0900

    Apply color type to the ProgressBar comp

--- src/ui/ProgressBar/index.tsx
+++ src/ui/ProgressBar/index.tsx
@@ -1,23 +1,30 @@
 import React, { useMemo } from 'react';
 import './index.scss';
 
+export enum ProgressBarColorTypes {
+  PRIMARY = 'progress-bar-color--primary',
+  GRAY = 'progress-bar-color--gray',
+}
+
 export interface ProgressBarProps {
   className?: string;
   maxSize: number;
   currentSize: number;
+  colorType?: ProgressBarColorTypes;
 }
 
 export const ProgressBar = ({
   className ='',
   maxSize,
   currentSize,
+  colorType = ProgressBarColorTypes.PRIMARY,
 }: ProgressBarProps): React.ReactElement => {
   const width = useMemo(() => {
-    console.log('progress bar comp', currentSize, maxSize)
+    console.log('progress bar comp', currentSize, maxSize, `${currentSize / maxSize * 100}%`)
     return `${currentSize / maxSize * 100}%`;
   }, [currentSize, maxSize]);
   return (
-    <div className={`sendbird-progress-bar ${className}`}>
+    <div className={`sendbird-progress-bar ${className} ${colorType}`}>
       <div className="sendbird-progress-bar__fill" style={{ width }} />
     </div>
   );
