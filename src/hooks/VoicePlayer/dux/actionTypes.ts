97b5c8adc08d9e2ce68b4d14a293a2189fc0ba35
EunSeo-Baek <bosk1914@gmail.com>
2023-03-02 11:36:52 +0900

    Clean up voice player action types

--- src/hooks/VoicePlayer/dux/actionTypes.ts
+++ src/hooks/VoicePlayer/dux/actionTypes.ts
@@ -1,11 +1,16 @@
-export type InitializeAudioUnit = 'INITIALIZE_AUDIO_UNIT';
-export type SetCurrentPlayer = 'SET_CURRENT_PLAYER';
-export type OnVoicePlayerPlay = 'ON_VOICE_PLAYER_PLAY';
-export type OnVoicePlayerPause = 'ON_VOICE_PLAYER_PAUSE';
-export type OnCurrentTimeUpdate = 'ON_CURRENT_TIME_UPDATE';
+export const actionTypes = {
+  INITIALIZE_AUDIO_UNIT: 'INITIALIZE_AUDIO_UNIT',
+  SET_CURRENT_PLAYER: 'SET_CURRENT_PLAYER',
+  ON_VOICE_PLAYER_PLAY: 'ON_VOICE_PLAYER_PLAY',
+  ON_VOICE_PLAYER_PAUSE: 'ON_VOICE_PLAYER_PAUSE',
+  ON_CURRENT_TIME_UPDATE: 'ON_CURRENT_TIME_UPDATE',
+} as const;
 
-export const INITIALIZE_AUDIO_UNIT: InitializeAudioUnit = 'INITIALIZE_AUDIO_UNIT';
-export const SET_CURRENT_PLAYER: SetCurrentPlayer = 'SET_CURRENT_PLAYER';
-export const ON_VOICE_PLAYER_PLAY: OnVoicePlayerPlay = 'ON_VOICE_PLAYER_PLAY';
-export const ON_VOICE_PLAYER_PAUSE: OnVoicePlayerPause = 'ON_VOICE_PLAYER_PAUSE';
-export const ON_CURRENT_TIME_UPDATE: OnCurrentTimeUpdate = 'ON_CURRENT_TIME_UPDATE';
+type ObjectValues<T> = T[keyof T];
+export type VoicePlayerActionType = ObjectValues<typeof actionTypes>;
+
+export const INITIALIZE_AUDIO_UNIT: VoicePlayerActionType = 'INITIALIZE_AUDIO_UNIT';
+export const SET_CURRENT_PLAYER: VoicePlayerActionType = 'SET_CURRENT_PLAYER';
+export const ON_VOICE_PLAYER_PLAY: VoicePlayerActionType = 'ON_VOICE_PLAYER_PLAY';
+export const ON_VOICE_PLAYER_PAUSE: VoicePlayerActionType = 'ON_VOICE_PLAYER_PAUSE';
+export const ON_CURRENT_TIME_UPDATE: VoicePlayerActionType = 'ON_CURRENT_TIME_UPDATE';
