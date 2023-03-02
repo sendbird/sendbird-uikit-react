318e231138cfecef8ed19e8fde9775344e062d53
Sravan S <sravan.sukumarakurup@sendbird.com>
2023-03-02 13:29:06 +0900

    fix: add classname for empty OG-image (#412)
    Add classname: `sendbird-og-message-item-body__og-thumbnail__empty` to
    identify faulty images in OG message
    
    Clients can use CSS to target this class~
    
    ```
    .sendbird-og-message-item-body__og-thumbnail__empty {
      display: none;
    }
    ```
    
    fixes: https://sendbird.atlassian.net/browse/UIKIT-3304

--- src/ui/OGMessageItemBody/__tests__/OGMessageItemBody.spec.js
+++ src/ui/OGMessageItemBody/__tests__/OGMessageItemBody.spec.js
@@ -6,7 +6,19 @@ import OGMessageItemBody from "../index";
 describe('ui/OGMessageItemBody', () => {
   it('should do a snapshot test of the OGMessageItemBody DOM', function() {
     const text = "example-text";
-    const { asFragment } = render(<OGMessageItemBody />);
+    const { asFragment } = render(<OGMessageItemBody message={{
+      ogMetaData: {
+        title: text,
+        description: text,
+        url: text,
+        image: text,
+      },
+    }} />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should add .sendbird-og-message-item-body__og-thumbnail__image-empty classname when image is empty', () => {
+    const { container } = render(<OGMessageItemBody />);
+    expect(container.getElementsByClassName('sendbird-og-message-item-body__og-thumbnail__empty').length).toBe(1);
   });
 });
