61e8961f04364540dc02ec65521a5b73d25445b2
HoonBaek <hoon@sendbird.com>
2023-03-15 10:35:06 +0900

    Migrate SendbirdSdkContext to TS

--- src/lib/SendbirdSdkContext.tsx
+++ src/lib/SendbirdSdkContext.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-export const SendbirdSdkContext = React.createContext();
+type ContextAwareComponentType = {
+  (props: any): JSX.Element;
+  displayName: string;
+}
 
-const withSendbirdContext = (OriginalComponent, mapStoreToProps) => {
+export const SendbirdSdkContext = React.createContext({});
+
+const withSendbirdContext = (OriginalComponent, mapStoreToProps): ContextAwareComponentType => {
   const ContextAwareComponent = (props) => (
     <SendbirdSdkContext.Consumer>
       {(context) => {
