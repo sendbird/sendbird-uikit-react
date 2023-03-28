ab5450e9721232532ce1ddb590f22380a0a13c1d
Sravan S <sravan-s@protonmail.com>
2023-03-28 14:54:27 +0900

    chore: remove unnecessary copying

--- rollup.config.js
+++ rollup.config.js
@@ -118,15 +118,6 @@ module.exports = ({
           src: './CHANGELOG.md',
           dest: 'dist',
         },
-        {
-          src: './.size-snapshot.json',
-          dest: 'dist',
-        },
-        {
-          src: './scripts/package.template.json',
-          dest: 'dist',
-          rename: 'package.json',
-        },
       ],
     }),
     visualizer({
