f4f7c3f6b15afd1d7afdc0ea0f65ec4fde9f9f17
Irene Ryu <irene.ryu@sendbird.com>
2023-04-11 12:43:15 +0900

    feat: remove redundant span tag from normal text

--- src/smart-components/Message/components/TextFragment/index.tsx
+++ src/smart-components/Message/components/TextFragment/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { UserMessage } from '@sendbird/chat/message';
+import { match } from 'ts-pattern';
 
-import { TOKEN_TYPES, Token } from '../../utils/tokens/types'
+import { TOKEN_TYPES, Token } from '../../utils/tokens/types';
 import { useMessageContext } from '../../context/MessageProvider';
 import { keyGenerator } from '../../utils/tokens/keyGenerator';
 import MentionLabel from '../../../../ui/MentionLabel';
 import { USER_MENTION_PREFIX } from '../../consts';
 import LinkLabel from '../../../../ui/LinkLabel';
-import { LabelTypography } from '../../../../ui//Label';
-import { match } from "ts-pattern";
+import { LabelTypography } from '../../../../ui/Label';
 
 export type TextFragmentProps = {
   tokens: Token[];
@@ -27,31 +27,40 @@ export default function TextFragment({
     <>
       {tokens?.map((token, idx) => {
         const key = keyGenerator(createdAt, updatedAt, idx);
-        return (
-          <span className="sendbird-word" key={key}>
-            {match(token.type)
-              .with(TOKEN_TYPES.mention, () => (
-                <MentionLabel
-                  mentionTemplate={USER_MENTION_PREFIX}
-                  mentionedUserId={token.userId}
-                  mentionedUserNickname={token.value}
-                  isByMe={isByMe}
-                />
-              ))
-              .with(TOKEN_TYPES.url, () => (
-                <LinkLabel
-                  className="sendbird-word__url"
-                  src={token.value}
-                  type={LabelTypography.BODY_1}
-                >
-                  {token.value}
-                </LinkLabel>
-              ))
-              .otherwise(() => (
-                <>{token.value}</>
-              ))}
-          </span>
-        );
+        return match(token.type)
+          .with(TOKEN_TYPES.mention, () => (
+            <span className="sendbird-word" key={key}>
+              <MentionLabel
+                mentionTemplate={USER_MENTION_PREFIX}
+                mentionedUserId={token.userId}
+                mentionedUserNickname={token.value}
+                isByMe={isByMe}
+              />
+            </span>
+          ))
+          .with(TOKEN_TYPES.url, () => (
+            <span className="sendbird-word" key={key}>
+              <LinkLabel
+                className="sendbird-word__url"
+                src={token.value}
+                type={LabelTypography.BODY_1}
+              >
+                {token.value}
+              </LinkLabel>
+            </span>
+          ))
+          .otherwise(() => {
+            /**
+             * To preserve the leading / trailing white spaces
+             * by not using additional span tag convert them like below
+             * '160' is the Unicode code point for the non-breaking space character (&nbsp; entity)
+             * @link https://sendbird.slack.com/archives/GPGHESTL3/p1681180484341369
+             */
+            const whiteSpacePreservedText = token.value.replace(/\s/g, () => String.fromCharCode(160));
+            return (
+              <>{whiteSpacePreservedText}</>
+            );
+          });
       })}
     </>
   );
