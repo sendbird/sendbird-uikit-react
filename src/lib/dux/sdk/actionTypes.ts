c046a44d7a95304dc847725515eac58c6b689b2b
Sravan S <sravan-s@protonmail.com>
2023-04-12 18:00:36 +0900

    feat: refactor SDK reducer

---
+++ src/lib/dux/sdk/actionTypes.ts
@@ -0,0 +1,18 @@
+import SendbirdChat from "@sendbird/chat";
+import { CreateAction } from "../../../utils/typeHelpers/reducers/CreateAction";
+
+export const SDK_ACTIONS = {
+  INIT_SDK: 'INIT_SDK',
+  SET_SDK_LOADING: 'SET_SDK_LOADING',
+  RESET_SDK: 'RESET_SDK',
+  SDK_ERROR: 'SDK_ERROR',
+} as const;
+
+type PAYLOAD_TYPES = {
+  [SDK_ACTIONS.SET_SDK_LOADING]: boolean,
+  [SDK_ACTIONS.INIT_SDK]: SendbirdChat,
+  [SDK_ACTIONS.SDK_ERROR]: null,
+  [SDK_ACTIONS.RESET_SDK]: null,
+};
+
+export type SdkActionTypes = CreateAction<PAYLOAD_TYPES>;
