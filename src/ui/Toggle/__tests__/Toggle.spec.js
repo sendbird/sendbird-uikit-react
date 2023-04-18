d9bee1b8e13d483647f22afe2c3c8127146da002
HoonBaek <hoon@sendbird.com>
2023-04-18 16:58:03 +0900

    Improve test code

--- src/ui/Toggle/__tests__/Toggle.spec.js
+++ src/ui/Toggle/__tests__/Toggle.spec.js
@@ -12,19 +12,29 @@ describe('Toggle', () => {
 });
 
 describe('Toggle/utils: filterNumber', () => {
+  it('should return empty array', () => {
+    expect(filterNumber('')).toEqual([]);
+    expect(filterNumber('dlkfjk-ldjs.hge')).toEqual([]);
+    expect(filterNumber(null)).toEqual([]);
+    expect(filterNumber(undefined)).toEqual([]);
+  });
   it('should filter int from string', () => {
-    expect(filterNumber('abcd567hijk')[0]).toBe(567);
+    expect(filterNumber('123')).toEqual([123]);
+    expect(filterNumber('a1b2c3d')).toEqual([1, 2, 3]);
+    expect(filterNumber('abcd567hijk')).toEqual([567]);
   });
   it('should filter float from string', () => {
+    expect(filterNumber('1.23')).toEqual([1.23]);
     expect(filterNumber('abcd5.67hijk')[0]).toBe(5.67);
   });
   it('should filter several int', () => {
-    expect(filterNumber('abcd5 6 7hijk')).toStrictEqual([5, 6, 7]);
+    expect(filterNumber('abcd5 6 7hijk')).toEqual([5, 6, 7]);
   });
   it('should filter several float', () => {
-    expect(filterNumber('abcd5.6.7hijk')).toStrictEqual([5.6, 7]);
+    expect(filterNumber('abcd5.6.7hijk')).toEqual([5.6, 7]);
   });
   it('should filter negative numbers', () => {
-    expect(filterNumber('abcd-1 2-3.4hijk')).toStrictEqual([-1, 2, -3.4]);
+    expect(filterNumber('-10.5 20 30.5')).toEqual([-10.5, 20, 30.5]);
+    expect(filterNumber('abcd-1 2-3.4hijk')).toEqual([-1, 2, -3.4]);
   });
 });
--- src/ui/Toggle/utils.ts
+++ src/ui/Toggle/utils.ts
@@ -1,6 +1,8 @@
-export function filterNumber(str: string): Array<number> {
+export function filterNumber(str: string = ''): Array<number> {
   if (typeof str === 'number') {
-    return str;
+    return [str];
+  } else if (typeof str !== 'string') {
+    return [];
   }
   const regex = /(-?\d+)(\.\d+)?/g;
   const numbers = str.match(regex) || [];
