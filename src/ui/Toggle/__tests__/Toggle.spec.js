c69378975a3b10abf9ec23e339ceb54f44f269cb
HoonBaek <hoon@sendbird.com>
2023-04-18 16:48:02 +0900

    Add tests for filterNumber utils func

--- src/ui/Toggle/__tests__/Toggle.spec.js
+++ src/ui/Toggle/__tests__/Toggle.spec.js
@@ -2,10 +2,29 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Toggle from "../index";
+import { filterNumber } from '../utils';
 
 describe('Toggle', () => {
   it('should do a snapshot test of the Toggle DOM', function() {
     const { asFragment } = render(<Toggle />);
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('Toggle/utils: filterNumber', () => {
+  it('should filter int from string', () => {
+    expect(filterNumber('abcd567hijk')[0]).toBe(567);
+  });
+  it('should filter float from string', () => {
+    expect(filterNumber('abcd5.67hijk')[0]).toBe(5.67);
+  });
+  it('should filter several int', () => {
+    expect(filterNumber('abcd5 6 7hijk')).toStrictEqual([5, 6, 7]);
+  });
+  it('should filter several float', () => {
+    expect(filterNumber('abcd5.6.7hijk')).toStrictEqual([5.6, 7]);
+  });
+  it('should filter negative numbers', () => {
+    expect(filterNumber('abcd-1 2-3.4hijk')).toStrictEqual([-1, 2, -3.4]);
   });
 });
--- src/ui/Toggle/utils.ts
+++ src/ui/Toggle/utils.ts
@@ -1,4 +1,7 @@
 export function filterNumber(str: string): Array<number> {
+  if (typeof str === 'number') {
+    return str;
+  }
   const regex = /(-?\d+)(\.\d+)?/g;
   const numbers = str.match(regex) || [];
   return numbers.map(parseFloat);
