2e8d3ef20ef68b7dc6e784a125a89bf34ce9ff5c
Irene Ryu <irene.ryu@sendbird.com>
2023-04-26 13:33:46 +0900

    chore: replace nodeLinker: node_modules \w pnpm

--- src/lib/dux/sdk/reducers.ts
+++ src/lib/dux/sdk/reducers.ts
@@ -4,14 +4,14 @@ import initialState, { SdkStoreStateType } from './initialState';
 
 export default function reducer(state: SdkStoreStateType, action: SdkActionTypes): SdkStoreStateType {
   return match(action)
-    .with({ type: SDK_ACTIONS.SET_SDK_LOADING }, ({ payload }) => {
+    .with(SDK_ACTIONS.SET_SDK_LOADING, ({ payload }) => {
       return {
         ...state,
         initialized: false,
         loading: payload,
       };
     })
-    .with({ type: SDK_ACTIONS.SDK_ERROR }, () => {
+    .with(SDK_ACTIONS.SDK_ERROR, () => {
       return {
         ...state,
         initialized: false,
@@ -19,7 +19,7 @@ export default function reducer(state: SdkStoreStateType, action: SdkActionTypes
         error: true,
       };
     })
-    .with({ type: SDK_ACTIONS.INIT_SDK }, ({ payload }) => {
+    .with(SDK_ACTIONS.INIT_SDK, ({ payload }) => {
       return {
         sdk: payload,
         initialized: true,
@@ -27,7 +27,7 @@ export default function reducer(state: SdkStoreStateType, action: SdkActionTypes
         error: false,
       };
     })
-    .with({ type: SDK_ACTIONS.RESET_SDK }, () => {
+    .with(SDK_ACTIONS.RESET_SDK, () => {
       return initialState;
     })
     .otherwise(() => {
--- src/lib/dux/user/reducers.ts
+++ src/lib/dux/user/reducers.ts
@@ -3,18 +3,18 @@ import { UserActionTypes, USER_ACTIONS } from './actionTypes';
 import initialState, { UserStoreStateType } from './initialState';
 
 export default function reducer(state: UserStoreStateType, action: UserActionTypes): UserStoreStateType {
-  return match(action)
-    .with({ type: USER_ACTIONS.INIT_USER }, ({ payload }) => {
+  return match(action.type)
+    .with(USER_ACTIONS.INIT_USER, ({ payload }) => {
       return {
         initialized: true,
         loading: false,
         user: payload,
       };
     })
-    .with({ type: USER_ACTIONS.RESET_USER }, () => {
+    .with(USER_ACTIONS.RESET_USER, () => {
       return initialState;
     })
-    .with({ type: USER_ACTIONS.UPDATE_USER_INFO }, ({ payload }) => {
+    .with(USER_ACTIONS.UPDATE_USER_INFO, ({ payload }) => {
       return {
         ...state,
         user: payload,
