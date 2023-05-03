eb5306e7243223a072f56ad76c2f673eb3994171
HoonBaek <hoon@sendbird.com>
2023-05-03 14:53:39 +0900

    Modify the logger method types from optional to required

--- src/lib/SendbirdState.tsx
+++ src/lib/SendbirdState.tsx
@@ -18,10 +18,10 @@ interface RenderUserProfileProps {
 }
 
 export type Logger = {
-  info?(title?: unknown, description?: unknown): void;
-  error?(title?: unknown, description?: unknown): void;
-  warning?(title?: unknown, description?: unknown): void;
-};
+  info(message: string, payload?: Record<string, unknown>): void;
+  error(message: string, payload?: Record<string, unknown>): void;
+  warning(message: string, payload?: Record<string, unknown>): void;
+}
 
 export enum SendbirdUIKitThemes {
   light = 'light',
