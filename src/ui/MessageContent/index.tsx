20e33a27e336749d02e5f059c7c6751041d6410d
HoonBaek <hoon@sendbird.com>
2023-05-16 11:13:27 +0900

    Prevent event of mobile menu in MessageContent comp

--- src/ui/MessageContent/index.tsx
+++ src/ui/MessageContent/index.tsx
@@ -154,6 +154,7 @@ export default function MessageContent({
     },
   }, {
     delay: 300,
+    shouldPreventDefault: false,
   });
 
   if (message?.isAdminMessage?.() || message?.messageType === 'admin') {
