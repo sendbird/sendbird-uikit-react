4f974a9a86daa3d328f789c81a5c53d3343ff533
Matt Sell <matt@atomicbytes.com>
2023-05-17 12:27:04 -0500

    fix tsc issue in typescript sample

--- samples/typescript_sample/src/App.tsx
+++ samples/typescript_sample/src/App.tsx
@@ -1,38 +1,38 @@
-import './App.css';
-import '@sendbird/uikit-react/dist/index.css';
+import './App.css'
+import '@sendbird/uikit-react/dist/index.css'
 
-import React from 'react';
+import React from 'react'
 
-import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
-import ChannelList from '@sendbird/uikit-react/ChannelList';
-import Channel from '@sendbird/uikit-react/Channel';
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider'
+import ChannelList from '@sendbird/uikit-react/ChannelList'
+import Channel from '@sendbird/uikit-react/Channel'
 
-import { APP_ID, USER_ID, NICKNAME } from './const';
+import { APP_ID, USER_ID, NICKNAME } from './const'
 
 function App() {
-  const [currentChannelUrl, setCurrentChannelUrl] = React.useState('');
+  const [currentChannelUrl, setCurrentChannelUrl] = React.useState('')
   return (
     <div className="App">
       <SendbirdProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME}>
-        <div className="sendbird-app__channellist-wrap">
-          {/* ChannelList has wrong props will fix in 2.0.1
+        <>
+          <div className="sendbird-app__channellist-wrap">
+            {/* ChannelList has wrong props will fix in 2.0.1
           // @ts-ignore */}
-          <ChannelList
-            onChannelSelect={(channel) => {
-              if (channel?.url) {
-                setCurrentChannelUrl(channel.url);
-              }
-            }}
-          />
-        </div>
-        <div className="sendbird-app__conversation-wrap">
-          <Channel
-            channelUrl={currentChannelUrl}
-          />
-        </div>
+            <ChannelList
+              onChannelSelect={(channel) => {
+                if (channel?.url) {
+                  setCurrentChannelUrl(channel.url)
+                }
+              }}
+            />
+          </div>
+          <div className="sendbird-app__conversation-wrap">
+            <Channel channelUrl={currentChannelUrl} />
+          </div>
+        </>
       </SendbirdProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
