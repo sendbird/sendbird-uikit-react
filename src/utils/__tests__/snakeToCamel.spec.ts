53a68d8b91289f258aa0b88c884c75df5cd0d3a7
Irene Ryu <irene.ryu@sendbird.com>
2023-05-23 10:16:53 +0900

    Add test for camel -> camel

--- src/utils/__tests__/snakeToCamel.spec.ts
+++ src/utils/__tests__/snakeToCamel.spec.ts
@@ -1,85 +1,88 @@
 import { snakeToCamel } from '../snakeToCamel';
 
+// mock uikit configurations
+const mockSnakeCase = {
+  last_updated_at: 1234567890123,
+  uikit_configurations: {
+    common: {
+      enable_using_default_user_profile: false,
+    },
+    group_channel: {
+      channel: {
+        enable_ogtag: true,
+        enable_typing_indicator: true,
+        enable_reactions: true,
+        enable_mention: false,
+        enable_voice_message: false,
+        reply_type: 'quote_reply',
+        thread_reply_select_type: 'thread',
+        input: {
+          camera: {
+            enable_photo: true,
+            enable_video: true,
+          },
+          gallery: {
+            enable_photo: true,
+            enable_video: true,
+          },
+          enable_document: true,
+        },
+      },
+      channel_list: {
+        enable_typing_indicator: false,
+        enable_message_receipt_status: false,
+      },
+      setting: {
+        enable_message_search: false,
+      },
+    },
+  },
+};
+
+const mockCamelCase = {
+  lastUpdatedAt: 1234567890123,
+  uikitConfigurations: {
+    common: {
+      enableUsingDefaultUserProfile: false,
+    },
+    groupChannel: {
+      channel: {
+        enableMention: false,
+        enableOgtag: true,
+        enableReactions: true,
+        enableTypingIndicator: true,
+        enableVoiceMessage: false,
+        input: {
+          camera: {
+            enablePhoto: true,
+            enableVideo: true,
+          },
+          enableDocument: true,
+          gallery: {
+            enablePhoto: true,
+            enableVideo: true,
+          },
+        },
+        replyType: 'quote_reply',
+        threadReplySelectType: 'thread',
+      },
+      channelList: {
+        enableMessageReceiptStatus: false,
+        enableTypingIndicator: false,
+      },
+      setting: {
+        enableMessageSearch: false,
+      },
+    },
+  },
+};
 describe('snakeToCamel', () => {
   it('should convert snake_case keys to camelCase', () => {
-    // mock uikit configurations
-    const input = {
-      last_updated_at: 1234567890123,
-      uikit_configurations: {
-        common: {
-          enable_using_default_user_profile: false,
-        },
-        group_channel: {
-          channel: {
-            enable_ogtag: true,
-            enable_typing_indicator: true,
-            enable_reactions: true,
-            enable_mention: false,
-            enable_voice_message: false,
-            reply_type: 'quote_reply',
-            thread_reply_select_type: 'thread',
-            input: {
-              camera: {
-                enable_photo: true,
-                enable_video: true,
-              },
-              gallery: {
-                enable_photo: true,
-                enable_video: true,
-              },
-              enable_document: true,
-            },
-          },
-          channel_list: {
-            enable_typing_indicator: false,
-            enable_message_receipt_status: false,
-          },
-          setting: {
-            enable_message_search: false,
-          },
-        },
-      },
-    };
+    expect(snakeToCamel(mockSnakeCase)).toEqual(mockCamelCase);
+  });
 
-    const expectedOutput = {
-      lastUpdatedAt: 1234567890123,
-      uikitConfigurations: {
-        common: {
-          enableUsingDefaultUserProfile: false,
-        },
-        groupChannel: {
-          channel: {
-            enableMention: false,
-            enableOgtag: true,
-            enableReactions: true,
-            enableTypingIndicator: true,
-            enableVoiceMessage: false,
-            input: {
-              camera: {
-                enablePhoto: true,
-                enableVideo: true,
-              },
-              enableDocument: true,
-              gallery: {
-                enablePhoto: true,
-                enableVideo: true,
-              },
-            },
-            replyType: 'quote_reply',
-            threadReplySelectType: 'thread',
-          },
-          channelList: {
-            enableMessageReceiptStatus: false,
-            enableTypingIndicator: false,
-          },
-          setting: {
-            enableMessageSearch: false,
-          },
-        },
-      },
-    };
-
-    expect(snakeToCamel(input)).toEqual(expectedOutput);
+  it('should preserve camelCase object', () => {
+    expect(snakeToCamel(mockCamelCase)).toEqual(mockCamelCase);
   });
 
   it('should handle empty objects and arrays', () => {
