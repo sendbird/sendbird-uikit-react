8cbc0db1c33e44072e395172c793da711343e21f
Irene Ryu <irene.ryu@sendbird.com>
2023-05-31 15:25:40 +0900

    Resolve conflicts

---
+++ src/lib/UIKitConfigProvider/utils/getConfigsByPriority.ts
@@ -0,0 +1,22 @@
+import { DeepPartial, isSameType } from './types';
+import { UIKitConfigInfo } from '../types';
+
+/**
+ * @param localConfigs Set directly in code level. It has higher priority than remote ones
+ * @param remoteConfigs Set by Feature Config setting in Dashboard
+ * @returns
+ */
+export default function getConfigsByPriority(localConfigs: DeepPartial<UIKitConfigInfo>, remoteConfigs: UIKitConfigInfo): UIKitConfigInfo {
+  const prioritizedConfigs = { ...remoteConfigs }; // copy remoteConfigs to prevent mutation
+
+  Object.keys(localConfigs).forEach(key => {
+    if (prioritizedConfigs.hasOwnProperty(key) && isSameType(localConfigs[key], prioritizedConfigs[key])) {
+      prioritizedConfigs[key] = typeof localConfigs[key] === 'object'
+        // Recursively call getConfigsByPriority only when the value of the key is Object
+        ? getConfigsByPriority(localConfigs[key], prioritizedConfigs[key])
+        : localConfigs[key];
+    }
+  });
+
+  return prioritizedConfigs;
+}
