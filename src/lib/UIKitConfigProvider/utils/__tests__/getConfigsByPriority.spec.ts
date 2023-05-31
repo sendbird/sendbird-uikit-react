5df45eecdd60d8853046d14b1a58860442420303
Irene Ryu <irene.ryu@sendbird.com>
2023-05-31 13:18:57 +0900

    change the cond in getConfigsByPriority

--- src/lib/UIKitConfigProvider/utils/__tests__/getConfigsByPriority.spec.ts
+++ src/lib/UIKitConfigProvider/utils/__tests__/getConfigsByPriority.spec.ts
@@ -1,5 +1,56 @@
 import getConfigsByPriority from '../getConfigsByPriority';
 
+const mockRemoteConfigs = {
+  common: {
+    enableUsingDefaultUserProfile: false,
+  },
+  groupChannel: {
+    channel: {
+      enableMention: false,
+      enableOgtag: true,
+      enableReactions: true,
+      enableTypingIndicator: true,
+      enableVoiceMessage: false,
+      input: {
+        camera: {
+          enablePhoto: true,
+          enableVideo: true,
+        },
+        enableDocument: true,
+        gallery: {
+          enablePhoto: true,
+          enableVideo: true,
+        },
+      },
+      replyType: 'quote_reply',
+      threadReplySelectType: 'thread',
+    },
+    channelList: {
+      enableMessageReceiptStatus: false,
+      enableTypingIndicator: false,
+    },
+    setting: {
+      enableMessageSearch: false,
+    },
+  },
+  openChannel: {
+    channel: {
+      enableOgtag: true,
+      input: {
+        camera: {
+          enablePhoto: true,
+          enableVideo: true,
+        },
+        enableDocument: true,
+        gallery: {
+          enablePhoto: true,
+          enableVideo: true,
+        },
+      },
+    },
+  },
+};
+
 describe('getConfigsByPriority', () => {
   it('should override remoteConfigs value \w localConfigs which has higher priority', () => {
     const localConfigs = {
@@ -8,54 +59,10 @@ describe('getConfigsByPriority', () => {
       },
     };
     const remoteConfigs = {
+      // Non-existing configs in localConfigs should be merged too
+      ...mockRemoteConfigs,
       common: {
         enableUsingDefaultUserProfile: false, // Only this value should be replaced
-      },
-      // These below non-existing configs in localConfigs should be merged too
-      groupChannel: {
-        channel: {
-          enableMention: false,
-          enableOgtag: true,
-          enableReactions: true,
-          enableTypingIndicator: true,
-          enableVoiceMessage: false,
-          input: {
-            camera: {
-              enablePhoto: true,
-              enableVideo: true,
-            },
-            enableDocument: true,
-            gallery: {
-              enablePhoto: true,
-              enableVideo: true,
-            },
-          },
-          replyType: 'quote_reply',
-          threadReplySelectType: 'thread',
-        },
-        channelList: {
-          enableMessageReceiptStatus: false,
-          enableTypingIndicator: false,
-        },
-        setting: {
-          enableMessageSearch: false,
-        },
-      },
-      openChannel: {
-        channel: {
-          enableOgtag: true,
-          input: {
-            camera: {
-              enablePhoto: true,
-              enableVideo: true,
-            },
-            enableDocument: true,
-            gallery: {
-              enablePhoto: true,
-              enableVideo: true,
-            },
-          },
-        },
       },
     };
 
@@ -63,5 +70,11 @@ describe('getConfigsByPriority', () => {
       ...remoteConfigs,
       ...localConfigs,
     });
+  });
+
+  it('should merge correctly even localConfigs has empty object or nullable fields', () => {
+    const localConfigs = {};
+    const remoteConfigs = mockRemoteConfigs;
+    expect(getConfigsByPriority(localConfigs, remoteConfigs)).toEqual(remoteConfigs);
   });
 });
--- src/lib/UIKitConfigProvider/utils/getConfigsByPriority.ts
+++ src/lib/UIKitConfigProvider/utils/getConfigsByPriority.ts
@@ -1,6 +1,10 @@
 import { DeepPartial } from './types';
 import { UIKitConfigInfo } from '../types';
 
+function isSameType<T, U>(a: T, b: U): boolean {
+  return typeof a === typeof b;
+}
+
 /**
  * @param localConfigs Set directly in code level. It has higher priority than remote ones
  * @param remoteConfigs Set by Feature Config setting in Dashboard
@@ -10,15 +14,11 @@ export default function getConfigsByPriority(localConfigs: DeepPartial<UIKitConf
   const prioritizedConfigs = { ...remoteConfigs }; // copy remoteConfigs to prevent mutation
 
   Object.keys(localConfigs).forEach(key => {
-    if (
-      prioritizedConfigs.hasOwnProperty(key)
-      && localConfigs[key] !== null
-      // Recursively call getConfigsByPriority only when the value of the key is Object
-      && typeof localConfigs[key] === 'object'
-    ) {
-      prioritizedConfigs[key] = getConfigsByPriority(localConfigs[key], prioritizedConfigs[key]);
-    } else {
-      prioritizedConfigs[key] = localConfigs[key];
+    if (prioritizedConfigs.hasOwnProperty(key) && isSameType(localConfigs[key], prioritizedConfigs[key])) {
+      prioritizedConfigs[key] = typeof localConfigs[key] === 'object'
+        // Recursively call getConfigsByPriority only when the value of the key is Object
+        ? getConfigsByPriority(localConfigs[key], prioritizedConfigs[key])
+        : localConfigs[key];
     }
   });
 
