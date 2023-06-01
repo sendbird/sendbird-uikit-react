543b947771b704505727d84b9c82711689ca55f9
HoonBaek <hoon@sendbird.com>
2023-06-01 16:57:49 +0900

    fix: Disable mention in the broadcast channel

--- src/modules/Thread/components/ThreadMessageInput/index.tsx
+++ src/modules/Thread/components/ThreadMessageInput/index.tsx
@@ -12,6 +12,8 @@ import { useLocalization } from '../../../../lib/LocalizationContext';
 import VoiceMessageInputWrapper from '../../../Channel/components/MessageInput/VoiceMessageInputWrapper';
 import { Role } from '../../../../lib/types';
 import { useDirtyGetMentions } from '../../../Message/hooks/useDirtyGetMentions';
+import { isDisabledBecauseFrozen } from '../../../Channel/context/utils';
+import { isDisabledBecauseMuted } from '../../../Channel/context/utils';
 
 export interface ThreadMessageInputProps {
   className?: string;
@@ -67,9 +69,10 @@ const ThreadMessageInput = (
   const [showVoiceMessageInput, setShowVoiceMessageInput] = useState(false);
   const displaySuggestedMentionList = isOnline
     && isMentionEnabled
-    && mentionNickname.length > 0;
-  // && !utils.isDisabledBecauseFrozen(channel)
-  // && !utils.isDisabledBecauseMuted(channel);
+    && mentionNickname.length > 0
+    && !isDisabledBecauseFrozen(currentChannel)
+    && !isDisabledBecauseMuted(currentChannel)
+    && !currentChannel?.isBroadcast;
 
   // Reset when changing channel
   useEffect(() => {
