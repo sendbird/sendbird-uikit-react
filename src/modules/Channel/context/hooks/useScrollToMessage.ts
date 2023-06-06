3e3ffcb64309fba0e55f908998fc382f18e105c2
Sravan S <sravan-s@protonmail.com>
2023-06-06 15:42:07 +0900

    fix: Click on other message while scroll

--- src/modules/Channel/context/hooks/useScrollToMessage.ts
+++ src/modules/Channel/context/hooks/useScrollToMessage.ts
@@ -15,6 +15,25 @@ interface StaticParams {
   logger: Logger;
 }
 
+// To prevent multiple clicks on the message in the channel while scrolling
+function deactivateClick(scrollRef: React.RefObject<HTMLDivElement>) {
+  const element = scrollRef.current;
+  const parentNode = element?.parentNode as HTMLDivElement;
+  if (element && parentNode) {
+    element.style.pointerEvents = 'none';
+    parentNode.style.cursor = 'wait';
+  }
+}
+
+function activateClick(scrollRef: React.RefObject<HTMLDivElement>) {
+  const element = scrollRef.current;
+  const parentNode = element?.parentNode as HTMLDivElement;
+  if (element && parentNode) {
+    element.style.pointerEvents = 'auto';
+    parentNode.style.cursor = 'auto';
+  }
+}
+
 function useScrollToMessage({
   setInitialTimeStamp,
   setAnimatedMessageId,
@@ -30,15 +49,22 @@ function useScrollToMessage({
       ));
       setAnimatedMessageId(null);
       setTimeout(() => {
-        if (isPresent) {
-          logger.info('Channel: scroll to message - message is present');
-          setAnimatedMessageId(messageId);
-          scrollToRenderedMessage(scrollRef, createdAt);
-        } else {
-          logger.info('Channel: scroll to message - fetching older messages');
-          setInitialTimeStamp(null);
-          setInitialTimeStamp(createdAt);
-          setAnimatedMessageId(messageId);
+        try {
+          logger.info('Channel: scroll to message - disabling mouse events');
+          deactivateClick(scrollRef);
+          if (isPresent) {
+            logger.info('Channel: scroll to message - message is present');
+            setAnimatedMessageId(messageId);
+            scrollToRenderedMessage(scrollRef, createdAt);
+          } else {
+            logger.info('Channel: scroll to message - fetching older messages');
+            setInitialTimeStamp(null);
+            setInitialTimeStamp(createdAt);
+            setAnimatedMessageId(messageId);
+          }
+        } finally {
+          logger.info('Channel: scroll to message - enabled mouse events');
+          activateClick(scrollRef);
         }
       });
     }, [
