2436af2f9549873a36b894e18f51df64bea85e93
Irene Ryu <irene.ryu@sendbird.com>
2023-06-13 00:19:43 +0900

    Set fallback values \w global configs in App comp

--- src/modules/App/AppLayout.tsx
+++ src/modules/App/AppLayout.tsx
@@ -7,20 +7,23 @@ import { useMediaQueryContext } from '../../lib/MediaQueryContext';
 import { DesktopLayout } from './DesktopLayout';
 import { MobileLayout } from './MobileLayout';
 
+import useSendbirdStateContext from '../../hooks/useSendbirdStateContext';
+
 export const AppLayout: React.FC<AppLayoutProps> = (
   props: AppLayoutProps,
 ) => {
   const {
-    isReactionEnabled,
-    replyType,
     isMessageGroupingEnabled,
     allowProfileEdit,
-    showSearchIcon,
     onProfileEditSuccess,
     disableAutoSelect,
     currentChannel,
     setCurrentChannel,
   } = props;
+
+  const globalStore = useSendbirdStateContext();
+  const globalConfigs = globalStore?.config;
+
   const [showThread, setShowThread] = useState(false);
   const [threadTargetMessage, setThreadTargetMessage] = useState<UserMessage | FileMessage | null>(null);
   const [showSettings, setShowSettings] = useState(false);
@@ -28,6 +31,15 @@ export const AppLayout: React.FC<AppLayoutProps> = (
   const [highlightedMessage, setHighlightedMessage] = useState<number | null>(null);
   const [startingPoint, setStartingPoint] = useState<number | null>(null);
   const { isMobile } = useMediaQueryContext();
+
+  /**
+   * Below configs can be set via Dashboard UIKit config setting but as a lower priority than App props.
+   * So need to be have fallback value \w global configs even though each prop values are undefined
+   */
+  const replyType = props.replyType ?? globalConfigs?.replyType;
+  const isReactionEnabled = props.isReactionEnabled ?? globalConfigs?.isReactionEnabled;
+  const showSearchIcon = props.showSearchIcon ?? globalConfigs?.showSearchIcon;
+
   return (
     <>
       {
@@ -35,10 +47,10 @@ export const AppLayout: React.FC<AppLayoutProps> = (
           ? (
             <MobileLayout
               replyType={replyType}
+              showSearchIcon={showSearchIcon}
+              isReactionEnabled={isReactionEnabled}
               isMessageGroupingEnabled={isMessageGroupingEnabled}
               allowProfileEdit={allowProfileEdit}
-              isReactionEnabled={isReactionEnabled}
-              showSearchIcon={showSearchIcon}
               onProfileEditSuccess={onProfileEditSuccess}
               currentChannel={currentChannel}
               setCurrentChannel={setCurrentChannel}
@@ -52,11 +64,11 @@ export const AppLayout: React.FC<AppLayoutProps> = (
           )
           : (
             <DesktopLayout
-              isReactionEnabled={isReactionEnabled}
               replyType={replyType}
+              isReactionEnabled={isReactionEnabled}
+              showSearchIcon={showSearchIcon}
               isMessageGroupingEnabled={isMessageGroupingEnabled}
               allowProfileEdit={allowProfileEdit}
-              showSearchIcon={showSearchIcon}
               onProfileEditSuccess={onProfileEditSuccess}
               disableAutoSelect={disableAutoSelect}
               currentChannel={currentChannel}
