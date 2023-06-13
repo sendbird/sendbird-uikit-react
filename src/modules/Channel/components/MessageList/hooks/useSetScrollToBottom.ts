b9d524d79e346c493d51f6f64e912d5229d9c32b
Sravan S <sravan.sukumarakurup@sendbird.com>
2023-06-13 00:12:32 -0400

    fix: Clear scrollToMessage on loading (#637)
    Clear `scrollBottom` on channel state loading
     
    Fixes: https://sendbird.atlassian.net/browse/UIKIT-4137

--- src/modules/Channel/components/MessageList/hooks/useSetScrollToBottom.ts
+++ src/modules/Channel/components/MessageList/hooks/useSetScrollToBottom.ts
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDebounce } from '../../../../../hooks/useDebounce';
 
 const DELAY = 500;
 
-export function useSetScrollToBottom(): ({
+export function useSetScrollToBottom({
+  loading,
+}: {
+  loading: boolean;
+}): ({
   scrollBottom: number;
   scrollToBottomHandler: (e: React.UIEvent<HTMLDivElement, UIEvent>) => void;
 }) {
   const [scrollBottom, setScrollBottom] = useState(0);
+  useEffect(() => {
+    if (loading) {
+      setScrollBottom(0);
+    }
+  }, [loading]);
   const scrollCb = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
     const element = e.target as HTMLDivElement;
     try {
