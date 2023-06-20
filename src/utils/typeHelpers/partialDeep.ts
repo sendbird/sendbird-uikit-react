eea0398776cd36e4d9224081350a8235844d82af
Irene Ryu <irene.ryu@sendbird.com>
2023-06-20 16:40:02 +0900

    Add brief comment about PartialDeep

--- src/utils/typeHelpers/partialDeep.ts
+++ src/utils/typeHelpers/partialDeep.ts
@@ -1,3 +1,14 @@
+/**
+ * PartialDeep enables partial deep cloning of objects or nested objects.
+ * It recursively makes the properties of the given type optional,
+ * allowing partial modification at any level of nesting.
+ *
+ * Use case:
+ * When working with complex data structures, selectively modify properties of an object
+ * while maintaining the original structure and values.
+ *
+ * Brought the simplified idea from https://github.com/sindresorhus/type-fest/blob/main/source/partial-deep.d.ts
+ * */
 export type PartialDeep<T> = T extends object
   ? T extends (...args: any[]) => any
     ? T
