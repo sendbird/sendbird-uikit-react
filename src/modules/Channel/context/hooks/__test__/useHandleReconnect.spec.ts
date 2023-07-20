dbbbaa9a1036927baed3b502e934f6c3a46272e7
Irene Ryu <irene.ryu@sendbird.com>
2023-07-20 12:04:47 +0900

    Add more test case

--- src/modules/Channel/context/hooks/__test__/useHandleReconnect.spec.ts
+++ src/modules/Channel/context/hooks/__test__/useHandleReconnect.spec.ts
@@ -6,6 +6,8 @@ describe('useReconnectOnIdle', () => {
   beforeAll(() => {
     // Mock dispatchEvent for the document object
     document.dispatchEvent = jest.fn();
+    jest.spyOn(document, 'addEventListener');
+    jest.spyOn(document, 'removeEventListener');
   });
 
   afterEach(() => {
@@ -13,7 +15,7 @@ describe('useReconnectOnIdle', () => {
   });
 
   it('should update shouldReconnect on tab visibility change', () => {
-    const hook = renderHook(() => useReconnectOnIdle(true, { url: 'url' } as GroupChannel));
+    const hook = renderHook(() => useReconnectOnIdle(true, { url: 'url' } as GroupChannel, true));
     expect(hook.result.current.shouldReconnect).toBe(false);
 
     act(() => {
@@ -24,12 +26,26 @@ describe('useReconnectOnIdle', () => {
   });
 
   it('should not update shouldReconnect on isOnline change', () => {
-    const { result, rerender } = renderHook(({ isOnline }) => useReconnectOnIdle(isOnline, null), {
+    const { result, rerender } = renderHook(({ isOnline }) => useReconnectOnIdle(isOnline, { url: 'url' } as GroupChannel, true), {
       initialProps: { isOnline: false },
     });
-    expect(result.current.shouldReconnect).toBe(false);
+    expect(result.current.shouldReconnect).toBe(true);
 
     rerender({ isOnline: true });
     expect(result.current.shouldReconnect).toBe(false);
+  });
+
+  it('should not update shouldReconnect if reconnectOnIdle is false', async () => {
+    const reconnectOnIdle = false;
+    renderHook(({ isOnline }) => useReconnectOnIdle(isOnline, { url: 'url' } as GroupChannel, reconnectOnIdle), {
+      initialProps: { isOnline: false },
+    });
+
+    const spy = jest.spyOn(document, 'addEventListener');
+    document.dispatchEvent(new Event('visibilitychange'));
+
+    const lastCall = spy.mock.calls[spy.mock.calls.length - 1];
+    expect(lastCall).toEqual(['visibilitychange', expect.any(Function)]);
+    spy.mockRestore();
   });
 });
