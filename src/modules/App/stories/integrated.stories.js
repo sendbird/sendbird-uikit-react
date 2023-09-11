5100fb2e7ed481c352eb6912002da7d97e3b4c83
HoonBaek <hoon@sendbird.com>
2023-09-11 12:55:06 +0900

    chore: apply loglevel to the IntegratedApp on storybook sample

--- src/modules/App/stories/integrated.stories.js
+++ src/modules/App/stories/integrated.stories.js
@@ -331,6 +331,7 @@ export const GroupChannel = () => {
                 isVoiceMessageEnabled={sampleOptions.isVoiceMessageEnabled}
                 imageCompression={{ compressionRate: sampleOptions.imageCompression ? 0.7 : 1 }}
                 replyType={sampleOptions.replyType}
+                config={{ logLevel: 'all' }}
                 stringSet={{
                   // CHANNEL_SETTING__MODERATION__REGISTER_AS_OPERATOR: '오퍼레이터 등록',
                   // CHANNEL_SETTING__MODERATION__UNREGISTER_OPERATOR: '오퍼레이터 해제',
