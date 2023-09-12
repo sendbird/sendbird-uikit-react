a385da8e2d372e534dab58aa4b98297f29c6cd4f
HoonBaek <hoon@sendbird.com>
2023-09-12 11:23:55 +0900

    fix: apply review
      - do not set default value of uikitMultipleFilesMessageLimit to 15
      - validate (files.length === 0) first
      - use `match` of ts-pattern instead of if else loop
      - send images first than other files

--- src/modules/Channel/components/MessageInput/useHandleUploadFiles.ts
+++ src/modules/Channel/components/MessageInput/useHandleUploadFiles.ts
@@ -1,4 +1,6 @@
 import { useCallback } from 'react';
+import { match } from 'ts-pattern';
+
 import { Logger } from '../../../../lib/SendbirdState';
 import { SendMFMFunctionType } from '../../context/hooks/useSendMultipleFilesMessage';
 import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
@@ -29,51 +31,56 @@ export const useHandleUploadFiles = ({
   logger,
 }: UseHandleUploadFilesStaticProps): Array<HandleUploadFunctionType> => {
   const { config } = useSendbirdStateContext();
-  const uikitMultipleFilesMessageLimit = config?.uikitMultipleFilesMessageLimit ?? 15;
+  const uikitMultipleFilesMessageLimit = config?.uikitMultipleFilesMessageLimit;
 
   const handleUploadeFiles = useCallback((fileList: FileList) => {
     const files: File[] = Array.from(fileList);
 
+    // Validate Paremeters
     if (!sendFileMessage || !sendMultipleFilesMessage) {
       logger.warning('Channel|useHandleUploadFiles: required functions are undefined', { sendFileMessage, sendMultipleFilesMessage });
       return;
     }
-    if (files.length > 0) {
-      if (files.length > uikitMultipleFilesMessageLimit) {
-        logger.info(`Channel|useHandleUploadeFiles: Cannot upload files more than ${uikitMultipleFilesMessageLimit}`);
-        return;
-      }
-      if (files.some((file: File) => file.size > FILE_SIZE_LIMIT)) {
-        logger.info(`Channel|useHandleUploadeFiles: Cannot upload file size exceeding ${FILE_SIZE_LIMIT}`);
-        return;
-      }
+    if (files.length === 0) {
+      logger.warning('Channel|usehandleUploadFiles: given file list is empty.', { files });
+      return;
+    }
+    if (files.length > uikitMultipleFilesMessageLimit) {
+      logger.info(`Channel|useHandleUploadeFiles: Cannot upload files more than ${uikitMultipleFilesMessageLimit}`);
+      return;
+    }
+    if (files.some((file: File) => file.size > FILE_SIZE_LIMIT)) {
+      logger.info(`Channel|useHandleUploadeFiles: Cannot upload file size exceeding ${FILE_SIZE_LIMIT}`);
+      return;
     }
 
-    if (files.length === 1) {
-      const [file] = files;
-      sendFileMessage(file, quoteMessage);
-    } else if (files.length > 1) {
-      const imageFiles: Array<File> = [];
-      const otherFiles: Array<File> = [];
-      files.forEach((file: File) => {
-        if (isImage(file.type)) {
-          imageFiles.push(file);
-        } else {
-          otherFiles.push(file);
+    match(files.length)
+      .when(n => n === 1, () => {
+        const [file] = files;
+        sendFileMessage(file, quoteMessage);
+      })
+      .when(n => n > 1, () => {
+        const imageFiles: Array<File> = [];
+        const otherFiles: Array<File> = [];
+        files.forEach((file: File) => {
+          if (isImage(file.type)) {
+            imageFiles.push(file);
+          } else {
+            otherFiles.push(file);
+          }
+        });
+
+        if (imageFiles.length > 1) {
+          sendMultipleFilesMessage(imageFiles, quoteMessage);
+        } else if (imageFiles.length === 1) {
+          sendFileMessage(imageFiles[0], quoteMessage);
+        }
+        if (otherFiles.length > 0) {
+          otherFiles.forEach((file: File) => {
+            sendFileMessage(file, quoteMessage);
+          });
         }
       });
-
-      if (otherFiles.length > 0) {
-        otherFiles.forEach((file: File) => {
-          sendFileMessage(file, quoteMessage);
-        });
-      }
-      if (imageFiles.length === 1) {
-        sendFileMessage(imageFiles[0], quoteMessage);
-      } else if (imageFiles.length > 1) {
-        sendMultipleFilesMessage(imageFiles, quoteMessage);
-      }
-    }
   }, [
     sendFileMessage,
     sendMultipleFilesMessage,
