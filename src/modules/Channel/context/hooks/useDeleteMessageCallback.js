f334d3df5259d3e7c4a5c327a7b6c22a0b11390d
chohongm <hongman.cho@gmail.com>
2023-10-16 17:07:47 +0900

    Allow delete failed messages in offline state (re-fix)

--- src/modules/Channel/context/hooks/useDeleteMessageCallback.js
+++ src/modules/Channel/context/hooks/useDeleteMessageCallback.js
@@ -5,11 +5,11 @@ import * as messageActionTypes from '../dux/actionTypes';
 function useDeleteMessageCallback({ currentGroupChannel, messagesDispatcher }, { logger }) {
   return useCallback((message) => {
     logger.info('Channel | useDeleteMessageCallback: Deleting message', message);
-    const { requestState } = message;
+    const { sendingStatus } = message;
     return new Promise((resolve, reject) => {
-      logger.info('Channel | useDeleteMessageCallback: Deleting message requestState:', requestState);
+      logger.info('Channel | useDeleteMessageCallback: Deleting message requestState:', sendingStatus);
       // Message is only on local
-      if (requestState === 'failed' || requestState === 'pending') {
+      if (sendingStatus === 'failed' || sendingStatus === 'pending') {
         logger.info('Channel | useDeleteMessageCallback: Deleted message from local:', message);
         messagesDispatcher({
           type: messageActionTypes.ON_MESSAGE_DELETED_BY_REQ_ID,
@@ -18,7 +18,7 @@ function useDeleteMessageCallback({ currentGroupChannel, messagesDispatcher }, {
         resolve(message);
       }
 
-      logger.info('Channel | useDeleteMessageCallback: Deleting message from remote:', requestState);
+      logger.info('Channel | useDeleteMessageCallback: Deleting message from remote:', sendingStatus);
       currentGroupChannel.deleteMessage(message)
         .then(() => {
           logger.info('Channel | useDeleteMessageCallback: Deleting message success!', message);
