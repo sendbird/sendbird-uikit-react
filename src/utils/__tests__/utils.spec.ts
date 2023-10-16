7eb28b838781e147a874c8121733f7da18e03b4b
bang9 <gusrn1423@naver.com>
2023-10-16 11:54:47 +0900

    chore: update utils.spec.ts

--- src/utils/__tests__/utils.spec.ts
+++ src/utils/__tests__/utils.spec.ts
@@ -6,6 +6,7 @@ import {
   isUserMessage,
   isMultipleFilesMessage,
 } from '../index';
+import { AdminMessage, FileMessage, MultipleFilesMessage, UserMessage } from '@sendbird/chat/message';
 
 describe('Global-utils', () => {
   it('should find right index with binarySearch', () => {
@@ -34,7 +35,7 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => false,
       isMultipleFilesMessage: () => false,
       messageType: 'user',
-    };
+    } as UserMessage;
     expect(isUserMessage(mockUserMessage)).toBe(true);
     expect(isFileMessage(mockUserMessage)).toBe(false);
     expect(isAdminMessage(mockUserMessage)).toBe(false);
@@ -46,7 +47,7 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => false,
       isMultipleFilesMessage: () => false,
       messageType: 'file',
-    };
+    } as FileMessage;
     expect(isUserMessage(mockFileMessage)).toBe(false);
     expect(isFileMessage(mockFileMessage)).toBe(true);
     expect(isAdminMessage(mockFileMessage)).toBe(false);
@@ -58,7 +59,7 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => true,
       isMultipleFilesMessage: () => false,
       messageType: 'admin',
-    };
+    } as AdminMessage;
     expect(isUserMessage(mockAdminMessage)).toBe(false);
     expect(isFileMessage(mockAdminMessage)).toBe(false);
     expect(isAdminMessage(mockAdminMessage)).toBe(true);
@@ -70,7 +71,7 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => false,
       isMultipleFilesMessage: () => true,
       messageType: 'file',
-    };
+    } as MultipleFilesMessage;
     expect(isUserMessage(mockMulipleFilesMessage)).toBe(false);
     expect(isFileMessage(mockMulipleFilesMessage)).toBe(false);
     expect(isAdminMessage(mockMulipleFilesMessage)).toBe(false);
@@ -78,14 +79,14 @@ describe('Global-utils: verify message type util functions', () => {
   });
 
   it('should return true with incomplete properties', () => {
-    expect(isUserMessage({ isUserMessage: () => true })).toBe(true);
-    expect(isUserMessage({ messageType: 'user' })).toBe(true);
-    expect(isFileMessage({ isFileMessage: () => true })).toBe(true);
-    expect(isFileMessage({ messageType: 'file' })).toBe(true);
-    expect(isAdminMessage({ isAdminMessage: () => true })).toBe(true);
-    expect(isAdminMessage({ messageType: 'admin' })).toBe(true);
-    expect(isMultipleFilesMessage({ isMultipleFilesMessage: () => true })).toBe(true);
-    expect(isMultipleFilesMessage({ messageType: 'file', fileInfoList: [] })).toBe(true);
+    expect(isUserMessage({ isUserMessage: () => true } as UserMessage)).toBe(true);
+    expect(isUserMessage({ messageType: 'user' } as UserMessage)).toBe(true);
+    expect(isFileMessage({ isFileMessage: () => true } as FileMessage)).toBe(true);
+    expect(isFileMessage({ messageType: 'file' } as FileMessage)).toBe(true);
+    expect(isAdminMessage({ isAdminMessage: () => true } as AdminMessage)).toBe(true);
+    expect(isAdminMessage({ messageType: 'admin' } as AdminMessage)).toBe(true);
+    expect(isMultipleFilesMessage({ isMultipleFilesMessage: () => true } as MultipleFilesMessage)).toBe(true);
+    expect(isMultipleFilesMessage({ messageType: 'file', fileInfoList: [] } as MultipleFilesMessage)).toBe(true);
   });
 
   it('should refer to the method first rather than messageType', () => {
@@ -96,12 +97,12 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => false,
       isMultipleFilesMessage: () => false,
       messageType: 'file',
-    };
+    } as UserMessage;
     expect(isUserMessage(mockUserMessage)).toBe(true);
     expect(isFileMessage(mockUserMessage)).toBe(false);
     expect(isAdminMessage(mockUserMessage)).toBe(false);
     expect(isMultipleFilesMessage(mockUserMessage)).toBe(false);
-    expect(isUserMessage({ isUserMessage: () => false, messageType: 'user' })).toBe(false);
+    expect(isUserMessage({ isUserMessage: () => false, messageType: 'user' } as UserMessage)).toBe(false);
     const mockFileMessage = {
       messageId: 1,
       isUserMessage: () => false,
@@ -109,12 +110,12 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => false,
       isMultipleFilesMessage: () => false,
       messageType: 'admin',
-    };
+    } as FileMessage;
     expect(isUserMessage(mockFileMessage)).toBe(false);
     expect(isFileMessage(mockFileMessage)).toBe(true);
     expect(isAdminMessage(mockFileMessage)).toBe(false);
     expect(isMultipleFilesMessage(mockFileMessage)).toBe(false);
-    expect(isFileMessage({ isFileMessage: () => false, messsageType: 'file' })).toBe(false);
+    expect(isFileMessage({ isFileMessage: () => false, messageType: 'file' } as FileMessage)).toBe(false);
     const mockAdminMessage = {
       messageId: 2,
       isUserMessage: () => false,
@@ -122,12 +123,12 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => true,
       isMultipleFilesMessage: () => false,
       messageType: 'user',
-    };
+    } as AdminMessage;
     expect(isUserMessage(mockAdminMessage)).toBe(false);
     expect(isFileMessage(mockAdminMessage)).toBe(false);
     expect(isAdminMessage(mockAdminMessage)).toBe(true);
     expect(isMultipleFilesMessage(mockAdminMessage)).toBe(false);
-    expect(isAdminMessage({ isAdminMessage: () => false, messageType: 'admin' })).toBe(false);
+    expect(isAdminMessage({ isAdminMessage: () => false, messageType: 'admin' } as AdminMessage)).toBe(false);
     const mockMultipleFilesMessage = {
       messageId: 2,
       isUserMessage: () => false,
@@ -135,12 +136,12 @@ describe('Global-utils: verify message type util functions', () => {
       isAdminMessage: () => false,
       isMultipleFilesMessage: () => true,
       messageType: 'file',
-    };
+    } as MultipleFilesMessage;
     expect(isUserMessage(mockMultipleFilesMessage)).toBe(false);
     expect(isFileMessage(mockMultipleFilesMessage)).toBe(false);
     expect(isAdminMessage(mockMultipleFilesMessage)).toBe(false);
     expect(isMultipleFilesMessage(mockMultipleFilesMessage)).toBe(true);
-    expect(isMultipleFilesMessage({ isMultipleFilesMessage: () => false, messageType: 'file' })).toBe(false);
+    expect(isMultipleFilesMessage({ isMultipleFilesMessage: () => false, messageType: 'file' } as MultipleFilesMessage)).toBe(false);
   });
 });
 
@@ -169,8 +170,8 @@ describe('isURL', () => {
       'https://example.com/path/to/page.html?query=string',
       'www.example.com/path/to/page.html?query=string',
       'example.com/path/to/page.html?query=string',
-      'https://www.amazon.com/Hacker-Playbook-Practical-Penetration-Testing/dp/1494932636/ref=sr_1_5?crid=1IKVPDXYF5NQG&keywords=hacker+guide&qid=1681333238&sprefix=hacker+guid%2Caps%2C148&sr=8-5'
-    ]
+      'https://www.amazon.com/Hacker-Playbook-Practical-Penetration-Testing/dp/1494932636/ref=sr_1_5?crid=1IKVPDXYF5NQG&keywords=hacker+guide&qid=1681333238&sprefix=hacker+guid%2Caps%2C148&sr=8-5',
+    ];
     validURLs.forEach((url) => {
       expect(isUrl(url)).toBe(true);
     });
@@ -180,7 +181,7 @@ describe('isURL', () => {
       'aaa',
       '$123.123',
       'aaa@sendbird.com',
-    ]
+    ];
     invalidURLs.forEach((url) => {
       expect(isUrl(url)).toBe(false);
     });
