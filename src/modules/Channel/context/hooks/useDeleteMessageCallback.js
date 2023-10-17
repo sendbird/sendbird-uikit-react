277aaa0f6837bdecc19abfe43750918010f0b05b
Liam Hongman Cho <liam.cho@sendbird.com>
2023-10-17 13:39:34 +0900

    bug-fix: Allow delete failed messages in offline state (re-fix) (#780)
    Fixes: [UIKIT-4520](https://sendbird.atlassian.net/browse/UIKIT-4520)
    
    
    [UIKIT-4520]:
    https://sendbird.atlassian.net/browse/UIKIT-4520?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/modules/Channel/context/hooks/useDeleteMessageCallback.js
+++ src/modules/Channel/context/hooks/useDeleteMessageCallback.js
@@ -5,33 +5,33 @@ import * as messageActionTypes from '../dux/actionTypes';
 function useDeleteMessageCallback({ currentGroupChannel, messagesDispatcher }, { logger }) {
   return useCallback((message) => {
     logger.info('Channel | useDeleteMessageCallback: Deleting message', message);
-    const { requestState } = message;
+    const { sendingStatus } = message;
     return new Promise((resolve, reject) => {
-      logger.info('Channel | useDeleteMessageCallback: Deleting message requestState:', requestState);
+      logger.info('Channel | useDeleteMessageCallback: Deleting message requestState:', sendingStatus);
       // Message is only on local
-      if (requestState === 'failed' || requestState === 'pending') {
+      if (sendingStatus === 'failed' || sendingStatus === 'pending') {
         logger.info('Channel | useDeleteMessageCallback: Deleted message from local:', message);
         messagesDispatcher({
           type: messageActionTypes.ON_MESSAGE_DELETED_BY_REQ_ID,
           payload: message.reqId,
         });
         resolve(message);
-      }
-
-      logger.info('Channel | useDeleteMessageCallback: Deleting message from remote:', requestState);
-      currentGroupChannel.deleteMessage(message)
-        .then(() => {
-          logger.info('Channel | useDeleteMessageCallback: Deleting message success!', message);
-          messagesDispatcher({
-            type: messageActionTypes.ON_MESSAGE_DELETED,
-            payload: message.messageId,
+      } else {
+        logger.info('Channel | useDeleteMessageCallback: Deleting message from remote:', sendingStatus);
+        currentGroupChannel.deleteMessage(message)
+          .then(() => {
+            logger.info('Channel | useDeleteMessageCallback: Deleting message success!', message);
+            messagesDispatcher({
+              type: messageActionTypes.ON_MESSAGE_DELETED,
+              payload: message.messageId,
+            });
+            resolve(message);
+          })
+          .catch((err) => {
+            logger.warning('Channel | useDeleteMessageCallback: Deleting message failed!', err);
+            reject(err);
           });
-          resolve(message);
-        })
-        .catch((err) => {
-          logger.warning('Channel | useDeleteMessageCallback: Deleting message failed!', err);
-          reject(err);
-        });
+      }
     });
   }, [currentGroupChannel, messagesDispatcher]);
 }
