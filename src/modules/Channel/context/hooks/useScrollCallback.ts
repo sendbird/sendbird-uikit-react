df6e68f6d4c4e5657f3a0c7d955bcc7036e8905b
bang9 <gusrn1423@naver.com>
2023-10-18 16:18:50 +0900

    chore: Channel useScrollCallback to ts

---
+++ src/modules/Channel/context/hooks/useScrollCallback.ts
@@ -0,0 +1,98 @@
+import { useCallback } from 'react';
+import { ReplyType, MessageListParams, BaseMessage } from '@sendbird/chat/message';
+
+import * as messageActionTypes from '../dux/actionTypes';
+import { ReplyType as ReplyTypeInternal } from '../../../../types';
+import { PREV_RESULT_SIZE } from '../const';
+import { GroupChannel, SendbirdGroupChat } from '@sendbird/chat/groupChannel';
+import { CustomUseReducerDispatcher } from '../../../../lib/SendbirdState';
+import { MessageListParams as MessageListParamsInternal } from '../ChannelProvider';
+import { LoggerInterface } from '../../../../lib/Logger';
+
+type UseScrollCallbackOptions = {
+  currentGroupChannel: GroupChannel;
+  oldestMessageTimeStamp: number;
+  userFilledMessageListQuery: MessageListParamsInternal;
+  replyType: ReplyTypeInternal;
+};
+
+type UseScrollCallbackParams = {
+  hasMorePrev: boolean;
+  logger: LoggerInterface
+  messagesDispatcher: CustomUseReducerDispatcher;
+  sdk: SendbirdGroupChat;
+};
+
+export type UseScrollCallbackReturn = (
+  callback?: (
+    param:
+      | [messages: null, error: unknown]
+      | [messages: BaseMessage[], error: null]
+  ) => void
+) => void;
+
+function useScrollCallback(
+  {
+    currentGroupChannel,
+    oldestMessageTimeStamp,
+    userFilledMessageListQuery,
+    replyType,
+  }: UseScrollCallbackOptions,
+  { hasMorePrev, logger, messagesDispatcher, sdk }: UseScrollCallbackParams,
+): UseScrollCallbackReturn {
+  return useCallback(
+    (callback) => {
+
+      if (!hasMorePrev) {
+        return;
+      }
+
+      const messageListParams: MessageListParamsInternal = {
+        prevResultSize: PREV_RESULT_SIZE,
+        isInclusive: true,
+        includeMetaArray: true,
+        includeReactions: sdk?.appInfo?.useReaction ?? false,
+      };
+
+      if (replyType === 'QUOTE_REPLY' || replyType === 'THREAD') {
+        messageListParams.includeThreadInfo = true;
+        messageListParams.includeParentMessageInfo = true;
+        messageListParams.replyType = ReplyType.ONLY_REPLY_TO_CHANNEL;
+      }
+
+      if (userFilledMessageListQuery) {
+        Object.keys(userFilledMessageListQuery).forEach((key) => {
+          messageListParams[key] = userFilledMessageListQuery[key];
+        });
+      }
+
+      logger.info('Channel: Fetching messages', {
+        currentGroupChannel,
+        userFilledMessageListQuery,
+      });
+
+      currentGroupChannel
+        .getMessagesByTimestamp(
+          oldestMessageTimeStamp || new Date().getTime(),
+          messageListParams as MessageListParams,
+        )
+        .then((messages) => {
+          messagesDispatcher({
+            type: messageActionTypes.FETCH_PREV_MESSAGES_SUCCESS,
+            payload: { currentGroupChannel, messages },
+          });
+          callback?.([messages, null]);
+        })
+        .catch((error) => {
+          messagesDispatcher({
+            type: messageActionTypes.FETCH_PREV_MESSAGES_FAILURE,
+            payload: { currentGroupChannel },
+          });
+          callback?.([null, error]);
+        });
+    },
+    [currentGroupChannel, oldestMessageTimeStamp, replyType],
+  );
+}
+
+export default useScrollCallback;
