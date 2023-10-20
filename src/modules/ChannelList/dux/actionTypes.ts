f6ce7bfe88df115fea9d175dfc08ea521eca9a38
bang9 <gusrn1423@naver.com>
2023-10-20 17:08:39 +0900

    chore: ChannelList/reducer.ts

--- src/modules/ChannelList/dux/actionTypes.ts
+++ src/modules/ChannelList/dux/actionTypes.ts
@@ -1,3 +1,6 @@
+import type { GroupChannel, GroupChannelListQuery } from '@sendbird/chat/groupChannel';
+import { CreateAction } from '../../../utils/typeHelpers/reducers/createAction';
+
 export const RESET_CHANNEL_LIST = 'RESET_CHANNEL_LIST';
 export const CREATE_CHANNEL = 'CREATE_CHANNEL';
 export const UNLOAD_CHANNELS = 'UNLOAD_CHANNELS';
@@ -30,3 +33,45 @@ export const ON_DELIVERY_RECEIPT_UPDATED = 'ON_DELIVERY_RECEIPT_UPDATED';
 
 export const CHANNEL_REPLACED_TO_TOP = 'CHANNEL_REPLACED_TO_TOP';
 export const CHANNEL_LIST_PARAMS_UPDATED = 'CHANNEL_LIST_PARAMS_UPDATED';
+
+type CHANNEL_LIST_PAYLOAD_TYPES = {
+  [RESET_CHANNEL_LIST]: null;
+  [CREATE_CHANNEL]: GroupChannel;
+  [UNLOAD_CHANNELS]: null;
+  [SET_CHANNEL_LOADING]: boolean;
+  [LEAVE_CHANNEL_SUCCESS]: ChannelURL;
+  [SET_CURRENT_CHANNEL]: GroupChannel | null;
+  [FETCH_CHANNELS_START]: null;
+
+  [FETCH_CHANNELS_SUCCESS]: GroupChannel[];
+  [FETCH_CHANNELS_FAILURE]: null;
+  [INIT_CHANNELS_START]: null;
+  [INIT_CHANNELS_SUCCESS]: {
+    channelList: GroupChannel[];
+    disableAutoSelect: boolean;
+  };
+  [INIT_CHANNELS_FAILURE]: null;
+  [INVITE_MEMBERS_SUCESS]: null;
+  [ON_USER_JOINED]: GroupChannel;
+  [ON_CHANNEL_DELETED]: ChannelURL;
+  [ON_LAST_MESSAGE_UPDATED]: GroupChannel;
+  [ON_USER_LEFT]: {
+    isMe: boolean;
+    channel: GroupChannel;
+  };
+  [ON_CHANNEL_CHANGED]: GroupChannel;
+  [ON_CHANNEL_ARCHIVED]: GroupChannel;
+  [ON_CHANNEL_FROZEN]: GroupChannel;
+  [ON_CHANNEL_UNFROZEN]: GroupChannel;
+  [ON_READ_RECEIPT_UPDATED]: GroupChannel;
+  [ON_DELIVERY_RECEIPT_UPDATED]: GroupChannel;
+  [CHANNEL_REPLACED_TO_TOP]: GroupChannel;
+  [CHANNEL_LIST_PARAMS_UPDATED]: {
+    channelListQuery: GroupChannelListQuery;
+    currentUserId: string | undefined;
+  };
+};
+
+type ChannelURL = string;
+
+export type ChannelListActionTypes = CreateAction<CHANNEL_LIST_PAYLOAD_TYPES>;
