10c8feff2008bf7bc0b9349c89cb38c183dcbe02
bang9 <gusrn1423@naver.com>
2023-10-20 14:54:44 +0900

    chore: update OpenChannelApp/Icons.tsx

--- src/modules/OpenChannelApp/assets/Icons.tsx
+++ src/modules/OpenChannelApp/assets/Icons.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react';
 
 import CollapseIcon from './ic-collapse.svg';
@@ -7,9 +8,9 @@ import MembersIcon from './icon-members.svg';
 import LiveBadgeIcon from './icon-badge-live.svg';
 import IconAvatarIcon from './icon-avatar.svg';
 
-export const Collapse = () => (<CollapseIcon />);
-export const Freeze = () => (<FreezeIcon />);
-export const Expand = () => (<ExpandIcon />);
-export const Members = () => (<MembersIcon />);
-export const LiveBadge = () => (<LiveBadgeIcon />);
-export const ProfileAvatar = () => (<IconAvatarIcon />);
+export const Collapse = () => <CollapseIcon />;
+export const Freeze = () => <FreezeIcon />;
+export const Expand = () => <ExpandIcon />;
+export const Members = () => <MembersIcon />;
+export const LiveBadge = () => <LiveBadgeIcon />;
+export const ProfileAvatar = () => <IconAvatarIcon />;
--- src/modules/OpenChannelApp/components/Profile.tsx
+++ src/modules/OpenChannelApp/components/Profile.tsx
@@ -1,27 +1,28 @@
 import React, { ReactElement } from 'react';
+import { User } from '@sendbird/chat';
 
 import './profile.scss';
 import { ProfileAvatar } from '../assets/Icons';
 
 interface Props {
-  user: SendBird.User;
+  user: User;
 }
 
 export default function Profile({
   user,
 }: Props): ReactElement {
   return (
-    <div className="profile-component">
-      <div className="profile-avatar">
+    <div className='profile-component'>
+      <div className='profile-avatar'>
         {
           user.profileUrl
             ? (<img src={user.profileUrl} alt={user.nickname} />)
             : (<ProfileAvatar />)
         }
       </div>
-      <div className="profile-text">
-        <div className="profile-name">{user.nickname}</div>
-        <div className="profile-nickname">{user.userId}</div>
+      <div className='profile-text'>
+        <div className='profile-name'>{user.nickname}</div>
+        <div className='profile-nickname'>{user.userId}</div>
       </div>
     </div>
   );
