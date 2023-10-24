3b022288fed787778cf161ea5ebc7b78790d2992
bang9 <gusrn1423@naver.com>
2023-10-24 10:25:46 +0900

    chore: clean up post_build.js

--- scripts/post_build.js
+++ scripts/post_build.js
@@ -14,48 +14,46 @@ const getDistPath = (subPath) =>
 /**
  * Remove all the unnecessary fields from package.json in root of the project
  */
-async function cleanupPackageJSON(json) {
+async function createPackageJSON(json) {
   const exportList = (await moduleExports).default;
-  const typesVersions = {};
-  const exports = Object.entries(exportList).reduce(
-    (acc, [module, filePath]) => {
-      const types = `./${filePath
-        .replace('src', 'types')
-        .replace(/\.tsx?$/, '.d.ts')}`;
-      typesVersions[module === 'index' ? '.' : `${module}`] = [types];
-      return {
-        ...acc,
-        [module === 'index' ? '.' : `./${module}`]: {
-          types,
-          require: `./cjs/${module}`,
-          import: `./${module}`,
-          default: `./${module}`,
-        },
-      };
-    },
-    {
-      './package.json': './package.json',
-      './dist/index.css': './dist/index.css',
-    },
-  );
+
+  const typesVersions = { '*': {} };
+  const exports = {
+    './package.json': './package.json',
+    './dist/index.css': './dist/index.css',
+  };
+
+  Object.entries(exportList).forEach(([module, filePath]) => {
+    const dtsPath = `./${filePath
+      .replace('src', 'types')
+      .replace(/\.tsx?$/, '.d.ts')}`;
+
+    typesVersions['*'][module === 'index' ? '.' : module] = [dtsPath];
+    exports[module === 'index' ? '.' : `./${module}`] = {
+      types: dtsPath,
+      require: `./cjs/${module}.js`,
+      import: `./${module}.js`,
+      default: `./${module}.js`,
+    };
+  });
   return {
     ...packageTemplate,
-    exports,
-    typesVersions: { '*': typesVersions },
     version: json.version,
     peerDependencies: json.peerDependencies,
     dependencies: json.dependencies,
     browserslist: json.browserslist,
+    exports,
+    typesVersions,
   };
 }
 
 async function movePackageJSON() {
   const packageJSONDistPath = path.resolve(getDistPath(), 'package.json');
-  const cleanedUpPackageJSON = await cleanupPackageJSON(packageJson);
+  const cleanedUpPackageJSON = await createPackageJSON(packageJson);
   fs.writeFileSync(
     packageJSONDistPath,
     JSON.stringify(cleanedUpPackageJSON, null, 2),
-    { flag: 'w' },
+    { flag: 'w' }
   );
 }
 
@@ -63,20 +61,27 @@ function copyCJSPackageJSON() {
   const packageJSONDistPath = path.resolve(getDistPath('/cjs'), 'package.json');
   fs.writeFileSync(
     packageJSONDistPath,
-    JSON.stringify(
-      { type: 'commonjs' },
-      null,
-      2,
-    ),
-    { flag: 'w' },
+    JSON.stringify({ type: 'commonjs' }, null, 2),
+    { flag: 'w' }
   );
+}
+
+function moveCSS() {
+  const cjs = 'dist/cjs';
+  const dist = 'dist/dist';
+  execSync(`rm ${cjs}/index.css ${cjs}/index.css.map`, { stdio: 'inherit' });
+  execSync(`mkdir -p ${dist}`, { stdio: 'inherit' });
+  execSync(`mv dist/index.css dist/index.css.map ${dist}`, { stdio: 'inherit' });
+}
+
+function buildTypeDefinitions() {
+  execSync('tsc --project tsconfig.json --emitDeclarationOnly --declarationDir dist/types --declaration', { stdio: 'inherit' });
 }
 
 /** Copy content of package.json to dist, but remove unnecessary fields */
 movePackageJSON();
 /** Copy content of package.json to dist/cjs, to support cjs module separately */
 copyCJSPackageJSON();
-execSync('rm dist/cjs/index.css', { stdio: 'inherit' });
-execSync('mkdir -p dist/dist', { stdio: 'inherit' });
-execSync('mv dist/index.css dist/dist/index.css', { stdio: 'inherit' });
-execSync('tsc --project tsconfig.json --emitDeclarationOnly --declarationDir dist/types --declaration');
+
+moveCSS();
+buildTypeDefinitions();
