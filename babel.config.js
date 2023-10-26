aef7cf12efc05e0c04cb9d0a05a4613a0677b0ac
bang9 <gusrn1423@naver.com>
2023-10-26 16:21:43 +0900

    chore: revert babel.config.js and add env plugins explicitly

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,13 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-      },
-    ],
+    '@babel/preset-env',
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
   plugins: [
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true,
-      },
-    ],
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-export-default-from',
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-proposal-private-property-in-object',
   ],
 };
