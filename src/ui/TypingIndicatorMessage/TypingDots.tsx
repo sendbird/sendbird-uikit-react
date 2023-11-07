0d0acf522f7de18899cf980faf77dee7673d7a20
Liam Hongman Cho <liam.cho@sendbird.com>
2023-11-07 13:41:33 +0900

    feat: Add new TypingDots UI component (#835)
    Fixes: [UIKIT-4563](https://sendbird.atlassian.net/browse/UIKIT-4563)
    
    https://www.webmobilefirst.com/en/screencasts/m8A0hyjGJS/
    
    figma design:
    https://www.figma.com/file/SVbXU00FhjztekD8AiVukK/UIKit_Work-file_React?node-id=1869%3A800&mode=dev
    
    [UIKIT-4563]:
    https://sendbird.atlassian.net/browse/UIKIT-4563?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/ui/TypingIndicatorMessage/TypingDots.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './typingDots.scss';
+
+const TypingDots = () => {
+
+  return (
+    <div className='typing-dots-container'>
+      <span/>
+      <span/>
+      <span/>
+    </div>
+  );
+};
+
+export default TypingDots;
