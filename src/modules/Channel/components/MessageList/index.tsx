ddbf9bd4afc90a8d88f828dfacc791716728df6f
chohongm <hongman.cho@gmail.com>
2023-11-22 18:10:46 +0900

    remove comment

--- src/modules/Channel/components/MessageList/index.tsx
+++ src/modules/Channel/components/MessageList/index.tsx
@@ -170,7 +170,6 @@ const MessageList: React.FC<MessageListProps> = ({
         });
         markAsReadScheduler.push(currentGroupChannel);
       }
-      console.log('## reached bottom: ', );
       setIsScrollBottom(true);
     },
     onReachedTop: () => {
