40d04bca82e236710783ed87d831087cff6e178f
HoonBaek <hoon@sendbird.com>
2023-11-24 11:03:44 +0900

    Split the ChannelListHeader to container and view

---
+++ src/modules/GroupChannelList/components/ChannelListHeader/ChannelListHeaderView.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Avatar from '../../../../ui/Avatar';
+import Label, { LabelColors, LabelTypography } from '../../../../ui/Label';
+import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
+import { useLocalization } from '../../../../lib/LocalizationContext';
+
+export interface ChannelListHeaderViewProps {
+  renderTitle?: () => React.ReactElement;
+  renderIconButton?: (props: void) => React.ReactElement;
+  onEdit?: (props: void) => void;
+  allowProfileEdit?: boolean;
+}
+
+export const ChannelListHeaderView = ({
+  renderTitle,
+  renderIconButton,
+  onEdit,
+  allowProfileEdit,
+}: ChannelListHeaderViewProps) => {
+  const { stores } = useSendbirdStateContext();
+  const { user } = stores.userStore;
+
+  const { stringSet } = useLocalization();
+
+  return (
+    <div
+      className={[
+        'sendbird-channel-header',
+        allowProfileEdit ? 'sendbird-channel-header--allow-edit' : '',
+      ].join(' ')}
+    >
+      {renderTitle?.() || (
+        <div
+          className="sendbird-channel-header__title"
+          role="button"
+          onClick={() => {
+            onEdit?.();
+          }}
+          onKeyDown={() => {
+            onEdit?.();
+          }}
+          tabIndex={0}
+        >
+          <div className="sendbird-channel-header__title__left">
+            <Avatar
+              width="32px"
+              height="32px"
+              src={user.profileUrl}
+              alt={user.nickname}
+            />
+          </div>
+          <div className="sendbird-channel-header__title__right">
+            <Label
+              className="sendbird-channel-header__title__right__name"
+              type={LabelTypography.SUBTITLE_2}
+              color={LabelColors.ONBACKGROUND_1}
+            >
+              {user.nickname || stringSet.NO_NAME}
+            </Label>
+            <Label
+              className="sendbird-channel-header__title__right__user-id"
+              type={LabelTypography.BODY_2}
+              color={LabelColors.ONBACKGROUND_2}
+            >
+              {user.userId}
+            </Label>
+          </div>
+        </div>
+      )}
+      <div className="sendbird-channel-header__right-icon">
+        {renderIconButton?.()}
+      </div>
+    </div>
+  );
+};
--- src/modules/GroupChannelList/components/ChannelListHeader/index.tsx
+++ src/modules/GroupChannelList/components/ChannelListHeader/index.tsx
@@ -1,11 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { LocalizationContext } from '../../../../lib/LocalizationContext';
-import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
-import Label, { LabelTypography, LabelColors } from '../../../../ui/Label';
-
+import { ChannelListHeaderView } from './ChannelListHeaderView';
 import './index.scss';
-import Avatar from '../../../../ui/Avatar';
 
 interface ChannelListHeaderProps {
   renderTitle?: () => React.ReactElement;
@@ -14,65 +10,9 @@ interface ChannelListHeaderProps {
   allowProfileEdit?: boolean;
 }
 
-const ChannelListHeader = ({
-  renderTitle,
-  renderIconButton,
-  onEdit,
-  allowProfileEdit,
-}: ChannelListHeaderProps) => {
-  const { stores } = useSendbirdStateContext();
-  const { user } = stores.userStore;
-
-  const { stringSet } = useContext(LocalizationContext);
-
-  return (
-    <div
-      className={[
-        'sendbird-channel-header',
-        allowProfileEdit ? 'sendbird-channel-header--allow-edit' : '',
-      ].join(' ')}
-    >
-      {
-        renderTitle?.() || (
-          <div
-            className="sendbird-channel-header__title"
-            role="button"
-            onClick={() => { onEdit?.(); }}
-            onKeyDown={() => { onEdit?.(); }}
-            tabIndex={0}
-          >
-            <div className="sendbird-channel-header__title__left">
-              <Avatar
-                width="32px"
-                height="32px"
-                src={user.profileUrl}
-                alt={user.nickname}
-              />
-            </div>
-            <div className="sendbird-channel-header__title__right">
-              <Label
-                className="sendbird-channel-header__title__right__name"
-                type={LabelTypography.SUBTITLE_2}
-                color={LabelColors.ONBACKGROUND_1}
-              >
-                {user.nickname || stringSet.NO_NAME}
-              </Label>
-              <Label
-                className="sendbird-channel-header__title__right__user-id"
-                type={LabelTypography.BODY_2}
-                color={LabelColors.ONBACKGROUND_2}
-              >
-                {user.userId}
-              </Label>
-            </div>
-          </div>
-        )
-      }
-      <div className="sendbird-channel-header__right-icon">
-        {renderIconButton?.()}
-      </div>
-    </div>
-  );
+const ChannelListHeader = (props: ChannelListHeaderProps) => {
+  return <ChannelListHeaderView {...props} />;
 };
 
 export default ChannelListHeader;
+export { ChannelListHeaderView, ChannelListHeaderViewProps } from './ChannelListHeaderView';
