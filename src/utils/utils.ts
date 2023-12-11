69e76ff0bee33fb135bbadae052b10f22b080c38
bang9 <gusrn1423@naver.com>
2023-12-11 18:10:48 +0900

    fix: do not call preventDefault in mobile safari

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,6 +10,14 @@ export const getSenderName = (message: SendableMessageType) => (
   )
 );
 export const isAboutSame = (a: number, b: number, px: number) => Math.abs(a - b) <= px;
+export const isMobileWKWebviewOrSafari = (userAgent: string) => {
+  const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
+  const isChromeIOS = /CriOS/i.test(userAgent);
+  const isWKWebview = /WebKit/i.test(userAgent);
+  const isSafari = /Safari/i.test(userAgent);
+
+  return isIOS && !isChromeIOS && (isWKWebview || isSafari);
+};
 
 export default {
   getSenderName,
