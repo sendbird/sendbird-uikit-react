bf4eb148c2f19ee8f0fe99c31b75548606d2cfc8
HoonBaek <hoon@sendbird.com>
2024-01-09 14:48:17 +0900

    Remove unused ~View components

--- src/modules/GroupChannel/components/Message/index.tsx
+++ src/modules/GroupChannel/components/Message/index.tsx
@@ -1,14 +1,345 @@
-import React from 'react';
-import { MessageView, type MessageViewProps } from './MessageView';
+import React, { useRef, useMemo, useState, useEffect, useLayoutEffect } from 'react';
+import format from 'date-fns/format';
+import type { FileMessage } from '@sendbird/chat/message';
+
+import useDidMountEffect from '../../../../utils/useDidMountEffect';
+import SuggestedMentionList from '../SuggestedMentionList';
+import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
+import { getClassName, getSuggestedReplies, isSendableMessage } from '../../../../utils';
+import { isDisabledBecauseFrozen, isDisabledBecauseMuted } from '../../context/utils';
+import { MAX_USER_MENTION_COUNT, MAX_USER_SUGGESTION_COUNT } from '../../context/const';
+
+import type { EveryMessage, RenderCustomSeparatorProps, RenderMessageProps } from '../../../../types';
+import DateSeparator from '../../../../ui/DateSeparator';
+import Label, { LabelTypography, LabelColors } from '../../../../ui/Label';
+import MessageInput from '../../../../ui/MessageInput';
+import MessageContent from '../../../../ui/MessageContent';
+import FileViewer from '../FileViewer';
+import RemoveMessageModal from '../RemoveMessageModal';
+import { MessageInputKeys } from '../../../../ui/MessageInput/const';
+import { useLocalization } from '../../../../lib/LocalizationContext';
+import { useDirtyGetMentions } from '../../../Message/hooks/useDirtyGetMentions';
+import SuggestedReplies from '../SuggestedReplies';
+import { useIIFE } from '@sendbird/uikit-tools';
 import { useGroupChannelContext } from '../../context/GroupChannelProvider';
 
-export const Message = (props: MessageViewProps) => {
-  const context = useGroupChannelContext();
+export interface MessageProps {
+  message: EveryMessage;
+  hasSeparator?: boolean;
+  chainTop?: boolean;
+  chainBottom?: boolean;
+  handleScroll?: (isBottomMessageAffected?: boolean) => void;
+  // for extending
+  renderMessage?: (props: RenderMessageProps) => React.ReactElement;
+  renderCustomSeparator?: (props: RenderCustomSeparatorProps) => React.ReactElement;
+  renderEditInput?: () => React.ReactElement;
+  renderMessageContent?: () => React.ReactElement;
+}
+
+// TODO: Refactor this component, is too complex now
+export const Message = ({
+  message,
+  hasSeparator,
+  chainTop,
+  chainBottom,
+  handleScroll,
+  renderCustomSeparator,
+  renderEditInput,
+  renderMessage,
+  renderMessageContent,
+}: MessageProps): React.ReactElement => {
+  const { dateLocale, stringSet } = useLocalization();
+  const globalStore = useSendbirdStateContext();
+  const {
+    loading,
+    currentChannel,
+    animatedMessageId,
+    setAnimatedMessageId,
+    scrollToMessage,
+    replyType,
+    threadReplySelectType,
+    isReactionEnabled,
+    toggleReaction,
+    nicknamesMap,
+    setQuoteMessage,
+    renderUserMentionItem,
+    onQuoteMessageClick,
+    onReplyInThreadClick,
+    onMessageAnimated,
+    messages,
+    updateUserMessage,
+    sendUserMessage,
+    resendMessage,
+    deleteMessage,
+  } = useGroupChannelContext();
+
+  const { userId, isOnline, isMentionEnabled, userMention, logger } = globalStore.config;
+  const maxUserMentionCount = userMention?.maxMentionCount || MAX_USER_MENTION_COUNT;
+  const maxUserSuggestionCount = userMention?.maxSuggestionCount || MAX_USER_SUGGESTION_COUNT;
+
+  const { emojiManager } = useSendbirdStateContext();
+
+  const initialized = !loading && Boolean(currentChannel);
+
+  const [showEdit, setShowEdit] = useState(false);
+  const [showRemove, setShowRemove] = useState(false);
+  const [showFileViewer, setShowFileViewer] = useState(false);
+  const [isAnimated, setIsAnimated] = useState(false);
+  const [mentionNickname, setMentionNickname] = useState('');
+  const [mentionedUsers, setMentionedUsers] = useState([]);
+  const [mentionedUserIds, setMentionedUserIds] = useState([]);
+  const [messageInputEvent, setMessageInputEvent] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [mentionSuggestedUsers, setMentionSuggestedUsers] = useState([]);
+  const editMessageInputRef = useRef(null);
+  const displaySuggestedMentionList = isOnline
+    && isMentionEnabled
+    && mentionNickname.length > 0
+    && !isDisabledBecauseFrozen(currentChannel)
+    && !isDisabledBecauseMuted(currentChannel);
+  const disabled = !initialized || isDisabledBecauseFrozen(currentChannel) || isDisabledBecauseMuted(currentChannel) || !isOnline;
+
+  const mentionNodes = useDirtyGetMentions({ ref: editMessageInputRef }, { logger });
+  const ableMention = mentionNodes?.length < maxUserMentionCount;
+
+  useEffect(() => {
+    setMentionedUsers(
+      mentionedUsers.filter(({ userId }) => {
+        const i = mentionedUserIds.indexOf(userId);
+        if (i < 0) {
+          return false;
+        } else {
+          mentionedUserIds.splice(i, 1);
+          return true;
+        }
+      }),
+    );
+  }, [mentionedUserIds]);
+
+  useLayoutEffect(() => {
+    // Keep the scrollBottom value after fetching new message list
+    handleScroll?.(true);
+  }, []);
+  /**
+   * Move the messsage list scroll
+   * when the message's height is changed by `showEdit` OR `message.reactions`
+   */
+  useDidMountEffect(() => {
+    handleScroll?.();
+  }, [showEdit, message?.reactions?.length]);
+  useDidMountEffect(() => {
+    handleScroll?.(true);
+  }, [message?.updatedAt]);
+
+  useLayoutEffect(() => {
+    const timeouts = [];
+
+    if (animatedMessageId === message.messageId) {
+      timeouts.push(
+        setTimeout(() => {
+          setIsAnimated(true);
+        }, 500),
+      );
+      timeouts.push(
+        setTimeout(() => {
+          setAnimatedMessageId(0);
+          onMessageAnimated?.();
+        }, 1600),
+      );
+    } else {
+      setIsAnimated(false);
+    }
+
+    return () => {
+      timeouts.forEach((it) => clearTimeout(it));
+    };
+  }, [animatedMessageId, setAnimatedMessageId]);
+
+  const renderedMessage = useMemo(() => {
+    return renderMessage?.({ message, chainTop, chainBottom });
+  }, [message, renderMessage]);
+
+  const renderedCustomSeparator = useMemo(() => {
+    return renderCustomSeparator?.({ message });
+  }, [message, renderCustomSeparator]);
+
+  if (renderedMessage) {
+    return (
+      <div
+        // do not delete this data attribute, used for scroll to given message
+        // and also for testing
+        data-sb-message-id={message.messageId}
+        data-sb-created-at={message.createdAt}
+        className={getClassName(['sendbird-msg-hoc sendbird-msg--scroll-ref', isAnimated ? 'sendbird-msg-hoc__animated' : ''])}
+      >
+        {/* date-separator */}
+        {
+          // TODO: Add message instance as a function parameter
+          hasSeparator
+          && (renderedCustomSeparator || (
+            <DateSeparator>
+              <Label type={LabelTypography.CAPTION_2} color={LabelColors.ONBACKGROUND_2}>
+                {format(message.createdAt, stringSet.DATE_FORMAT__MESSAGE_LIST__DATE_SEPARATOR, {
+                  locale: dateLocale,
+                })}
+              </Label>
+            </DateSeparator>
+          ))
+        }
+        {renderedMessage}
+      </div>
+    );
+  }
+
+  if (showEdit && message?.isUserMessage?.()) {
+    return (
+      renderEditInput?.() || (
+        <>
+          {displaySuggestedMentionList && (
+            <SuggestedMentionList
+              currentChannel={currentChannel}
+              targetNickname={mentionNickname}
+              inputEvent={messageInputEvent}
+              renderUserMentionItem={renderUserMentionItem}
+              onUserItemClick={(user) => {
+                if (user) {
+                  setMentionedUsers([...mentionedUsers, user]);
+                }
+                setMentionNickname('');
+                setSelectedUser(user);
+                setMessageInputEvent(null);
+              }}
+              onFocusItemChange={() => {
+                setMessageInputEvent(null);
+              }}
+              onFetchUsers={(users) => {
+                setMentionSuggestedUsers(users);
+              }}
+              ableAddMention={ableMention}
+              maxMentionCount={maxUserMentionCount}
+              maxSuggestionCount={maxUserSuggestionCount}
+            />
+          )}
+          <MessageInput
+            isEdit
+            channel={currentChannel}
+            disabled={disabled}
+            ref={editMessageInputRef}
+            mentionSelectedUser={selectedUser}
+            isMentionEnabled={isMentionEnabled}
+            message={message}
+            onStartTyping={() => {
+              currentChannel?.startTyping?.();
+            }}
+            onUpdateMessage={({ messageId, message, mentionTemplate }) => {
+              updateUserMessage(messageId, {
+                message,
+                mentionedUsers,
+                mentionedMessageTemplate: mentionTemplate,
+              });
+              setShowEdit(false);
+              currentChannel?.endTyping?.();
+            }}
+            onCancelEdit={() => {
+              setMentionNickname('');
+              setMentionedUsers([]);
+              setMentionedUserIds([]);
+              setMentionSuggestedUsers([]);
+              setShowEdit(false);
+              currentChannel?.endTyping?.();
+            }}
+            onUserMentioned={(user) => {
+              if (selectedUser?.userId === user?.userId) {
+                setSelectedUser(null);
+                setMentionNickname('');
+              }
+            }}
+            onMentionStringChange={(mentionText) => {
+              setMentionNickname(mentionText);
+            }}
+            onMentionedUserIdsUpdated={(userIds) => {
+              setMentionedUserIds(userIds);
+            }}
+            onKeyDown={(e) => {
+              if (
+                displaySuggestedMentionList
+                && mentionSuggestedUsers?.length > 0
+                && ((e.key === MessageInputKeys.Enter && ableMention)
+                  || e.key === MessageInputKeys.ArrowUp
+                  || e.key === MessageInputKeys.ArrowDown)
+              ) {
+                setMessageInputEvent(e);
+                return true;
+              }
+              return false;
+            }}
+          />
+        </>
+      )
+    );
+  }
+
+  const shouldRenderSuggestedReplies = useIIFE(() => {
+    if (message.messageId !== currentChannel?.lastMessage?.messageId) return false;
+    if (getSuggestedReplies(message).length === 0) return false;
+    const lastMessage = messages[messages.length - 1];
+    if (lastMessage && isSendableMessage(lastMessage) && lastMessage.sendingStatus !== 'succeeded') return false;
+
+    return true;
+  });
+
   return (
-    <MessageView
-      {...props}
-      {...context}
-    />
+    <div
+      className={getClassName(['sendbird-msg-hoc sendbird-msg--scroll-ref', isAnimated ? 'sendbird-msg-hoc__animated' : ''])}
+      style={{ marginBottom: '2px' }}
+      data-sb-message-id={message.messageId}
+      data-sb-created-at={message.createdAt}
+    >
+      {/* date-separator */}
+      {hasSeparator
+        && (renderedCustomSeparator || (
+          <DateSeparator>
+            <Label type={LabelTypography.CAPTION_2} color={LabelColors.ONBACKGROUND_2}>
+              {format(message.createdAt, stringSet.DATE_FORMAT__MESSAGE_LIST__DATE_SEPARATOR, {
+                locale: dateLocale,
+              })}
+            </Label>
+          </DateSeparator>
+        ))}
+      {/* Message */}
+      {renderMessageContent?.() || (
+        <MessageContent
+          className="sendbird-message-hoc__message-content"
+          userId={userId}
+          scrollToMessage={scrollToMessage}
+          channel={currentChannel}
+          message={message}
+          disabled={!isOnline}
+          chainTop={chainTop}
+          chainBottom={chainBottom}
+          isReactionEnabled={isReactionEnabled}
+          replyType={replyType}
+          threadReplySelectType={threadReplySelectType}
+          nicknamesMap={nicknamesMap}
+          emojiContainer={emojiManager.emojiContainer}
+          showEdit={setShowEdit}
+          showRemove={setShowRemove}
+          showFileViewer={setShowFileViewer}
+          resendMessage={resendMessage}
+          deleteMessage={deleteMessage as any}
+          toggleReaction={toggleReaction}
+          setQuoteMessage={setQuoteMessage}
+          onReplyInThread={onReplyInThreadClick}
+          onQuoteMessageClick={onQuoteMessageClick}
+          onMessageHeightChange={handleScroll}
+        />
+      )}
+      {/** Suggested Replies */}
+      {shouldRenderSuggestedReplies && <SuggestedReplies replyOptions={getSuggestedReplies(message)} onSendMessage={sendUserMessage} />}
+      {/* Modal */}
+      {showRemove && <RemoveMessageModal message={message} onCancel={() => setShowRemove(false)} />}
+      {showFileViewer && <FileViewer message={message as FileMessage} onCancel={() => setShowFileViewer(false)} />}
+    </div>
   );
 };
 
--- src/modules/GroupChannel/components/MessageInputWrapper/index.tsx
+++ src/modules/GroupChannel/components/MessageInputWrapper/index.tsx
@@ -1,47 +1,283 @@
-import React from 'react';
+import './index.scss';
+import React, { useState, useEffect } from 'react';
+import type { User } from '@sendbird/chat';
+import type { GroupChannel } from '@sendbird/chat/groupChannel';
+import type {
+  FileMessage,
+  FileMessageCreateParams,
+  MultipleFilesMessage,
+  MultipleFilesMessageCreateParams,
+  UserMessage,
+  UserMessageCreateParams,
+} from '@sendbird/chat/message';
 
-import type { ChannelProviderInterface } from '../../../Channel/context/ChannelProvider';
-import type { GroupChannelProviderInterface } from '../../context/GroupChannelProvider';
-import { MessageInputWrapperView } from './MessageInputWrapperView';
+import {
+  isOperator as isChannelOperator,
+  isDisabledBecauseFrozen,
+  isDisabledBecauseMuted,
+} from '../../context/utils';
+import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
+import { useLocalization } from '../../../../lib/LocalizationContext';
+import SuggestedMentionList from '../SuggestedMentionList';
+import { useDirtyGetMentions } from '../../../Message/hooks/useDirtyGetMentions';
+import { SendableMessageType } from '../../../../utils';
+import QuoteMessageInput from '../../../../ui/QuoteMessageInput';
+import VoiceMessageInputWrapper from './VoiceMessageInputWrapper';
+import MessageInput from '../../../../ui/MessageInput';
+import { useMediaQueryContext } from '../../../../lib/MediaQueryContext';
+import { MessageInputKeys } from '../../../../ui/MessageInput/const';
+import { useHandleUploadFiles } from './useHandleUploadFiles';
+import { useHandleUploadFiles as useHandleUploadFilesLegacy } from '../../../Channel/context/hooks/useHandleUploadFiles';
 
-export type MessageInputWrapperProps = {
-  disabled?: boolean;
+type SendFileMessageType = (params: FileMessageCreateParams) => Promise<FileMessage>;
+type SendFileMessageTypeLegacy = (file: File, quoteMessage?: SendableMessageType) => Promise<FileMessage>;
+type SendVoiceMessageType = (params: FileMessageCreateParams, duration: number) => Promise<FileMessage>;
+type SendVoiceMessageTypeLegacy = (file: File, duration: number, quoteMessage?: SendableMessageType) => void;
+type SendMultipleFilesMessageType = (params: MultipleFilesMessageCreateParams) => Promise<MultipleFilesMessage>;
+type SendMultipleFilesMessageTypeLegacy = (files: Array<File>, quoteMessage?: SendableMessageType) => Promise<MultipleFilesMessage>;
+
+export interface MessageInputWrapperProps {
+  currentChannel: GroupChannel;
+  isMultipleFilesMessageEnabled?: boolean;
+  loading: boolean;
+  quoteMessage: SendableMessageType | null;
+  setQuoteMessage: React.Dispatch<React.SetStateAction<SendableMessageType | null>>;
+  messageInputRef: React.MutableRefObject<HTMLDivElement>;
+  sendUserMessage: (params: UserMessageCreateParams) => Promise<UserMessage> | void;
+  sendFileMessage: SendFileMessageType | SendFileMessageTypeLegacy;
+  sendVoiceMessage: SendVoiceMessageType | SendVoiceMessageTypeLegacy;
+  sendMultipleFilesMessage: SendMultipleFilesMessageType | SendMultipleFilesMessageTypeLegacy;
+  // render
+  renderUserMentionItem?: (props: { user: User }) => React.ReactElement;
   renderFileUploadIcon?: () => React.ReactElement;
   renderVoiceMessageIcon?: () => React.ReactElement;
   renderSendMessageIcon?: () => React.ReactElement;
-};
+}
 
-export const MessageInputWrapper = (props: MessageInputWrapperProps & (GroupChannelProviderInterface | ChannelProviderInterface)) => {
-  /**
-   * GroupChannelProviderInterface has currentChannel
-   * ChannelProviderInterface has currentGroupChannel
-   */
+export const MessageInputWrapper = React.forwardRef((
+  props: MessageInputWrapperProps,
+  ref: React.MutableRefObject<any>,
+) => {
+  // Props
+  const {
+    currentChannel,
+    isMultipleFilesMessageEnabled: localIsMFMEnabled,
+    loading,
+    quoteMessage,
+    setQuoteMessage,
+    messageInputRef,
+    sendUserMessage,
+    sendFileMessage,
+    sendVoiceMessage,
+    sendMultipleFilesMessage,
+    // render
+    renderUserMentionItem,
+    renderFileUploadIcon,
+    renderVoiceMessageIcon,
+    renderSendMessageIcon,
+  } = props;
   const isLegacyChannel = Object.hasOwn(props, 'currentGroupChannel');
+  const { stringSet } = useLocalization();
+  const { isMobile } = useMediaQueryContext();
+  const { config } = useSendbirdStateContext();
+  const {
+    isOnline,
+    isMentionEnabled,
+    isVoiceMessageEnabled,
+    isMultipleFilesMessageEnabled: globalIsMFMenabled,
+    userMention,
+    logger,
+  } = config;
+  const { maxMentionCount, maxSuggestionCount } = userMention;
 
-  if (!isLegacyChannel) {
-    const { disabled } = props as MessageInputWrapperProps & GroupChannelProviderInterface;
-    return (
-      <MessageInputWrapperView
-        {...props as MessageInputWrapperProps & GroupChannelProviderInterface}
-        isDisabled={disabled}
-      />
+  const { isBroadcast } = currentChannel;
+  const isOperator = isChannelOperator(currentChannel);
+
+  // States
+  const [mentionNickname, setMentionNickname] = useState('');
+  const [mentionedUsers, setMentionedUsers] = useState([]);
+  const [mentionedUserIds, setMentionedUserIds] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [mentionSuggestedUsers, setMentionSuggestedUsers] = useState([]);
+  const [messageInputEvent, setMessageInputEvent] = useState(null);
+  const [showVoiceMessageInput, setShowVoiceMessageInput] = useState(false);
+
+  // Conditions
+  const isMessageInputDisabled = loading
+    || !currentChannel
+    || isDisabledBecauseFrozen(currentChannel)
+    || isDisabledBecauseMuted(currentChannel)
+    || !isOnline;
+  const showSuggestedMentionList = !isMessageInputDisabled
+    && isMentionEnabled
+    && mentionNickname.length > 0
+    && !isBroadcast;
+  const isMultipleFilesMessageEnabled = localIsMFMEnabled ?? globalIsMFMenabled;
+  const mentionNodes = useDirtyGetMentions({ ref: ref || messageInputRef }, { logger });
+  const ableMention = mentionNodes?.length < maxMentionCount;
+
+  // Operate states
+  useEffect(() => {
+    setMentionNickname('');
+    setMentionedUsers([]);
+    setMentionedUserIds([]);
+    setSelectedUser(null);
+    setMentionSuggestedUsers([]);
+    setMessageInputEvent(null);
+    setShowVoiceMessageInput(false);
+  }, [currentChannel?.url]);
+  useEffect(() => {
+    setMentionedUsers(
+      mentionedUsers.filter(({ userId }) => {
+        const i = mentionedUserIds.indexOf(userId);
+        if (i < 0) {
+          return false;
+        } else {
+          mentionedUserIds.splice(i, 1);
+          return true;
+        }
+      }),
     );
-  } else {
-    const {
-      currentGroupChannel,
-      sendMessage,
-      disabled
-    } = props as MessageInputWrapperProps & ChannelProviderInterface;
-    return (
-      <MessageInputWrapperView
-        {...props as MessageInputWrapperProps & ChannelProviderInterface}
-        currentChannel={currentGroupChannel}
-        sendUserMessage={sendMessage}
-        isDisabled={disabled}
-      />
-    );
+  }, [mentionedUserIds]);
+
+  // Callbacks
+  const handleUploadFiles = isLegacyChannel
+    ? useHandleUploadFilesLegacy({
+      sendFileMessage: sendFileMessage as SendFileMessageTypeLegacy,
+      sendMultipleFilesMessage: sendMultipleFilesMessage as SendMultipleFilesMessageTypeLegacy,
+      quoteMessage,
+    }, { logger })
+    : useHandleUploadFiles({
+      sendFileMessage: sendFileMessage as SendFileMessageType,
+      sendMultipleFilesMessage: sendMultipleFilesMessage as SendMultipleFilesMessageType,
+    }, { logger });
+
+  if (isBroadcast && !isOperator) {
+    /* Only `Operator` can send messages in the Broadcast channel */
+    // TODO: But why do we hide this? why not disabling it?
+    return null;
   }
-};
+  // other conditions
+  return (
+    <div className={`sendbird-message-input-wrapper${showVoiceMessageInput ? '--voice-message' : ''}`}>
+      {showSuggestedMentionList && (
+        <SuggestedMentionList
+          currentChannel={currentChannel}
+          targetNickname={mentionNickname}
+          inputEvent={messageInputEvent}
+          renderUserMentionItem={renderUserMentionItem}
+          onUserItemClick={(user) => {
+            if (user) {
+              setMentionedUsers([...mentionedUsers, user]);
+            }
+            setMentionNickname('');
+            setSelectedUser(user);
+            setMessageInputEvent(null);
+          }}
+          onFocusItemChange={() => {
+            setMessageInputEvent(null);
+          }}
+          onFetchUsers={(users) => {
+            setMentionSuggestedUsers(users);
+          }}
+          ableAddMention={ableMention}
+          maxMentionCount={maxMentionCount}
+          maxSuggestionCount={maxSuggestionCount}
+        />
+      )}
+      {quoteMessage && (
+        <div className="sendbird-message-input-wrapper__quote-message-input">
+          <QuoteMessageInput replyingMessage={quoteMessage} onClose={() => setQuoteMessage(null)} />
+        </div>
+      )}
+      {showVoiceMessageInput ? (
+        <VoiceMessageInputWrapper
+          channel={currentChannel}
+          onSubmitClick={(recordedFile, duration) => {
+            if (isLegacyChannel) {
+              (sendVoiceMessage as SendVoiceMessageTypeLegacy)(recordedFile, duration, quoteMessage);
+            } else {
+              (sendVoiceMessage as SendVoiceMessageType)({ file: recordedFile }, duration);
+            }
+            setQuoteMessage(null);
+            setShowVoiceMessageInput(false);
+          }}
+          onCancelClick={() => {
+            setShowVoiceMessageInput(false);
+          }}
+        />
+      ) : (
+        <MessageInput
+          className="sendbird-message-input-wrapper__message-input"
+          channel={currentChannel}
+          channelUrl={currentChannel?.url}
+          mentionSelectedUser={selectedUser}
+          isMentionEnabled={isMentionEnabled}
+          isVoiceMessageEnabled={isVoiceMessageEnabled}
+          isSelectingMultipleFilesEnabled={isMultipleFilesMessageEnabled}
+          onVoiceMessageIconClick={() => {
+            setShowVoiceMessageInput(true);
+          }}
+          setMentionedUsers={setMentionedUsers}
+          placeholder={
+            (quoteMessage && stringSet.MESSAGE_INPUT__QUOTE_REPLY__PLACE_HOLDER)
+            || (isDisabledBecauseFrozen(currentChannel) && stringSet.MESSAGE_INPUT__PLACE_HOLDER__DISABLED)
+            || (isDisabledBecauseMuted(currentChannel)
+              && (isMobile ? stringSet.MESSAGE_INPUT__PLACE_HOLDER__MUTED_SHORT : stringSet.MESSAGE_INPUT__PLACE_HOLDER__MUTED))
+          }
+          ref={ref || messageInputRef}
+          disabled={isMessageInputDisabled}
+          renderFileUploadIcon={renderFileUploadIcon}
+          renderSendMessageIcon={renderSendMessageIcon}
+          renderVoiceMessageIcon={renderVoiceMessageIcon}
+          onStartTyping={() => {
+            currentChannel?.startTyping();
+          }}
+          onSendMessage={({ message, mentionTemplate }) => {
+            sendUserMessage({
+              message,
+              mentionedUsers,
+              mentionedMessageTemplate: mentionTemplate,
+            });
+            setMentionNickname('');
+            setMentionedUsers([]);
+            setQuoteMessage(null);
+            currentChannel?.endTyping?.();
+          }}
+          onFileUpload={(fileList) => {
+            handleUploadFiles(fileList);
+            setQuoteMessage(null);
+          }}
+          onUserMentioned={(user) => {
+            if (selectedUser?.userId === user?.userId) {
+              setSelectedUser(null);
+              setMentionNickname('');
+            }
+          }}
+          onMentionStringChange={(mentionText) => {
+            setMentionNickname(mentionText);
+          }}
+          onMentionedUserIdsUpdated={(userIds) => {
+            setMentionedUserIds(userIds);
+          }}
+          onKeyDown={(e) => {
+            if (
+              showSuggestedMentionList
+              && mentionSuggestedUsers?.length > 0
+              && ((e.key === MessageInputKeys.Enter && ableMention)
+                || e.key === MessageInputKeys.ArrowUp
+                || e.key === MessageInputKeys.ArrowDown)
+            ) {
+              setMessageInputEvent(e);
+              return true;
+            }
+            return false;
+          }}
+        />
+      )}
+    </div>
+  );
+});
 
 export { VoiceMessageInputWrapper, VoiceMessageInputWrapperProps } from './VoiceMessageInputWrapper';
 
