80d2d2e7a0c8791e80fe750504577a895f874ae5
HoonBaek <hoon@sendbird.com>
2024-01-09 14:54:49 +0900

    Clean up exporting path and interface

--- src/modules/GroupChannel/context/hooks/useToggleReactionCallback.ts
+++ src/modules/GroupChannel/context/hooks/useToggleReactionCallback.ts
@@ -3,32 +3,38 @@ import { GroupChannel } from '@sendbird/chat/groupChannel';
 import { LoggerInterface } from '../../../../lib/Logger';
 import { BaseMessage } from '@sendbird/chat/message';
 
+const LOG_PRESET = 'useToggleReactionCallback:';
+
 export default function useToggleReactionCallback(
-  currentGroupChannel: GroupChannel | null,
+  currentChannel: GroupChannel | null,
   logger?: LoggerInterface,
 ) {
   return useCallback(
     (message: BaseMessage, key: string, isReacted: boolean) => {
+      if (!currentChannel) {
+        logger.warning(`${LOG_PRESET} currentChannel doesn't exist`, currentChannel);
+        return;
+      }
       if (isReacted) {
-        currentGroupChannel
+        currentChannel
           .deleteReaction(message, key)
           .then((res) => {
-            logger.info('Delete reaction success', res);
+            logger.info(`${LOG_PRESET} Delete reaction success`, res);
           })
           .catch((err) => {
-            logger.warning('Delete reaction failed', err);
+            logger.warning(`${LOG_PRESET} Delete reaction failed`, err);
           });
       } else {
-        currentGroupChannel
+        currentChannel
           .addReaction(message, key)
           .then((res) => {
-            logger.info('Add reaction success', res);
+            logger.info(`${LOG_PRESET} Add reaction success`, res);
           })
           .catch((err) => {
-            logger.warning('Add reaction failed', err);
+            logger.warning(`${LOG_PRESET} Add reaction failed`, err);
           });
       }
     },
-    [currentGroupChannel],
+    [currentChannel],
   );
 }
