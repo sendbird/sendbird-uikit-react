ca00c2c9e88d7380c11bd3ba138d40073376b7eb
bang9 <gusrn1423@naver.com>
2024-01-19 11:53:08 +0900

    chore: apply review

--- src/modules/Channel/context/hooks/useFileInfoListWithUploaded.ts
+++ src/modules/Channel/context/hooks/useFileInfoListWithUploaded.ts
@@ -4,6 +4,8 @@ import { UploadedFileInfoWithUpload } from '../../../../types';
 
 export const useFileInfoListWithUploaded = (message: CoreMessageType): UploadedFileInfoWithUpload[] => {
   if (!message || !message.isMultipleFilesMessage || !message.isMultipleFilesMessage()) return [];
+  // SendingStatus.SCHEDULED is currently not covered in UIKit
+  //  So we can ignore for now, but for future, it is better to explicitly use PENDING, FAILED, or CANCELED.
   else if (message.sendingStatus === SendingStatus.SUCCEEDED) {
     return message.fileInfoList.map((it) => ({
       ...it,
