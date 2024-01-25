597c42304d44fc421a5055d39ff66c0964a8f638
bang9 <gusrn1423@naver.com>
2024-01-25 21:33:06 +0900

    fix: do not disabled member in super/broadcast group channel

--- src/modules/ChannelSettings/components/ModerationPanel/InviteUsersModal.tsx
+++ src/modules/ChannelSettings/components/ModerationPanel/InviteUsersModal.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { User } from '@sendbird/chat';
 
 import Modal from '../../../../ui/Modal';
 import { ButtonTypes } from '../../../../ui/Button';
@@ -6,105 +7,104 @@ import UserListItem from '../../../../ui/UserListItem';
 import { useChannelSettingsContext } from '../../context/ChannelSettingsProvider';
 import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
 import { useLocalization } from '../../../../lib/LocalizationContext';
+import { useOnScrollPositionChangeDetector } from '../../../../hooks/useOnScrollReachedEndDetector';
+import { UserListQuery } from '../../../../types';
 
+type UserId = string;
 interface Props {
   onCancel(): void;
-  onSubmit(members: Array<string>): void;
+  onSubmit(userIds: UserId[]): void;
 }
 
-const isAboutSame = (a, b, px) => (Math.abs(a - b) <= px);
-
-export default function InviteUsers({
-  onCancel,
-  onSubmit,
-}: Props): ReactElement {
-  const [members, setMembers] = useState([]);
-  const [selectedMembers, setSelectedMembers] = useState({});
-  const [userQuery, setUserQuery] = useState(null);
+export default function InviteUsers({ onCancel, onSubmit }: Props) {
+  const [users, setUsers] = useState([]);
+  const [userListQuery, setUserListQuery] = useState(null);
+  const [selectedUsers, setSelectedUsers] = useState<Record<UserId, User>>({});
 
   const state = useSendbirdStateContext();
   const sdk = state?.stores?.sdkStore?.sdk;
   const globalUserListQuery = state?.config?.userListQuery;
 
-  const { channel, overrideInviteUser, queries } = useChannelSettingsContext();
-  const userFilledQuery = {
-    ...queries?.applicationUserListQuery,
-  };
+  const { channel, overrideInviteUser, queries, forceUpdateUI } = useChannelSettingsContext();
   const { stringSet } = useLocalization();
 
-  useEffect(() => {
-    const userListQuery = globalUserListQuery
-      ? globalUserListQuery()
-      : sdk?.createApplicationUserListQuery(userFilledQuery);
-    if (userListQuery?.next) {
-      userListQuery.next().then((members) => {
-        setMembers(members);
-      });
-      setUserQuery(userListQuery);
+  const onScroll = useOnScrollPositionChangeDetector({
+    async onReachedBottom() {
+      if (userListQuery?.hasNext) {
+        const users = await userListQuery.next();
+        setUsers((prev) => [...prev, ...users]);
+      }
+    },
+  });
+
+  const onInviteUsers = async () => {
+    const userIdsToInvite = Object.keys(selectedUsers);
+    if (typeof overrideInviteUser === 'function') {
+      overrideInviteUser({ users: userIdsToInvite, onClose: onCancel, channel });
+    } else {
+      await channel?.inviteWithUserIds(userIdsToInvite);
+      onSubmit(userIdsToInvite);
     }
+  };
+
+  const onSelectUser = (user: User) => {
+    setSelectedUsers(({ ...draft }) => {
+      if (draft[user.userId]) {
+        delete draft[user.userId];
+      } else {
+        draft[user.userId] = user;
+      }
+      return draft;
+    });
+  };
+
+  const membersMap = useMemo(() => {
+    // UIKit policy: In a super or broadcast channel, do not check the members when inviting users.
+    if (channel?.isSuper || channel?.isBroadcast) return { [sdk.currentUser.userId]: sdk.currentUser };
+
+    return channel?.members.reduce((acc, cur) => {
+      acc[cur.userId] = cur;
+      return acc;
+    }, {} as Record<UserId, User>);
+  }, [channel?.members.length]);
+
+  useEffect(() => {
+    const fetchUsersAndSetQuery = async () => {
+      let query: UserListQuery = globalUserListQuery?.() ?? sdk?.createApplicationUserListQuery(queries?.applicationUserListQuery);
+      if (query) {
+        const users = await query.next();
+        setUserListQuery(query);
+        setUsers(users);
+      }
+    };
+
+    fetchUsersAndSetQuery();
   }, [sdk]);
+
   return (
     <div>
       <Modal
         isFullScreenOnMobile
-        disabled={Object.keys(selectedMembers).length === 0}
+        disabled={Object.keys(selectedUsers).length === 0}
         submitText={stringSet.BUTTON__INVITE}
         type={ButtonTypes.PRIMARY}
         onCancel={() => onCancel()}
-        onSubmit={() => {
-          const members = Object.keys(selectedMembers).filter((m) => selectedMembers[m]);
-          if (typeof overrideInviteUser === 'function') {
-            overrideInviteUser({
-              users: members,
-              onClose: onCancel,
-              channel,
-            });
-            return;
-          }
-          channel?.inviteWithUserIds(members).then(() => {
-            onSubmit(members);
-          });
-        }}
+        onSubmit={onInviteUsers}
         titleText={stringSet.CHANNEL_SETTING__MEMBERS__SELECT_TITLE}
       >
-        <div
-          className="sendbird-more-members__popup-scroll"
-          onScroll={(e) => {
-            const { hasNext } = userQuery;
-            const target = e.target as HTMLTextAreaElement;
-            const fetchMore = isAboutSame(target.clientHeight + target.scrollTop, target.scrollHeight, 20);
-            if (hasNext && fetchMore) {
-              userQuery.next().then((users) => {
-                setMembers([
-                  ...members,
-                  ...users,
-                ]);
-              });
-            }
-          }}
-        >
+        <div className="sendbird-more-members__popup-scroll" onScroll={onScroll}>
           <div className="sendbird-more-members__popup-scroll__inner">
-            {members.map((member) => {
-              const isJoinedMember = channel?.members?.some(({ userId }) => userId === member?.userId);
+            {users.map((user) => {
+              const isMember = Boolean(membersMap[user.userId]);
+              const isSelected = Boolean(selectedUsers[user.userId]);
               return (
                 <UserListItem
+                  key={user.userId}
                   checkBox
-                  checked={isJoinedMember || selectedMembers[member.userId]}
-                  disabled={isJoinedMember}
-                  onChange={
-                    (event) => {
-                      const modifiedSelectedMembers = {
-                        ...selectedMembers,
-                        [event.target.id]: event.target.checked,
-                      };
-                      if (!event.target.checked) {
-                        delete modifiedSelectedMembers[event.target.id];
-                      }
-                      setSelectedMembers(modifiedSelectedMembers);
-                    }
-                  }
-                  user={member}
-                  key={member.userId}
+                  checked={isMember || isSelected}
+                  disabled={isMember}
+                  user={user}
+                  onChange={() => onSelectUser(user)}
                 />
               );
             })}
