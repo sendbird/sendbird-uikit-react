e2b787332e4497b045ccfe296b64be674108a73c
chohongm <hongman.cho@gmail.com>
2024-02-22 17:08:00 +0900

    update package

---
+++ src/modules/GroupChannel/context/hooks/temp.ts
@@ -0,0 +1,53 @@
+import {MessageTemplateData} from '../../../../ui/TemplateMessageItemBody/types';
+
+export const PersonalTemplatesDataForTesting: MessageTemplateData[] = [
+  {
+    key: 'low-balance-alert',
+    variables: {
+      'account.image': {
+        url: 'https://ddtyj01cn62am.cloudfront.net/notifications/chat-example-balance-reminder-account.png',
+        width: 120,
+        height: 120,
+      },
+      'account.name': 'Salary account',
+      'account.balance': '$12',
+    },
+  },
+  {
+    key: 'fitness-promo',
+    variables: {
+      image: {
+        url: 'https://ddtyj01cn62am.cloudfront.net/notifications/chat-example-promotion-fitness.jpg',
+        width: 984,
+        height: 543,
+      },
+      productName: 'Sendbird Fitness',
+      'button1.title': 'Lean more',
+    },
+  },
+  {
+    key: 'march-promo',
+    variables: {
+      customer_name: 'John',
+      discount_percentage: '50%',
+    },
+  },
+  {
+    key: 'socar-car-item',
+  },
+];
+
+export const SocarWarRoomTemplatesData: MessageTemplateData[] = [] = [
+  {
+    key: 't0',
+  },
+  {
+    key: 't1',
+  },
+  {
+    key: 't2',
+  },
+  {
+    key: 't3',
+  },
+];
