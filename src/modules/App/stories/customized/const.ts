495a7518761d0a26d482d9f29afb2a940dfa9d1a
Liam Hongman Cho <liam.cho@sendbird.com>
2024-02-28 14:16:26 +0900

    chore: Remove unused story (#993)

--- src/modules/App/stories/customized/const.ts
+++
@@ -1,9 +0,0 @@
-const age = 75;
-export const CustomSampleAppId = process.env.STORYBOOK_APP_ID;
-export const CustomSampleUserNames = [
-  `hoon${age}1`,
-  `hoon${age}2`,
-  `hoon${age}3`,
-  `hoon${age}4`,
-  `eunseo${age}1`,
-];
