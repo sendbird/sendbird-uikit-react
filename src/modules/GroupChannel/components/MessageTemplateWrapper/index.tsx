ed309231f5efd5a3e54b8c3681d4b4d15f507280
chohongm <hongman.cho@gmail.com>
2024-02-29 16:24:51 +0900

    apply feed

--- src/modules/GroupChannel/components/MessageTemplateWrapper/index.tsx
+++ src/modules/GroupChannel/components/MessageTemplateWrapper/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BaseMessage } from '@sendbird/chat/message';
 import MessageTemplate, { MessageTemplateProps } from '../../../../ui/MessageTemplate';
-import { MessageProvider } from '@sendbird/react-uikit-message-template-view';
+import { MessageProvider as MessageTemplateProvider } from '@sendbird/react-uikit-message-template-view';
 
 export interface MessageTemplateWrapperProps extends MessageTemplateProps {
   message: BaseMessage;
 }
 
 export const MessageTemplateWrapper = ({ message, templateItems }: MessageTemplateWrapperProps) => {
-  return <MessageProvider message={message}>
+  return <MessageTemplateProvider message={message}>
     <MessageTemplate templateItems={templateItems} />
-  </MessageProvider>;
+  </MessageTemplateProvider>;
 };
 
 export default MessageTemplateWrapper;
