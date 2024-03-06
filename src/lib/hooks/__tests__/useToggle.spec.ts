688f7ea47af601d6ce064eba373c6c0634c897ee
Chris Heo <chrisallo.h@gmail.com>
2024-03-06 11:42:25 -0800

    useToggle hook

---
+++ src/lib/hooks/__tests__/useToggle.spec.ts
@@ -0,0 +1,49 @@
+
+import { act, renderHook } from '@testing-library/react';
+import useToggle from '../useToggle';
+
+describe('useToggle', () => {
+  it('toggle()', async () => {
+    const fn = jest.fn((toggled: boolean) => {
+      expect(toggled).toBeTruthy();
+    });
+    const { result } = renderHook(() => useToggle(fn));
+    const toggle = result.current;
+    
+    act(() => { toggle(); });
+
+    await new Promise<void>(resolve => {
+      setTimeout(() => resolve(), 600);
+    });
+    expect(fn).toBeCalledTimes(1);
+  });
+  it('toggle() even times', async () => {
+    const fn = jest.fn();
+    const { result } = renderHook(() => useToggle(fn));
+    const toggle = result.current;
+    
+    act(() => { toggle(); });
+    act(() => { toggle(); });
+
+    await new Promise<void>(resolve => {
+      setTimeout(() => resolve(), 600);
+    });
+    expect(fn).not.toBeCalled();
+  });
+  it('toggle() odd times', async () => {
+    const fn = jest.fn((toggled: boolean) => {
+      expect(toggled).toBeTruthy();
+    });
+    const { result } = renderHook(() => useToggle(fn));
+    const toggle = result.current;
+    
+    act(() => { toggle(); });
+    act(() => { toggle(); });
+    act(() => { toggle(); });
+
+    await new Promise<void>(resolve => {
+      setTimeout(() => resolve(), 600);
+    });
+    expect(fn).toBeCalledTimes(1);
+  });
+});
---
+++ src/lib/hooks/useToggle.ts
@@ -0,0 +1,54 @@
+
+import { useEffect, useState } from 'react';
+
+export interface ToggleOptions {
+  initialValue?: boolean;
+  throttlingTime?: number;
+}
+
+const DEFAULT_TOGGLE_INITIAL_VALUE = false;
+const DEFAULT_TOGGLE_THROTTLING_TIME = 500;
+
+/**
+ * @description Use toggle with throttling.
+ *  [usage]
+ *  const toggle = useToggle(false, (isOn: boolean) => {
+ *    // do something with the flag.
+ *  });
+ *  ...
+ *  toggle();
+ *  toggle();
+ *  toggle(); // this 3x calls fall into one toggle handler call (throttled).
+ * 
+ *  ...
+ *  toggle();
+ *  toggle(); // this 2x calls do not call toggle handler because the value does not change eventually.
+ */
+function useToggle(
+  toggleHandler: (isOn: boolean) => any,
+  options: ToggleOptions = {},
+) {
+  const initialValue = options.initialValue ?? DEFAULT_TOGGLE_INITIAL_VALUE;
+  const [isOn, setIsOn] = useState(initialValue);
+  const [timer, setTimer] = useState<ReturnType<typeof setTimeout> | null>(null);
+  const [isFinallyOn, setIsFinallyOn] = useState(initialValue);
+
+  useEffect(() => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    setTimer(setTimeout(() => {
+      setTimer(null);
+      if (isFinallyOn !== isOn) {
+        toggleHandler(isOn);
+        setIsFinallyOn(isFinallyOn);
+      }
+    }, options.throttlingTime ?? DEFAULT_TOGGLE_THROTTLING_TIME));
+  }, [isOn]);
+
+  return () => {
+    setIsOn((prev: boolean) => !prev);
+  };
+}
+
+export default useToggle;
