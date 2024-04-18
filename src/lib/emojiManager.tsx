b69ff96c76f54207d6557393ff250bc457e10817
bang9 <gusrn1423@naver.com>
2024-04-18 17:13:17 +0900

    chore: add space

--- src/lib/emojiManager.tsx
+++ src/lib/emojiManager.tsx
@@ -24,7 +24,6 @@ export interface EmojiManagerParams {
 
 export class EmojiManager {
   private _emojiContainer: EmojiContainer;
-
   constructor(props: EmojiManagerParams) {
     const { sdk, logger } = props;
     sdk?.getAllEmoji?.()
