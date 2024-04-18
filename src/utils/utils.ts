eb8c3808484a2b6a3c5992d536f4772c7ef4dbde
bang9 <gusrn1423@naver.com>
2024-04-18 09:55:45 +0900

    fix: remove image section in og message if there is no og image

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,14 +1,10 @@
 import { SendableMessageType } from './index';
 
-export const noop = () => { /** noop * */ };
+export const noop = () => {
+  /** noop * */
+};
 export const getSenderProfileUrl = (message: SendableMessageType) => message.sender && message.sender.profileUrl;
-export const getSenderName = (message: SendableMessageType) => (
-  message.sender && (
-    message.sender.friendName
-    || message.sender.nickname
-    || message.sender.userId
-  )
-);
+export const getSenderName = (message: SendableMessageType) => message.sender && (message.sender.friendName || message.sender.nickname || message.sender.userId);
 export const isAboutSame = (a: number, b: number, px: number) => Math.abs(a - b) <= px;
 export const isMobileIOS = (userAgent: string) => {
   const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
@@ -27,6 +23,16 @@ export const deleteNullish = <T>(obj: T): T => {
   });
   return cleaned;
 };
+
+export function openURL(url?: string | null) {
+  let safeURL = url;
+  if (safeURL) {
+    if (!safeURL.startsWith('http://') && !safeURL.startsWith('https://')) {
+      safeURL = 'https://' + safeURL;
+    }
+    window.open(safeURL);
+  }
+}
 
 export default {
   getSenderName,
