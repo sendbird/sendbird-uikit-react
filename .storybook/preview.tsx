7d9d85b5e07065f1ad6ab1f3792ba8610dc9e269
HoonBaek <hoon@sendbird.com>
2024-04-23 14:39:55 +0900

    Use des in controls & Decrease version of addons

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,6 +6,7 @@ const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
