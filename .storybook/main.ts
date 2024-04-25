8a0cdd326c5751b2552c4d76d2caa9cba57a585a
bang9 <gusrn1423@naver.com>
2024-04-25 19:09:55 +0900

    chore: update main configs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import { mergeConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import path, { dirname, join } from 'path';
+import { dirname, join } from 'path';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -13,32 +13,13 @@ function getAbsolutePath(value: string): any {
 export default {
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-essentials')],
-  core: {
-    builder: getAbsolutePath('@storybook/builder-vite'),
-  },
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
-  viteFinal: async (config) => {
+  viteFinal: (config) => {
     return mergeConfig(config, {
-      css: {
-        preprocessorOptions: {
-          scss: {
-            quietDeps: true,
-            additionalData: `@import '${path.resolve(__dirname, '../src')}/styles/variables';`,
-          },
-        },
-      },
-      plugins: [
-        svgr({
-          include: '**/*.svg',
-        }),
-      ],
+      plugins: [svgr({ include: '**/*.svg' })],
     });
-  },
-  docs: {
-    autodocs: 'tag',
-    toc: true,
   },
 };
