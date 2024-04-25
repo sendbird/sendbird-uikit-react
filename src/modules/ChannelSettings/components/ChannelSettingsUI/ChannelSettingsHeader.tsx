49ce97908ce9565ea57c254eccd8328f529f31d7
HoonBaek <hoon@sendbird.com>
2024-04-25 12:34:19 +0900

    Apply Header to the ChannelSettings

--- src/modules/ChannelSettings/components/ChannelSettingsUI/ChannelSettingsHeader.tsx
+++ src/modules/ChannelSettings/components/ChannelSettingsUI/ChannelSettingsHeader.tsx
@@ -3,37 +3,44 @@ import React, { MouseEvent } from 'react';
 import { useLocalization } from '../../../../lib/LocalizationContext';
 import useSendbirdStateContext from '../../../../hooks/useSendbirdStateContext';
 
-import Label, { LabelColors, LabelTypography } from '../../../../ui/Label';
 import IconButton from '../../../../ui/IconButton';
 import Icon, { IconTypes } from '../../../../ui/Icon';
+import Header, { type HeaderCustomProps } from '../../../../ui/Header';
 
-export interface ChannelSettingsHeaderProps {
+export interface ChannelSettingsHeaderProps extends HeaderCustomProps {
   onCloseClick?: (e: MouseEvent<HTMLButtonElement>) => void;
 }
 export const ChannelSettingsHeader = ({
   onCloseClick,
+  // Header custom props
+  renderLeft,
+  renderMiddle,
+  renderRight,
 }: ChannelSettingsHeaderProps) => {
   const { stringSet } = useLocalization();
   const { config } = useSendbirdStateContext();
   const { logger } = config;
 
   return (
-    <div className="sendbird-channel-settings__header">
-      <Label type={LabelTypography.H_2} color={LabelColors.ONBACKGROUND_1}>
-        {stringSet.CHANNEL_SETTING__HEADER__TITLE}
-      </Label>
-      <div className="sendbird-channel-settings__header-icon">
-        <IconButton
-          width="32px"
-          height="32px"
-          onClick={(e) => {
-            logger.info('ChannelSettings: Click close');
-            onCloseClick(e);
-          }}
-        >
-          <Icon className="sendbird-channel-settings__close-icon" type={IconTypes.CLOSE} height="22px" width="22px" />
-        </IconButton>
-      </div>
-    </div>
-  );
+    <Header
+      className="sendbird-channel-settings__header"
+      title={stringSet.CHANNEL_SETTING__HEADER__TITLE}
+      renderLeft={renderLeft}
+      renderMiddle={renderMiddle}
+      renderRight={renderRight ?? (() => (
+        <div className="sendbird-channel-settings__header-icon">
+          <IconButton
+            width="32px"
+            height="32px"
+            onClick={(e) => {
+              logger.info('ChannelSettings: Click close');
+              onCloseClick(e);
+            }}
+          >
+            <Icon className="sendbird-channel-settings__close-icon" type={IconTypes.CLOSE} height="22px" width="22px" />
+          </IconButton>
+        </div>
+      ))}
+    />
+  )
 };
