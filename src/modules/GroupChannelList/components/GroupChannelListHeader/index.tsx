c1716723c3f1acd4aa9210a0e44e392c37baa72f
HoonBaek <hoon@sendbird.com>
2024-05-02 10:45:00 +0900

    reduce meanless component rendering

--- src/modules/GroupChannelList/components/GroupChannelListHeader/index.tsx
+++ src/modules/GroupChannelList/components/GroupChannelListHeader/index.tsx
@@ -35,44 +35,46 @@ export const GroupChannelListHeader = ({
     <Header
       className={`sendbird-channel-header ${allowProfileEdit ? 'sendbird-channel-header--allow-edit' : ''}`}
       renderLeft={renderLeft}
-      renderMiddle={renderProfile?.() ? renderProfile : (() => (
-        <div
-          className="sendbird-channel-header__title"
-          role="button"
-          onClick={() => {
-            onEdit?.();
-          }}
-          onKeyDown={() => {
-            onEdit?.();
-          }}
-          tabIndex={0}
-        >
-          <div className="sendbird-channel-header__title__left">
-            <Avatar
-              width="32px"
-              height="32px"
-              src={user.profileUrl}
-              alt={user.nickname}
-            />
+      renderMiddle={() => (
+        renderProfile?.() ?? (
+          <div
+            className="sendbird-channel-header__title"
+            role="button"
+            onClick={() => {
+              onEdit?.();
+            }}
+            onKeyDown={() => {
+              onEdit?.();
+            }}
+            tabIndex={0}
+          >
+            <div className="sendbird-channel-header__title__left">
+              <Avatar
+                width="32px"
+                height="32px"
+                src={user.profileUrl}
+                alt={user.nickname}
+              />
+            </div>
+            <div className="sendbird-channel-header__title__right">
+              <Label
+                className="sendbird-channel-header__title__right__name"
+                type={LabelTypography.SUBTITLE_2}
+                color={LabelColors.ONBACKGROUND_1}
+              >
+                {user.nickname || stringSet.NO_NAME}
+              </Label>
+              <Label
+                className="sendbird-channel-header__title__right__user-id"
+                type={LabelTypography.BODY_2}
+                color={LabelColors.ONBACKGROUND_2}
+              >
+                {user.userId}
+              </Label>
+            </div>
           </div>
-          <div className="sendbird-channel-header__title__right">
-            <Label
-              className="sendbird-channel-header__title__right__name"
-              type={LabelTypography.SUBTITLE_2}
-              color={LabelColors.ONBACKGROUND_1}
-            >
-              {user.nickname || stringSet.NO_NAME}
-            </Label>
-            <Label
-              className="sendbird-channel-header__title__right__user-id"
-              type={LabelTypography.BODY_2}
-              color={LabelColors.ONBACKGROUND_2}
-            >
-              {user.userId}
-            </Label>
-          </div>
-        </div>
-      ))}
+        )
+      )}
       renderRight={renderRight ?? renderIconButton}
     />
   );
