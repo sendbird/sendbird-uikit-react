403b65496fc84134ceba323f7657eaab3208ddd6
HoonBaek <hoon@sendbird.com>
2024-05-03 17:45:44 +0900

    fix debug to all

--- apps/testing/src/App.tsx
+++ apps/testing/src/App.tsx
@@ -10,7 +10,7 @@ const defaultProps: InitialParams = {
   appId: import.meta.env.VITE_APP_ID,
   userId: import.meta.env.VITE_USER_ID ?? 'test',
   accessToken: import.meta.env.VITE_ACCESS_TOKEN,
-  config: { logLevel: 'debug' },
+  config: { logLevel: 'all' },
 };
 
 function GroupChannelPage() {
