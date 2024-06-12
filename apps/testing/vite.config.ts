4cc36e3c5535bf52a2fe0250e6c76b511cf9fb0a
Irene Ryu <irene.ryu@sendbird.com>
2024-06-12 18:11:28 +0900

    Add postcss config to test app config

--- apps/testing/vite.config.ts
+++ apps/testing/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vitePluginSvgr from 'vite-plugin-svgr';
+import postcssRtl from "postcss-rtlcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginSvgr({ include: '**/*.svg' })],
+  css: {
+    postcss: {
+      plugins: [postcssRtl({ mode: 'override' })],
+    },
+  },
 });
