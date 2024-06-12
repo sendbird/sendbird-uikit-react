a910cdd833a46b0063816012f32bf061f602d8bc
HoonBaek <hoon@sendbird.com>
2024-06-12 13:45:23 +0900

    Devide MenuItems component from the MobileBottomSheet menu

--- src/ui/MobileMenu/MobileContextMenu.tsx
+++ src/ui/MobileMenu/MobileContextMenu.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import type { BaseMenuProps } from './types';
 import type { FileMessage, UserMessage } from '@sendbird/chat/message';
+import { GroupChannel } from '@sendbird/chat/groupChannel';
 
-import ContextMenu, { MenuItems, MenuItem } from '../ContextMenu';
+import ContextMenu, { MenuItems } from '../ContextMenu';
+import { MenuItem, PrebuildMenuItemPropsType } from '../MessageMenu/items';
 
 import {
   isFailedMessage,
@@ -17,7 +19,6 @@ import {
 import { useLocalization } from '../../lib/LocalizationContext';
 import Icon, { IconTypes, IconColors } from '../Icon';
 import Label, { LabelColors, LabelTypography } from '../Label';
-import { GroupChannel } from '@sendbird/chat/groupChannel';
 
 const MobileContextMenu: React.FunctionComponent<BaseMenuProps> = (props: BaseMenuProps): React.ReactElement => {
   const {
@@ -36,22 +37,18 @@ const MobileContextMenu: React.FunctionComponent<BaseMenuProps> = (props: BaseMe
     onReplyInThread,
     isOpenedFromThread = false,
     onDownloadClick = null,
+    renderMenuItems,
   } = props;
   const isByMe = message?.sender?.userId === userId;
   const { stringSet } = useLocalization();
+
+  // Menu Items condition
   const showMenuItemCopy: boolean = isUserMessage(message as UserMessage);
   const showMenuItemEdit: boolean = (isUserMessage(message as UserMessage) && isSentMessage(message) && isByMe);
   const showMenuItemResend: boolean = (isFailedMessage(message) && message?.isResendable && isByMe);
-
   const showMenuItemDelete: boolean = !isPendingMessage(message) && isByMe;
   const showMenuItemDeleteByState = isByMe && (deleteMenuState === undefined || deleteMenuState !== 'HIDE');
   const showMenuItemDeleteFinal = showMenuItemDeleteByState && showMenuItemDelete;
-
-  const disableDelete = (
-    (deleteMenuState !== undefined && deleteMenuState === 'DISABLE')
-    || (message?.threadInfo?.replyCount ?? 0) > 0
-  );
-
   const showMenuItemDownload: boolean = !isPendingMessage(message) && isFileMessage(message)
     && !(isVoiceMessage(message) && ((channel as GroupChannel)?.isSuper || (channel as GroupChannel)?.isBroadcast));
   const showMenuItemReply: boolean = (replyType === 'QUOTE_REPLY')
@@ -63,6 +60,228 @@ const MobileContextMenu: React.FunctionComponent<BaseMenuProps> = (props: BaseMe
     && !isPendingMessage(message)
     && !isThreadMessage(message)
     && channel?.isGroupChannel();
+  const disableDelete = (
+    (deleteMenuState !== undefined && deleteMenuState === 'DISABLE')
+    || (message?.threadInfo?.replyCount ?? 0) > 0
+  );
+
+  const Copy = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        hideMenu();
+        copyToClipboard((message as UserMessage)?.message);
+      }}
+    >
+      {props?.children ?? (
+        <>
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={LabelColors.ONBACKGROUND_1}
+          >
+            {stringSet?.MESSAGE_MENU__COPY}
+          </Label>
+          <Icon
+            type={IconTypes.COPY}
+            fillColor={IconColors.PRIMARY}
+            width="24px"
+            height="24px"
+          />
+        </>
+      )}
+    </MenuItem>
+  );
+  const Reply = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        hideMenu();
+        setQuoteMessage?.(message);
+      }}
+    >
+      {props?.children ?? (
+        <>
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={
+              (message?.parentMessageId ?? 0) > 0
+                ? LabelColors.ONBACKGROUND_4
+                : LabelColors.ONBACKGROUND_1
+            }
+          >
+            {stringSet.MESSAGE_MENU__REPLY}
+          </Label>
+          <Icon
+            type={IconTypes.REPLY}
+            fillColor={
+              (message?.parentMessageId ?? 0) > 0
+                ? IconColors.ON_BACKGROUND_4
+                : IconColors.PRIMARY
+            }
+            width="24px"
+            height="24px"
+          />
+        </>
+      )}
+    </MenuItem>
+  );
+  const Thread = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        hideMenu();
+        onReplyInThread?.({ message });
+      }}
+    >
+      {props?.children ?? (
+        <>
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={LabelColors.ONBACKGROUND_1}
+          >
+            {stringSet.MESSAGE_MENU__THREAD}
+          </Label>
+          <Icon
+            type={IconTypes.THREAD}
+            fillColor={IconColors.PRIMARY}
+            width="24px"
+            height="24px"
+          />
+        </>
+      )}
+    </MenuItem>
+  );
+  const Edit = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        hideMenu();
+        showEdit?.(true);
+      }}
+    >
+      {props?.children ?? (
+        <>
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={LabelColors.ONBACKGROUND_1}
+          >
+            {stringSet.MESSAGE_MENU__EDIT}
+          </Label>
+          <Icon
+            type={IconTypes.EDIT}
+            fillColor={IconColors.PRIMARY}
+            width="24px"
+            height="24px"
+          />
+        </>
+      )}
+    </MenuItem>
+  );
+  const Resend = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        hideMenu();
+        resendMessage?.(message);
+      }}
+    >
+      {props?.children ?? (
+        <>
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={LabelColors.ONBACKGROUND_1}
+          >
+            {stringSet.MESSAGE_MENU__RESEND}
+          </Label>
+          <Icon
+            type={IconTypes.REFRESH}
+            fillColor={IconColors.PRIMARY}
+            width="24px"
+            height="24px"
+          />
+        </>
+      )}
+    </MenuItem>
+  );
+  const Delete = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        if (isFailedMessage(message)) {
+          hideMenu();
+          deleteMessage?.(message);
+        } else if (!disableDelete) {
+          hideMenu();
+          showRemove?.(true);
+        }
+      }}
+    >
+      {props?.children ?? (
+        <>
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={
+              disableDelete
+                ? LabelColors.ONBACKGROUND_4
+                : LabelColors.ONBACKGROUND_1
+            }
+          >
+            {stringSet.MESSAGE_MENU__DELETE}
+          </Label>
+          <Icon
+            type={IconTypes.DELETE}
+            fillColor={
+              disableDelete
+                ? IconColors.ON_BACKGROUND_4
+                : IconColors.PRIMARY
+            }
+            width="24px"
+            height="24px"
+          />
+        </>
+      )}
+    </MenuItem>
+  );
+  const Download = (props: PrebuildMenuItemPropsType) => (
+    <MenuItem
+      {...props}
+      onClick={() => {
+        hideMenu();
+      }}
+    >
+      {props?.children ?? (
+        <a
+          className="sendbird-message__contextmenu--hyperlink"
+          rel="noopener noreferrer"
+          href={fileMessage?.url}
+          target="_blank"
+          onClick={onDownloadClick}
+        >
+          <Label
+            type={LabelTypography.SUBTITLE_1}
+            color={LabelColors.ONBACKGROUND_1}
+          >
+            {stringSet.MESSAGE_MENU__SAVE}
+          </Label>
+          <Icon
+            type={IconTypes.DOWNLOAD}
+            fillColor={IconColors.PRIMARY}
+            width="24px"
+            height="24px"
+          />
+        </a>
+      )}
+    </MenuItem>
+  );
+  const prebuildItems = {
+    Copy,
+    Reply,
+    Thread,
+    Edit,
+    Resend,
+    Delete,
+    Download,
+  };
 
   const fileMessage = message as FileMessage;
   return (
@@ -75,197 +294,19 @@ const MobileContextMenu: React.FunctionComponent<BaseMenuProps> = (props: BaseMe
           parentContainRef={parentRef}
           closeDropdown={hideMenu}
         >
-          {showMenuItemCopy && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-copy"
-              onClick={() => {
-                hideMenu();
-                copyToClipboard((message as UserMessage)?.message);
-              }}
-              testID="ui_mobile_message_item_menu_copy"
-            >
-              <Label
-                type={LabelTypography.SUBTITLE_1}
-                color={LabelColors.ONBACKGROUND_1}
-              >
-                {stringSet?.MESSAGE_MENU__COPY}
-              </Label>
-              <Icon
-                type={IconTypes.COPY}
-                fillColor={IconColors.PRIMARY}
-                width="24px"
-                height="24px"
-              />
-            </MenuItem>
-          )}
-          {showMenuItemReply && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-reply"
-              onClick={() => {
-                hideMenu();
-                setQuoteMessage?.(message);
-              }}
-              disable={(message?.parentMessageId ?? 0) > 0}
-              testID="ui_mobile_message_item_menu_reply"
-            >
-              <Label
-                type={LabelTypography.SUBTITLE_1}
-                color={
-                  (message?.parentMessageId ?? 0) > 0
-                    ? LabelColors.ONBACKGROUND_4
-                    : LabelColors.ONBACKGROUND_1
-                }
-              >
-                {stringSet.MESSAGE_MENU__REPLY}
-              </Label>
-              <Icon
-                type={IconTypes.REPLY}
-                fillColor={
-                  (message?.parentMessageId ?? 0) > 0
-                    ? IconColors.ON_BACKGROUND_4
-                    : IconColors.PRIMARY
-                }
-                width="24px"
-                height="24px"
-              />
-            </MenuItem>
-          )}
-          {showMenuItemThread && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-reply"
-              onClick={() => {
-                hideMenu();
-                onReplyInThread?.({ message });
-              }}
-              testID="ui_mobile_message_item_menu_thread"
-            >
-              <Label
-                type={LabelTypography.SUBTITLE_1}
-                color={LabelColors.ONBACKGROUND_1}
-              >
-                {stringSet.MESSAGE_MENU__THREAD}
-              </Label>
-              <Icon
-                type={IconTypes.THREAD}
-                fillColor={IconColors.PRIMARY}
-                width="24px"
-                height="24px"
-              />
-            </MenuItem>
-          )}
-          {showMenuItemEdit && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-edit"
-              onClick={() => {
-                hideMenu();
-                showEdit?.(true);
-              }}
-              testID="ui_mobile_message_item_menu_edit"
-            >
-              <Label
-                type={LabelTypography.SUBTITLE_1}
-                color={LabelColors.ONBACKGROUND_1}
-              >
-                {stringSet.MESSAGE_MENU__EDIT}
-              </Label>
-              <Icon
-                type={IconTypes.EDIT}
-                fillColor={IconColors.PRIMARY}
-                width="24px"
-                height="24px"
-              />
-            </MenuItem>
-          )}
-          {showMenuItemResend && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-resend"
-              onClick={() => {
-                hideMenu();
-                resendMessage?.(message);
-              }}
-              testID="ui_mobile_message_item_menu_resend"
-            >
-              <Label
-                type={LabelTypography.SUBTITLE_1}
-                color={LabelColors.ONBACKGROUND_1}
-              >
-                {stringSet.MESSAGE_MENU__RESEND}
-              </Label>
-              <Icon
-                type={IconTypes.REFRESH}
-                fillColor={IconColors.PRIMARY}
-                width="24px"
-                height="24px"
-              />
-            </MenuItem>
-          )}
-          {showMenuItemDeleteFinal && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-delete"
-              onClick={() => {
-                if (isFailedMessage(message)) {
-                  hideMenu();
-                  deleteMessage?.(message);
-                } else if (!disableDelete) {
-                  hideMenu();
-                  showRemove?.(true);
-                }
-              }}
-              disable={disableDelete}
-              testID="ui_mobile_message_item_menu_delete"
-            >
-              <Label
-                type={LabelTypography.SUBTITLE_1}
-                color={
-                  disableDelete
-                    ? LabelColors.ONBACKGROUND_4
-                    : LabelColors.ONBACKGROUND_1
-                }
-              >
-                {stringSet.MESSAGE_MENU__DELETE}
-              </Label>
-              <Icon
-                type={IconTypes.DELETE}
-                fillColor={
-                  disableDelete
-                    ? IconColors.ON_BACKGROUND_4
-                    : IconColors.PRIMARY
-                }
-                width="24px"
-                height="24px"
-              />
-            </MenuItem>
-          )}
-          {showMenuItemDownload && (
-            <MenuItem
-              className="sendbird-message__mobile-context-menu-item menu-item-save"
-              onClick={() => {
-                hideMenu();
-              }}
-              testID="ui_mobile_message_item_menu_download_file"
-            >
-              <a
-                className="sendbird-message__contextmenu--hyperlink"
-                rel="noopener noreferrer"
-                href={fileMessage?.url}
-                target="_blank"
-                onClick={onDownloadClick}
-              >
-                <Label
-                  type={LabelTypography.SUBTITLE_1}
-                  color={LabelColors.ONBACKGROUND_1}
-                >
-                  {stringSet.MESSAGE_MENU__SAVE}
-                </Label>
-                <Icon
-                  type={IconTypes.DOWNLOAD}
-                  fillColor={IconColors.PRIMARY}
-                  width="24px"
-                  height="24px"
-                />
-              </a>
-            </MenuItem>
-          )}
+          {
+            renderMenuItems?.({ close: hideMenu, prebuildItems }) ?? (
+              <>
+                {showMenuItemCopy && (<Copy />)}
+                {showMenuItemReply && (<Reply />)}
+                {showMenuItemThread && (<Thread />)}
+                {showMenuItemEdit && (<Edit />)}
+                {showMenuItemResend && (<Resend />)}
+                {showMenuItemDeleteFinal && (<Delete />)}
+                {showMenuItemDownload && (<Download />)}
+              </>
+            )
+          }
         </MenuItems>
       )}
     />
