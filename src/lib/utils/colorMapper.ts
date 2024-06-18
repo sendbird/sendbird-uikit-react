715c752fcabc176ee9caf4bc7930758e92d6af28
Irene Ryu <irene.ryu@sendbird.com>
2024-06-18 12:51:36 +0900

    Address @bang9's comment

--- src/lib/utils/colorMapper.ts
+++ src/lib/utils/colorMapper.ts
@@ -41,6 +41,35 @@ const colorMappingMap = new Map<string, string>(
   Object.entries(colorMapping).map(([key, value]) => [value, key]),
 );
 
+/**
+ * Converts descriptive color keys to their numeric equivalents.
+ *
+ * This function takes a set of CSS variable keys and transforms
+ * descriptive color keys (e.g. 'extra-light', 'main') into their
+ * numeric equivalents(e.g. 100, 200, ..., 500) as defined in the colorMapping object.
+ *
+ * e.g.
+ * Input:
+ * {
+ *   '--sendbird-light-primary-extra-dark': '#00487c',
+ *   '--sendbird-light-primary-dark': '#4bb3fd',
+ *   '--sendbird-light-primary-main': '#3e6680',
+ *   '--sendbird-light-primary-light': '#0496ff',
+ *   '--sendbird-light-primary-extra-light': '#027bce',
+ * }
+ *
+ * Output:
+ * {
+ *   '--sendbird-light-primary-500': '#00487c',
+ *   '--sendbird-light-primary-400': '#4bb3fd',
+ *   '--sendbird-light-primary-300': '#3e6680',
+ *   '--sendbird-light-primary-200': '#0496ff',
+ *   '--sendbird-light-primary-100': '#027bce',
+ * }
+ *
+ * @param {ColorSet} colorSet - The input object containing CSS variables with descriptive keys.
+ * @returns {ColorSet} The transformed object with numeric keys.
+ */
 export const mapColorKeys = (colorSet: ColorSet): ColorSet => {
   const mappedColors: ColorSet = {};
 
