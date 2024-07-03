b18164840fa700648aad697652efb7461606c0af
HoonBaek <hoon@sendbird.com>
2024-07-03 19:39:33 +0900

    Export default UserListItemMenu

--- src/ui/UserListItemMenu/index.tsx
+++ src/ui/UserListItemMenu/index.tsx
@@ -1,2 +1,5 @@
+import UserListItemMenu from './UserListItemMenu';
+
 export { UserListItemMenu } from './UserListItemMenu';
 export { UserListItemMenuProvider, useUserListItemMenuContext } from './context';
+export default UserListItemMenu;
