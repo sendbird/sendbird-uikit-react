ff2ffcaa58a7bf2503437c9582896b24164dec50
Liam Cho <liam.cho@sendbird.com>
2024-07-11 14:50:10 +0900

    fix

--- apps/testing/src/pages/GroupChannelPage.tsx
+++ apps/testing/src/pages/GroupChannelPage.tsx
@@ -4,5 +4,14 @@ import { defaultProps } from '../libs/const.ts';
 
 export function GroupChannelPage() {
   const props = useConfigParams(defaultProps);
-  return <GroupChannelApp {...props} breakpoint={/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)} config={{ logLevel: 'all' }} />;
+  return <GroupChannelApp
+    {...props}
+    breakpoint={/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)}
+    config={{ logLevel: 'all' }}
+    uikitOptions={{
+      groupChannel: {
+        enableFormTypeMessage: true,
+      }
+    }}
+  />;
 }
