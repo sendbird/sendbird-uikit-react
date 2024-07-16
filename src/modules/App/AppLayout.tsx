8ae81afb77f4a1f3a982b40143d83a73c2ca9ff2
Irene Ryu <irene.ryu@sendbird.com>
2024-07-16 23:55:28 +0900

    feat: address RTL UI feedback

--- src/modules/App/AppLayout.tsx
+++ src/modules/App/AppLayout.tsx
@@ -9,7 +9,6 @@ import { MobileLayout } from './MobileLayout';
 import useSendbirdStateContext from '../../hooks/useSendbirdStateContext';
 import { SendableMessageType } from '../../utils';
 import { getCaseResolvedReplyType } from '../../lib/utils/resolvedReplyType';
-import useApplyTextDirection from './hooks/useApplyTextDirection';
 
 export const AppLayout = (props: AppLayoutProps) => {
   const {
@@ -20,7 +19,6 @@ export const AppLayout = (props: AppLayoutProps) => {
     currentChannel,
     setCurrentChannel,
     enableLegacyChannelModules,
-    htmlTextDirection,
   } = props;
 
   const globalStore = useSendbirdStateContext();
@@ -33,8 +31,6 @@ export const AppLayout = (props: AppLayoutProps) => {
   const [highlightedMessage, setHighlightedMessage] = useState<number | null>(null);
   const [startingPoint, setStartingPoint] = useState<number | null>(null);
   const { isMobile } = useMediaQueryContext();
-
-  useApplyTextDirection(htmlTextDirection);
 
   /**
    * Below configs can be set via Dashboard UIKit config setting but as a lower priority than App props.
