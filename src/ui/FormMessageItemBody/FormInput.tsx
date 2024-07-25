6047dcd59b2ac53e010f902bce550010d786c730
Liam Hongman Cho <liam.cho@sendbird.com>
2024-07-25 14:49:59 +0900

    Update src/ui/FormMessageItemBody/FormInput.tsx
    Co-authored-by: Hyungu Kang | Airen <gusrn1423@naver.com>

--- src/ui/FormMessageItemBody/FormInput.tsx
+++ src/ui/FormMessageItemBody/FormInput.tsx
@@ -184,7 +184,8 @@ const FormInput = (props: InputProps) => {
                     <>
                       <textarea
                         className={classnames(
-                          `sendbird-input__input ${!!errorMessage && 'error'}`,
+                          'sendbird-input__input',
+                          !!errorMessage && 'error'
                           'sendbird-form-message__input__textarea',
                         )}
                         required={required}
