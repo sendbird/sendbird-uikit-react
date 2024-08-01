52397fd40e6d71fb54851ed276da8c1d950cafa3
Irene Ryu <irene.ryu@sendbird.com>
2024-08-01 14:00:56 +0900

    address feedback

--- postcssRtlOptions.mjs
+++ postcssRtlOptions.mjs
@@ -1,15 +1,17 @@
+const CLASS_NAMES_TO_CHECK = [
+  // Normal message list
+  'sendbird-message-content',
+  // Thread message list
+  'sendbird-thread-list-item-content',
+  'sendbird-parent-message-info'
+];
+
 export default {
   prefixSelectorTransformer: (prefix, selector) => {
     // To increase specificity https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity 
     // for certain classnames within .sendbird-conversation__messages context.
     // This only applies when the root dir is set as 'rtl' but forceLeftToRightMessageLayout is true.
-    if ([
-      // Normal message list
-      'sendbird-message-content',
-      // Thread message list
-      'sendbird-thread-list-item-content',
-      'sendbird-parent-message-info'
-    ].some(cls => selector.includes(cls))) {
+    if (CLASS_NAMES_TO_CHECK.some(cls => selector.includes(cls))) {
       return `.sendbird-conversation__messages${prefix} ${selector}`;
     }
     return `${prefix} ${selector}`;
