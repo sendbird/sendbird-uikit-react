38e0f9d9f100cb8487059dd224cb7ce19b53cf00
Liam Cho <liam.cho@sendbird.com>
2024-08-29 12:01:50 +0900

    remove test codes

--- apps/testing/src/libs/const.ts
+++ apps/testing/src/libs/const.ts
@@ -1,14 +1,7 @@
 import { InitialParams } from '../utils/paramsBuilder.ts';
 
 export const defaultProps: InitialParams = {
-  // appId: import.meta.env.VITE_APP_ID,
-  // userId: import.meta.env.VITE_USER_ID ?? 'test',
-  // accessToken: import.meta.env.VITE_ACCESS_TOKEN,
-  appId: '5D27A98C-D935-4EDA-846A-BCCD90E8E55B',
-  userId: 'test',
-  // appId: '5F533BB6-D6BB-4C90-A429-B9DAEDBEAD54',
-  // userId: 'js_form_v2_test_user',
-  // accessToken: '507fef71b8f746b35c9da4a1f513cea49ae7d982',
-  // customApiHost: 'https://api-no2.sendbirdtest.com',
-  // customWebSocketHost: 'wss://ws-no2.sendbirdtest.com',
+  appId: import.meta.env.VITE_APP_ID,
+  userId: import.meta.env.VITE_USER_ID ?? 'test',
+  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 };
--- apps/testing/src/pages/GroupChannelPage.tsx
+++ apps/testing/src/pages/GroupChannelPage.tsx
@@ -4,16 +4,5 @@ import { defaultProps } from '../libs/const.ts';
 
 export function GroupChannelPage() {
   const props = useConfigParams(defaultProps);
-  return <GroupChannelApp
-    {...props}
-    breakpoint={/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)}
-    config={{ logLevel: 'all' }}
-    uikitOptions={{
-      groupChannel: {
-        enableFormTypeMessage: true,
-        enableSuggestedReplies: true,
-        enableFeedback: true,
-      }
-    }}
-  />;
+  return <GroupChannelApp {...props} breakpoint={/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)} config={{ logLevel: 'all' }} />;
 }
