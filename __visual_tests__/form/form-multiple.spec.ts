469bc93711b3306b4831aafa5a598cd12ad84e1f
Liam Cho <liam.cho@sendbird.com>
2024-10-11 17:19:02 +0900

    fix

--- __visual_tests__/form/form-multiple.spec.ts
+++ __visual_tests__/form/form-multiple.spec.ts
@@ -1,6 +1,6 @@
-import { Page, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
-import { assertScreenshot, fillForm, runBeforeEach } from '../utils';
+import { assertScreenshot, fillForm, runBeforeEach, testDraftValues } from '../utils';
 import { MockMessageFormProps } from '../__fixtures__/form/interface';
 
 runBeforeEach();
@@ -163,45 +163,3 @@ test('204', async ({ page }) => {
   await testDraftValues(page, form);
   await assertScreenshot(page);
 });
-
-async function testDraftValues(page: Page, form: MockMessageFormProps, clickSubmit = false) {
-  let numPriorChips = 0;
-  await Promise.all(form.items.map(async (item, index) => {
-    if (item.draft_values && item.draft_values.length > 0) {
-      const layout: string = item.style['layout'];
-      if (layout === 'chip') {
-        await fillDraftValue(page, layout, numPriorChips++, item.draft_values);
-      } else {
-        await fillDraftValue(page, layout, index - numPriorChips, item.draft_values);
-      }
-    }
-  }));
-  if (clickSubmit) {
-    const submitButton = page.locator('.sendbird-form-message__submit-button');
-    await submitButton.click();
-  }
-}
-
-async function fillDraftValue(page: Page, layout: string, index: number, values: string[]) {
-  switch (layout) {
-    case 'text':
-    case 'textarea':
-    case 'number':
-    case 'phone':
-    case 'email': {
-      const inputField = page.locator('.sendbird-input__input').nth(index);
-      await inputField.fill(values[0]);
-      break;
-    }
-    case 'chip': {
-      const chipRoot = page.locator('.sendbird-form-message__input__chip-container').nth(index);
-      await Promise.all(values.map(async (value) => {
-        // Locate the child element whose child has the specific value.
-        const targetChild = chipRoot.locator('> *').locator(':scope', { hasText: value });
-        await targetChild.click();
-      }));
-      break;
-    }
-    default:
-  }
-}
--- __visual_tests__/utils.ts
+++ __visual_tests__/utils.ts
@@ -1,4 +1,5 @@
-import { Page, expect, test } from '@playwright/test';
+import { Page, expect, test, Locator } from '@playwright/test';
+import { MockMessageFormProps } from './__fixtures__/form/interface';
 
 const selectors = {
   input: 'form-input',
@@ -18,4 +19,54 @@ export function runBeforeEach() {
     await page.goto('http://localhost:5273/');
     await page.getByTestId(selectors.input).click();
   });
+}
+
+export async function testDraftValues(page: Page, form: MockMessageFormProps, clickSubmit = false) {
+  let numPriorChips = 0;
+  await Promise.all(form.items.map(async (item, index) => {
+    if (item.draft_values && item.draft_values.length > 0) {
+      const layout: string = item.style['layout'];
+      if (layout === 'chip') {
+        await fillDraftValue(page, layout, numPriorChips++, item.draft_values);
+      } else {
+        await fillDraftValue(page, layout, index - numPriorChips, item.draft_values);
+      }
+    }
+  }));
+  if (clickSubmit) {
+    await submitForm(page);
+  }
+}
+
+async function submitForm(page: Page) {
+  const submitButton = page.locator('.sendbird-form-message__submit-button');
+  await submitButton.click();
+}
+
+async function fillDraftValue(page: Page, layout: string, index: number, values: string[]) {
+  switch (layout) {
+    case 'text':
+    case 'textarea':
+    case 'number':
+    case 'phone':
+    case 'email': {
+      const inputField = page.locator('.sendbird-input__input').nth(index);
+      await inputField.fill(values[0]);
+      break;
+    }
+    case 'chip': {
+      const chipRoot = page.locator('.sendbird-form-message__input__chip-container').nth(index);
+      await toggleChips(page, chipRoot, values);
+      break;
+    }
+    default:
+  }
+}
+
+async function toggleChips(page: Page, chipContainer: Locator, chipsToToggle: string[]) {
+  await Promise.all(chipsToToggle.map(async (value) => {
+    // Locate the child element whose child has the specific value.
+    const targetChild = chipContainer.locator('> *').locator(':scope', { hasText: value });
+    await targetChild.click();
+  }));
 }
