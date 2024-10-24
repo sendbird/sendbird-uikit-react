2fb0d7ca5a4f19619d630461973dfbea8c7a7bb7
Liam Hongman Cho <liam.cho@sendbird.com>
2024-10-24 14:33:10 +0900

    test: Snapshot test for form message (#1223)
    Fixes [NOTI-3489](https://sendbird.atlassian.net/browse/NOTI-3489)
    
    ### Test sheet
    
    https://docs.google.com/spreadsheets/d/16lv6-77jS00_8tJOPHLcB-7wgON4miFw-BfvUJFyUE4/edit?gid=0#gid=0
    
    ### How to run snapshot test
    
    `yarn test:visual`
    
    ### Android results
    
    https://github.com/sendbird/uikit-android/blob/develop/uikit/src/test/snapshots/images/com.sendbird.uikit_MessageFormSnapshotTest_test%20snapshot%20for%20message%20form_form-204.png
    
    ### Changelogs
    
    [NOTI-3489]:
    https://sendbird.atlassian.net/browse/NOTI-3489?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ apps/visual-test/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vitePluginSvgr from 'vite-plugin-svgr';
+import postcssRtl from "postcss-rtlcss";
+// @ts-ignore
+import postcssRtlOptions from '../../postcssRtlOptions.mjs';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), vitePluginSvgr({ include: '**/*.svg' })],
+  css: {
+    postcss: {
+      plugins: [postcssRtl(postcssRtlOptions)],
+    },
+  },
+  server: {
+    port: 5273,
+  },
+});
