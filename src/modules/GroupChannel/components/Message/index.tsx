52d3d5d0d4aaec6ddf5c3b459abf9f75f3dee0d6
Liam Cho <liam.cho@sendbird.com>
2024-10-24 14:32:30 +0900

    fix

--- src/modules/GroupChannel/components/Message/index.tsx
+++ src/modules/GroupChannel/components/Message/index.tsx
@@ -45,10 +45,9 @@ export const Message = (props: MessageProps): React.ReactElement => {
 
     const lastMessageInView = messages[messages.length - 1];
     const hasUnsentMessage = isSendableMessage(lastMessageInView) && lastMessageInView.sendingStatus !== 'succeeded';
-    const sentMessages = messages.filter((message) => message.messageId > 0);
     const showSuggestedReplies = showSuggestedRepliesFor === 'all_messages'
       ? true
-      : message.messageId === sentMessages[sentMessages.length - 1].messageId;
+      : message.messageId === lastMessageInView.messageId;
     return enableSuggestedReplies && getSuggestedReplies(message).length > 0 && !hasUnsentMessage && showSuggestedReplies;
   });
 
