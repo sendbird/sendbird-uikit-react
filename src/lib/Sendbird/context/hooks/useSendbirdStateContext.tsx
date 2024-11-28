4d41d1ac706164e8c5f0e0742e25031966caa659
HoonBaek <hoon@sendbird.com>
2024-11-28 17:19:07 +0900

    left comment for useSendbirdStateContext

--- src/lib/Sendbird/context/hooks/useSendbirdStateContext.tsx
+++ src/lib/Sendbird/context/hooks/useSendbirdStateContext.tsx
@@ -1,6 +1,12 @@
 import { SendbirdState } from "../../types";
 import useSendbird from "./useSendbird";
 
+/**
+ * This hook is exported to support backward compatibility. Do not use this hook internally.
+ */
+/**
+ * @deprecated This hook has been deprecated, please use `useSendbird` instead.
+ */
 export function useSendbirdStateContext(): SendbirdState {
   const { state, actions } = useSendbird();
   return { ...state, ...actions };
