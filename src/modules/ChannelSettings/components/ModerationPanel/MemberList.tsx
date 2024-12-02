99aa96c5a7136a5c611d13a495fdb5635f97033e
Baek EunSeo <hoon@sendbird.com>
2024-12-02 12:03:43 +0900

    fix: [main branch] Broken UI - message input & settings menu (#1265)
    [CLNP-5910](https://sendbird.atlassian.net/browse/CLNP-5910)
    [CLNP-5909](https://sendbird.atlassian.net/browse/CLNP-5909)
    
    * fixed broken UI on message input & channel settings operator menu

--- src/modules/ChannelSettings/components/ModerationPanel/MemberList.tsx
+++ src/modules/ChannelSettings/components/ModerationPanel/MemberList.tsx
@@ -37,6 +37,7 @@ export const MemberList = ({
   } = useChannelSettingsContext();
   const { stringSet } = useContext(LocalizationContext);
 
+  const isOperator = channel.myRole === Role.OPERATOR;
   const refreshList = useCallback(() => {
     if (!channel) {
       setMembers([]);
@@ -63,35 +64,37 @@ export const MemberList = ({
                 channel,
                 size: 'small',
                 avatarSize: '24px',
-                renderListItemMenu: (props) => (
-                  <UserListItemMenu {...props}
-                    onToggleOperatorState={({ newStatus: isOperator }) => {
-                      const newMembers = [...members];
-                      for (const newMember of newMembers) {
-                        if (newMember.userId === member.userId) {
-                          newMember.role = isOperator ? Role.OPERATOR : Role.NONE;
-                          break;
+                renderListItemMenu: isOperator
+                  ? (props) => (
+                    <UserListItemMenu {...props}
+                      onToggleOperatorState={({ newStatus: isOperator }) => {
+                        const newMembers = [...members];
+                        for (const newMember of newMembers) {
+                          if (newMember.userId === member.userId) {
+                            newMember.role = isOperator ? Role.OPERATOR : Role.NONE;
+                            break;
+                          }
                         }
-                      }
-                      setMembers(newMembers);
-                    }}
-                    onToggleMuteState={({ newStatus: isMuted }) => {
-                      const newMembers = [...members];
-                      for (const newMember of newMembers) {
-                        if (newMember.userId === member.userId) {
-                          newMember.isMuted = isMuted;
-                          break;
+                        setMembers(newMembers);
+                      }}
+                      onToggleMuteState={({ newStatus: isMuted }) => {
+                        const newMembers = [...members];
+                        for (const newMember of newMembers) {
+                          if (newMember.userId === member.userId) {
+                            newMember.isMuted = isMuted;
+                            break;
+                          }
                         }
-                      }
-                      setMembers(newMembers);
-                    }}
-                    onToggleBanState={() => {
-                      setMembers(members.filter(({ userId }) => {
-                        return userId !== member.userId;
-                      }));
-                    }}
-                  />
-                ),
+                        setMembers(newMembers);
+                      }}
+                      onToggleBanState={() => {
+                        setMembers(members.filter(({ userId }) => {
+                          return userId !== member.userId;
+                        }));
+                      }}
+                    />
+                  )
+                  : () => <></>,
               })
             }
           </React.Fragment>
