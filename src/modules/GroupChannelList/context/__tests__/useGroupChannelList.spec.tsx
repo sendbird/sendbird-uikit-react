09a54b5bb23d6d8b9b8ecf15ef23c966f61d1c83
Junyoung Lim <git-babel@users.noreply.github.com>
2024-12-02 11:58:51 +0900

    [CLNP-5914] Fix unit tests and UI error (#1267)
    * Changelog
      * Fixed the broken unit tests of `GroupChannelListProvider`
      * Fixed the UI error when click `open in channel` in the other channel
    * Use `useDeepCompareEffect` in `ThreadProvider` and
    `GroupChannelListProvider`

--- src/modules/GroupChannelList/context/__tests__/useGroupChannelList.spec.tsx
+++ src/modules/GroupChannelList/context/__tests__/useGroupChannelList.spec.tsx
@@ -1,4 +1,4 @@
-import { GroupChannelListProvider, useGroupChannelListContext } from '../GroupChannelListProvider';
+import { GroupChannelListProvider } from '../GroupChannelListProvider';
 import { renderHook } from '@testing-library/react';
 import React from 'react';
 import { useGroupChannelList } from '../useGroupChannelList';
@@ -14,7 +14,12 @@ const mockState = {
       initialized: true,
     },
   },
-  config: { logger: console },
+  config: {
+    logger: console,
+    groupChannelList: {
+      enableTypingIndicator: true,
+    },
+  },
 };
 jest.mock('../../../../lib/Sendbird/context/hooks/useSendbird', () => ({
   __esModule: true,
@@ -33,17 +38,12 @@ jest.mock('@sendbird/uikit-tools', () => ({
   useGroupChannelHandler: jest.fn(() => {}),
 }));
 
-jest.mock('../GroupChannelListProvider', () => ({
-  ...jest.requireActual('../GroupChannelListProvider'),
-  useGroupChannelListContext: jest.fn(),
-}));
-
 const initialState = {
   className: '',
   selectedChannelUrl: '',
   disableAutoSelect: false,
-  allowProfileEdit: false,
-  isTypingIndicatorEnabled: false,
+  allowProfileEdit: true,
+  isTypingIndicatorEnabled: true,
   isMessageReceiptStatusEnabled: false,
   onChannelSelect: undefined,
   onChannelCreated: undefined,
@@ -59,14 +59,8 @@ const initialState = {
   loadMore: null,
 };
 
-const mockStore = {
-  getState: jest.fn(() => initialState),
-  setState: jest.fn(),
-  subscribe: jest.fn(() => jest.fn()),
-};
-
 const wrapper = ({ children }) => (
-  <GroupChannelListProvider onChannelSelect={jest.fn()} onChannelCreated={jest.fn()}>
+  <GroupChannelListProvider>
     {children}
   </GroupChannelListProvider>
 );
@@ -78,15 +72,12 @@ describe('GroupChannelListProvider', () => {
   });
 
   it('throws an error if used outside of GroupChannelListProvider', () => {
-    (useGroupChannelListContext as jest.Mock).mockReturnValue(null);
-
     expect(() => {
-      renderHook(() => useGroupChannelList(), { wrapper });
+      renderHook(() => useGroupChannelList());
     }).toThrow(new Error('useGroupChannelList must be used within a GroupChannelListProvider'));
   });
 
   it('provide the correct initial state', () => {
-    (useGroupChannelListContext as jest.Mock).mockReturnValue(mockStore);
     const { result } = renderHook(() => useGroupChannelList(), { wrapper });
 
     expect(result.current.state).toEqual(expect.objectContaining(initialState));
