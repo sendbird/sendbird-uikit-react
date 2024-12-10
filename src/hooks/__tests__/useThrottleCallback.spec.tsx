3cd30d54071ec9125d23af424f69568d9800abc4
junyoung.lim <junyoung.lim@sendbird.com>
2024-12-10 16:25:29 +0900

    Fix lint errors

--- src/hooks/__tests__/useThrottleCallback.spec.tsx
+++ src/hooks/__tests__/useThrottleCallback.spec.tsx
@@ -18,10 +18,14 @@ describe('useThrottleCallback', () => {
     throttleCallback();
     throttleCallback();
 
-    await new Promise(resolve => setTimeout(resolve, 100));
+    await new Promise(resolve => {
+      setTimeout(resolve, 100);
+    });
     expect(mockCallback).toHaveBeenCalledTimes(1);
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise(resolve => {
+      setTimeout(resolve, 1000);
+    });
     expect(mockCallback).toHaveBeenCalledTimes(1);
 
   });
@@ -37,13 +41,19 @@ describe('useThrottleCallback', () => {
     throttleCallback();
     throttleCallback();
 
-    await new Promise(resolve => setTimeout(resolve, 100));
+    await new Promise(resolve => {
+      setTimeout(resolve, 100);
+    });
     expect(mockCallback).toHaveBeenCalledTimes(0);
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise(resolve => {
+      setTimeout(resolve, 1000);
+    });
     expect(mockCallback).toHaveBeenCalledTimes(1);
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise(resolve => {
+      setTimeout(resolve, 1000);
+    });
     expect(mockCallback).toHaveBeenCalledTimes(1);
   });
 
