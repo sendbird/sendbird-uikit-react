38ff25bca7cd08ca7ec8617d5ce5e50457b0d56c
Junyoung Lim <git-babel@users.noreply.github.com>
2024-12-13 10:22:18 +0900

    [CLNP-6003] test: Add tests for hooks in `src/hooks` (#1283)
    ### Changelog
    * Added tests for hooks in `src/hooks`
    
    #### before
    <img width="1186" alt="스크린샷 2024-12-10 오후 4 09 19"
    src="https://github.com/user-attachments/assets/b22b914a-1280-4d22-b4f1-3e735fdd409e">
    
    #### after
    <img width="1099" alt="스크린샷 2024-12-10 오후 4 08 14"
    src="https://github.com/user-attachments/assets/3431656d-ff7a-4bfd-9df6-ee255d022dee">

---
+++ src/hooks/__tests__/useAsyncRequest.spec.tsx
@@ -0,0 +1,44 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useAsyncRequest } from '../useAsyncRequest';
+
+describe('useAsyncRequest', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('handle request with no response correctly', async () => {
+    const mockPromise = Promise.resolve();
+    const mockRequest = jest.fn().mockReturnValue(mockPromise);
+
+    const { result } = renderHook(() => useAsyncRequest(mockRequest));
+
+    await mockPromise;
+
+    expect(result.current.loading).toBe(false);
+  });
+
+  it('handle request with response correctly', async () => {
+    const mockResponse = { code: 'ok' };
+    const mockPromise = Promise.resolve(mockResponse);
+    const mockRequest = jest.fn().mockReturnValue(mockPromise);
+
+    const { result } = renderHook(() => useAsyncRequest(mockRequest));
+
+    await mockPromise;
+
+    expect(result.current.response).toBe(mockResponse);
+    expect(result.current.loading).toBe(false);
+  });
+
+  it('cancel request correctly', async () => {
+    const mockCancel = jest.fn();
+    const mockRequest = { cancel: mockCancel };
+
+    const { unmount } = renderHook(() => useAsyncRequest(mockRequest));
+
+    unmount();
+
+    expect(mockCancel).toBeCalled();
+  });
+
+});
