7ddff95d4e81b5070bc87bee416048acc7b92813
Chris Heo <chrisallo.h@gmail.com>
2025-01-02 21:19:26 -0800

    review fix

--- src/hooks/useConnectionState.ts
+++ src/hooks/useConnectionState.ts
@@ -1,6 +1,8 @@
 
 import { useState } from 'react';
-import { ConnectionHandler, ConnectionState } from '@sendbird/chat';
+import { ConnectionState } from '@sendbird/chat';
+
+import ConnectionHandler from '../lib/handlers/ConnectionHandler';
 import useSendbirdStateContext from './useSendbirdStateContext'
 import uuidv4 from '../utils/uuid';
 
@@ -9,13 +11,13 @@ export const useConnectionState = (): ConnectionState => {
   const { sdkStore } = stores;
   const { sdk } = sdkStore;
 
-  const [connectionState, setConnectionState] = useState(sdk.connectionState)
+  const [connectionState, setConnectionState] = useState(sdk.connectionState);
   sdk.addConnectionHandler(uuidv4(), new ConnectionHandler({
     onConnected: () => setConnectionState(ConnectionState.OPEN),
     onDisconnected: () => setConnectionState(ConnectionState.CLOSED),
     onReconnectStarted: () => setConnectionState(ConnectionState.CONNECTING),
     onReconnectSucceeded: () => setConnectionState(ConnectionState.OPEN),
     onReconnectFailed: () => setConnectionState(ConnectionState.CLOSED),
-  }))
-  return connectionState
-}
+  }));
+  return connectionState;
+};
