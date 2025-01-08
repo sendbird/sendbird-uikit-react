91844b65475aed8adf793bea8ebbf19370c9aaa4
junyoung.lim <junyoung.lim@sendbird.com>
2025-01-08 16:46:12 +0900

    Remove the console for debugging

--- src/utils/useDidMountEffect.ts
+++ src/utils/useDidMountEffect.ts
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 const useDidMountEffect = (func: () => void, deps: Array<unknown>): void => {
   const [didMount, setDidMount] = useState(false);
   useEffect(() => {
-    console.log('effect');
     if (didMount) {
       func();
     } else {
