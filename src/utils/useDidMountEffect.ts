486e2562a5b1989f985ce3feca7a986d0288662e
junyoung.lim <junyoung.lim@sendbird.com>
2025-01-08 14:38:19 +0900

    Fix broken test

--- src/utils/useDidMountEffect.ts
+++ src/utils/useDidMountEffect.ts
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 
 const useDidMountEffect = (func: () => void, deps: Array<unknown>): void => {
-  const [didMount, setDidmount] = useState(false);
+  const [didMount, setDidMount] = useState(false);
   useEffect(() => {
+    console.log('effect');
     if (didMount) {
       func();
     } else {
-      setDidmount(true);
+      setDidMount(true);
     }
   }, deps);
 };
