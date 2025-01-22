cdeb26e9487cbc7ffbdb29fd17e7d300f348bb56
Chris Heo <chrisallo.h@gmail.com>
2025-01-21 21:19:20 -0800

    [CLNP-6249] deprecated mark for ChannelXXX (#1302)
    - Added @deprecated annotation to the deprecated Channel components
    
    Fixes [CLNP-6249](https://sendbird.atlassian.net/browse/CLNP-6249)
    
    ### Changelogs
    
    ### Checklist
    
    Put an `x` in the boxes that apply. You can also fill these out after
    creating the PR. If unsure, ask the members.
    This is a reminder of what we look for before merging your code.
    
    - [ ] **All tests pass locally with my changes**
    - [ ] **I have added tests that prove my fix is effective or that my
    feature works**
    - [ ] **Public components / utils / props are appropriately exported**
    - [ ] I have added necessary documentation (if appropriate)
    
    
    ## External Contributions
    
    This project is not yet set up to accept pull requests from external
    contributors.
    
    If you have a pull request that you believe should be accepted, please
    contact
    the Developer Relations team <developer-advocates@sendbird.com> with
    details
    and we'll evaluate if we can set up a
    [CLA](https://en.wikipedia.org/wiki/Contributor_License_Agreement) to
    allow for the contribution.
    
    
    [CLNP-6249]:
    https://sendbird.atlassian.net/browse/CLNP-6249?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/modules/Channel/components/ChannelHeader/index.tsx
+++ src/modules/Channel/components/ChannelHeader/index.tsx
@@ -6,6 +6,12 @@ export interface ChannelHeaderProps {
   className?: string;
 }
 
+/**
+ * @deprecated This component is deprecated and will be removed in the next major update.
+ * Please use the `GroupChannel` component from '@sendbird/uikit-react/GroupChannel' instead.
+ * For more information, please refer to the migration guide:
+ * https://docs.sendbird.com/docs/chat/uikit/v3/react/introduction/group-channel-migration-guide
+ */
 export const ChannelHeader = ({ className }: ChannelHeaderProps) => {
   const context = useChannelContext();
   return (
--- src/modules/ChannelList/components/ChannelListUI/index.tsx
+++ src/modules/ChannelList/components/ChannelListUI/index.tsx
@@ -23,6 +23,12 @@ export interface ChannelListUIProps {
   renderPlaceHolderEmptyList?: (props: void) => React.ReactElement;
 }
 
+/**
+ * @deprecated This component is deprecated and will be removed in the next major update.
+ * Please use the `GroupChannel` component from '@sendbird/uikit-react/GroupChannel' instead.
+ * For more information, please refer to the migration guide:
+ * https://docs.sendbird.com/docs/chat/uikit/v3/react/introduction/group-channel-migration-guide
+ */
 const ChannelListUI: React.FC<ChannelListUIProps> = (props: ChannelListUIProps) => {
   const { renderHeader, renderChannelPreview, renderPlaceHolderError, renderPlaceHolderLoading, renderPlaceHolderEmptyList } = props;
 
