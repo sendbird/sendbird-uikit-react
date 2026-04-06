2ef39e308cfa3b7717f2c74ea06fc580addb17d7
Tyler Jeong <tyler.jeong@sendbird.com>
2026-04-06 15:48:48 +0900

    fix: add eslint disable comment for promise executor return in test

--- src/modules/MessageSearch/context/__tests__/useScrollCallback.spec.ts
+++ src/modules/MessageSearch/context/__tests__/useScrollCallback.spec.ts
@@ -163,6 +163,7 @@ describe('useScrollCallback', () => {
       }
     });
 
+    // eslint-disable-next-line no-promise-executor-return
     await new Promise(resolve => setTimeout(resolve, 0));
 
     expect(mockNext).toHaveBeenCalled();
